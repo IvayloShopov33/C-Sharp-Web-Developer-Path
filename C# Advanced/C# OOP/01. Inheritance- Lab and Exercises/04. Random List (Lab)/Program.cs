f86acbd7a74e38a18dbd2a09275ff63eb39ecc34
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-19 13:12:17 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/04. Random List (Lab)/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList<string> list = new RandomList<string>
+            {
+                "1",
+                "2",
+                "3"
+            };
+
+            list.RandomString();
+
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.Count);
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/04. Random List (Lab)/RandomList.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomRandomList
+{
+    public class RandomList<T> : List<T>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+
+        public T RandomString()
+        {
+            int randomIndex = random.Next(0, base.Count);
+            T element = base[randomIndex];
+            base.RemoveAt(randomIndex);
+
+            return element;
+        }
+    }
+}
