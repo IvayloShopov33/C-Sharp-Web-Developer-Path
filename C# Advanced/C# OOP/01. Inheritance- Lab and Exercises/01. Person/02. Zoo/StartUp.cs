88fd807db02e469f1c3bef8a8032eec4c3a25bd2
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-21 00:35:49 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/01. Person/02. Zoo/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Animal gorilla = new Gorilla("Simba");
+            Reptile snake = new Snake("Smok");
+            Mammal bear = new Bear("Mecho");
+
+            Console.WriteLine(gorilla.Name);
+            Console.WriteLine(snake.Name);
+            Console.WriteLine(bear.Name);
+        }
+    }
+}
