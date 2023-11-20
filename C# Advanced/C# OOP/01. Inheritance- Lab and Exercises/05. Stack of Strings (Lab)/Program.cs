3d7af117d23b05a925f472dccabc133bca3a4793
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-20 02:18:23 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/05. Stack of Strings (Lab)/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            List<string> items = new List<string>()
+            {
+                "1",
+                "2",
+                "3"
+            };
+
+            stackOfStrings.AddRange(items);
+            Console.WriteLine(string.Join(", ", stackOfStrings));
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/05. Stack of Strings (Lab)/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return base.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> items)
+        {
+            foreach (string item in items)
+            {
+                base.Push(item);
+            }
+        }
+    }
+}
