5a1b9a0eab0a1118abf25a326a310ab6914c06a0
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-22 00:40:01 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/03. Players and Monsters/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Hero hero = new Wizard("Shopov", 10);
+            Console.WriteLine(hero);
+
+            Elf elf = new MuseElf("Ronaldo", 15);
+            Console.WriteLine(elf);
+
+            Knight knight = new BladeKnight("Caesar", 60);
+            Console.WriteLine(knight);
+        }
+    }
+}
