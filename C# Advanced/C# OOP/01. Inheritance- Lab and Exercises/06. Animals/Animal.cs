81fa09f2e0bd63e9cd9fcf6e15af6b352c7f39c1
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-24 00:26:55 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/06. Animals/Animal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get { return this.gender; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.gender = value;
+            }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.AppendLine(this.GetType().Name);
+            output.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            output.AppendLine(this.ProduceSound());
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/06. Animals/StartUp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input == "Beast!")
+                {
+                    foreach (Animal animal in animals)
+                    {
+                        Console.WriteLine(animal);
+                    }
+
+                    break;
+                }
+
+                string[] animalDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    string name = animalDetails[0];
+                    int age = int.Parse(animalDetails[1]);
+                    string gender = string.Empty;
+                    if (input == "Cat")
+                    {
+                        gender = animalDetails[2];
+                        Cat cat = new Cat(name, age, gender);
+                        animals.Add(cat);
+                    }
+                    else if (input == "Dog")
+                    {
+                        gender = animalDetails[2];
+                        Dog dog = new Dog(name, age, gender);
+                        animals.Add(dog);
+                    }
+                    else if (input == "Frog")
+                    {
+                        gender = animalDetails[2];
+                        Frog frog = new Frog(name, age, gender);
+                        animals.Add(frog);
+                    }
+                    else if (input == "Kitten")
+                    {
+                        Kitten kitten = new Kitten(name, age);
+                        animals.Add(kitten);
+                    }
+                    else if (input == "Tomcat")
+                    {
+                        Tomcat tomcat = new Tomcat(name, age);
+                        animals.Add(tomcat);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
