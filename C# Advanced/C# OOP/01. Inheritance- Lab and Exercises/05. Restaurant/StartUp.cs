1ff99f1e6ad8b6a8e5c55c61cac14fbb5288406a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-24 00:25:29 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/01. Inheritance- Lab and Exercises/05. Restaurant/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Product fish = new Fish("Salmon", 10.50m);
+            Product coffee = new Coffee("Latte", 50);
+            Dessert cake = new Cake("Garash");
+            HotBeverage tea = new Tea("Tea", 6, 100);
+
+            Console.WriteLine(fish.Name);
+            Console.WriteLine(coffee.Price);
+            Console.WriteLine(cake.Calories);
+            Console.WriteLine(tea.Milliliters);
+        }
+    }
+}
