3a7c8485a5a2bdf985be0b8cd12660f3cdd5a08f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-25 00:20:20 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/02. Encapsulation- Lab and Exercises/1. Sort People by Name and Age (Lab)/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 1; i <= lines; i++)
+            {
+                string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(personDetails[0], personDetails[1], int.Parse(personDetails[2]));
+                people.Add(person);
+            }
+
+            people.OrderBy(p => p.FirstName)
+                   .ThenBy(p => p.Age)
+                   .ToList()
+                   .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
