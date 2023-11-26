b4997d33a0b0d01176feb428a4af947b471108b8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-27 01:41:12 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/02. Encapsulation- Lab and Exercises/3. Validation of Data (Lab)/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 1; i <= lines; i++)
+            {
+                string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    Person person = new Person(personDetails[0],
+                                        personDetails[1],
+                                        int.Parse(personDetails[2]),
+                                        decimal.Parse(personDetails[3]));
+
+                    people.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            decimal parcentage = decimal.Parse(Console.ReadLine());
+            people.ForEach(p => p.IncreaseSalary(parcentage));
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
