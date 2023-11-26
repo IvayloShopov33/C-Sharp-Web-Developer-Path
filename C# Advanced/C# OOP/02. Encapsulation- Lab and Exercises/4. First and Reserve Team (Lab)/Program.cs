df1c1fbe9ffb896b34373d9097667249764a256f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-27 01:42:21 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/02. Encapsulation- Lab and Exercises/4. First and Reserve Team (Lab)/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= lines; i++)
+            {
+                try
+                {
+                    string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    Person person = new Person(personDetails[0],
+                                        personDetails[1],
+                                        int.Parse(personDetails[2]),
+                                        decimal.Parse(personDetails[3]));
+
+                    people.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Team team = new Team("Shopov FC");
+
+            foreach (Person person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine(team);
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/02. Encapsulation- Lab and Exercises/4. First and Reserve Team (Lab)/Team.cs
@@ -0,0 +1,49 @@
+﻿namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public IReadOnlyList<Person> FirstTeam => this.firstTeam;
+
+        public IReadOnlyList<Person> ReserveTeam => this.reserveTeam;
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.firstTeam.Add(person);
+            }
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"First team has {this.FirstTeam.Count} players. {Environment.NewLine}Reserve team has {this.ReserveTeam.Count} players.";
+        }
+    }
+}
