d926846026e2ee64a70e33dad6fbe15db5f77bfc
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-11-29 00:48:52 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/02. Encapsulation- Lab and Exercises/03. Shopping Spree/Person.cs
@@ -0,0 +1,81 @@
+﻿namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private int money;
+        private List<Product> products;
+
+        public Person(string name, int money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Money
+        {
+            get
+            {
+                return this.money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public IReadOnlyList<Product> Products => this.products;
+
+        public void BuyProduct(Product product)
+        {
+            if (this.Money >= product.Cost)
+            {
+                this.Money -= product.Cost;
+                this.products.Add(product);
+                Console.WriteLine($"{this.Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} can't afford {product.Name}");
+            }
+        }
+
+        public override string ToString()
+        {
+            string outputProduct = string.Empty;
+            if (this.Products.Count == 0)
+            {
+                outputProduct = "Nothing bought";
+            }
+            else
+            {
+                outputProduct = string.Join(", ", this.Products.Select(x => x.Name));
+            }
+
+            return $"{this.Name} - {outputProduct}";
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/02. Encapsulation- Lab and Exercises/03. Shopping Spree/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new();
+            List<Product> products = new();
+            string[] peopleDetails = Console.ReadLine().Split(new char[2] { ';', '=' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] productsDetails = Console.ReadLine().Split(new char[2] { ';', '=' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < peopleDetails.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    try
+                    {
+                        Person person = new Person(peopleDetails[i - 1], int.Parse(peopleDetails[i]));
+                        people.Add(person);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Environment.Exit(0);
+                    }
+                }
+            }
+
+            for (int i = 0; i < productsDetails.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    try
+                    {
+                        Product product = new Product(productsDetails[i - 1], int.Parse(productsDetails[i]));
+                        products.Add(product);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Environment.Exit(0);
+                    }
+                }
+            }
+
+            string[] commands;
+            while (true)
+            {
+                commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (commands[0] == "END")
+                {
+                    foreach (Person person in people)
+                    {
+                        Console.WriteLine(person);
+                    }
+
+                    break;
+                }
+
+                Person selectedPerson = people.FirstOrDefault(x => x.Name == commands[0]);
+                Product selectedProduct = products.FirstOrDefault(x => x.Name == commands[1]);
+                if (selectedPerson != null && selectedProduct != null)
+                {
+                    selectedPerson.BuyProduct(selectedProduct);
+                }
+            }
+        }
+    }
+}
