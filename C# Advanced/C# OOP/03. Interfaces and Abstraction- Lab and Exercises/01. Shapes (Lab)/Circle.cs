9010c7a46778116d4e92ecf34bcb967a06ecd119
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-12-01 01:31:26 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/01. Shapes (Lab)/Circle.cs
@@ -0,0 +1,55 @@
+﻿namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public int Radius
+        {
+            get
+            {
+                return this.radius;
+            }
+            private set
+            {
+                this.radius = value;
+            }
+        }
+
+        public void Draw()
+        {
+            Console.Write(new string(' ', this.Radius));
+            Console.WriteLine(new string('*', this.Radius * 2 + 1));
+            int count = 0;
+            for (int i = 2; i < this.Radius + 1; i++)
+            {
+                Console.Write(new string(' ', this.Radius - i));
+                Console.Write("**");
+                Console.Write(new string(' ', this.Radius * 2 + 1 + count));
+                Console.WriteLine("**");
+                count += 2;
+            }
+
+            Console.Write("*");
+            Console.Write(new string(' ', this.Radius * 2 + 1 + count));
+            Console.WriteLine("*");
+
+            for (int i = this.Radius; i >= 2; i--)
+            {
+                Console.Write(new string(' ', this.Radius - i));
+                Console.Write("**");
+                count -= 2;
+                Console.Write(new string(' ', this.Radius * 2 + 1 + count));
+                Console.WriteLine("**");
+            }
+
+            Console.Write(new string(' ', this.Radius));
+            Console.WriteLine(new string('*', this.Radius * 2 + 1));
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/01. Shapes (Lab)/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                this.width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                this.height = value;
+            }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', this.Width));
+
+            for (int i = 0; i < this.Height - 2; i++)
+            {
+                Console.Write("*");
+                Console.Write(new string(' ', this.Width - 2));
+                Console.WriteLine("*");
+            }
+
+            Console.WriteLine(new string('*', this.Width));
+        }
+    }
+}
