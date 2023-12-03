a0ec4c4d26b0fe17157be2b47841887121706f93
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-12-04 01:41:37 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/03. Telephony/Program.cs
@@ -0,0 +1,49 @@
+﻿using Telephony.Models;
+using Telephony.Models.Interfaces;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] phoneNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] urls = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            ICallable callablePhone = default;
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                if (phoneNumber.Length == 7)
+                {
+                    callablePhone = new StationaryPhone();
+                }
+                else
+                {
+                    callablePhone = new Smartphone();
+                }
+
+                try
+                {
+                    Console.WriteLine(callablePhone.Call(phoneNumber));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            IBrowsable browsablePhone = new Smartphone();
+            foreach (string url in urls)
+            {
+                try
+                {
+                    Console.WriteLine(browsablePhone.Browse(url));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
