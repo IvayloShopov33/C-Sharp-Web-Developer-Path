8ffc9d0ec973df78c00fbf02ef9b95b668465f46
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-12-05 02:02:11 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/05. Birthday Celebrations/Core/Engine.cs
@@ -0,0 +1,42 @@
+﻿using BirthdayCelebrations.Core.Interfaces;
+using BirthdayCelebrations.Models;
+using BirthdayCelebrations.Models.Interfaces;
+
+namespace BirthdayCelebrations.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            List<IBirthable> identities = new List<IBirthable>();
+
+            string[] input;
+            while (true)
+            {
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "End")
+                {
+                    break;
+                }
+
+                if (input[0] == "Citizen" && input.Length == 5)
+                {
+                    IBirthable citizen = new Citizen(input[1], int.Parse(input[2]), input[3], input[4]);
+                    identities.Add(citizen);
+                }
+                else if (input[0] == "Pet" && input.Length == 3)
+                {
+                    IBirthable pet = new Pet(input[1], input[2]);
+                    identities.Add(pet);
+                }
+            }
+
+            string yearForBirthValidation = Console.ReadLine();
+            foreach (IBirthable identity in identities.Where(x => x.BirthDate.EndsWith(yearForBirthValidation)))
+            {
+                Console.WriteLine(identity.BirthDate);
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/05. Birthday Celebrations/Program.cs
@@ -0,0 +1,14 @@
+﻿using BirthdayCelebrations.Core;
+using BirthdayCelebrations.Core.Interfaces;
+
+namespace BirthdayCelebrations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
