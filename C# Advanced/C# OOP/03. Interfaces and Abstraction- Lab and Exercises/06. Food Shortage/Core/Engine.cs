c84fc0201a6c16d862043014b6d9019453161cea
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-12-06 01:32:46 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/06. Food Shortage/Core/Engine.cs
@@ -0,0 +1,50 @@
+﻿using FoodShortage.Core.Interfaces;
+using FoodShortage.Models;
+using FoodShortage.Models.Interfaces;
+
+namespace FoodShortage.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            List<IBuyer> buyers = new List<IBuyer>();
+
+            int peopleCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= peopleCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input.Length == 4)
+                {
+                    IBuyer citizen = new Citizen(input[0], int.Parse(input[1]), input[2], input[3]);
+                    buyers.Add(citizen);
+                }
+                else if (input.Length == 3)
+                {
+                    IBuyer rebel = new Rebel(input[0], int.Parse(input[1]), input[2]);
+                    buyers.Add(rebel);
+                }
+            }
+
+            string buyerName = string.Empty;
+            while (true)
+            {
+                buyerName = Console.ReadLine();
+                if (buyerName == "End")
+                {
+                    break;
+                }
+
+                IBuyer buyer = buyers.FirstOrDefault(x => x.Name == buyerName);
+                if (buyer != null)
+                {
+                    buyer.BuyFood();
+                }
+            }
+
+            int totalboughtFood = buyers.Select(x => x.Food).Sum();
+            Console.WriteLine(totalboughtFood);
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/06. Food Shortage/Program.cs
@@ -0,0 +1,14 @@
+﻿using FoodShortage.Core;
+using FoodShortage.Core.Interfaces;
+
+namespace FoodShortage
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
