26fb2888b59e7eda4fd386154bb825a7fdccddae
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-12-07 01:26:23 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/09. Explicit Interfaces/Core/Engine.cs
@@ -0,0 +1,29 @@
+﻿using ExplicitInterfaces.Core.Interfaces;
+using ExplicitInterfaces.Models;
+using ExplicitInterfaces.Models.Interfaces;
+
+namespace ExplicitInterfaces.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            string[] input;
+
+            while (true)
+            {
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "End")
+                {
+                    break;
+                }
+
+                IPerson person = new Citizen(input[0], input[1], int.Parse(input[2]));
+                IResident resident = new Citizen(input[0], input[1], int.Parse(input[2]));
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName() + person.GetName());
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/09. Explicit Interfaces/Models/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using ExplicitInterfaces.Models.Interfaces;
+
+namespace ExplicitInterfaces.Models
+{
+    public class Citizen : IResident, IPerson
+    {
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public string Country { get; private set; }
+
+        public int Age { get; private set; }
+
+        string IResident.GetName() => "Mr/Ms/Mrs ";
+
+        string IPerson.GetName() => this.Name;
+    }
+}
