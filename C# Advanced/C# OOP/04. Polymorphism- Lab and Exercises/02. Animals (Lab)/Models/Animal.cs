3de86904938155cc6b021c0e52fac6bd8091bf0e
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-12-08 01:08:22 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/04. Polymorphism- Lab and Exercises/02. Animals (Lab)/Models/Animal.cs
@@ -0,0 +1,40 @@
+﻿namespace Animals.Models
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public string FavouriteFood
+        {
+            get
+            {
+                return this.favouriteFood;
+            }
+            private set
+            {
+                this.favouriteFood = value;
+            }
+        }
+
+        public virtual string ExplainSelf() => base.ToString();
+    }
+}
---
+++ C# Advanced/C# OOP/04. Polymorphism- Lab and Exercises/02. Animals (Lab)/Program.cs
@@ -0,0 +1,16 @@
+﻿using Animals.Models;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat("Misho", "Whiskas");
+            Animal dog = new Dog("Sharo", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+        }
+    }
+}
