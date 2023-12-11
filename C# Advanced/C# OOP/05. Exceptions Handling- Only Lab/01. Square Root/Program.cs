4b4a9048355a0a522fa6622adf7b96f8eb9f69e7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-12-12 01:04:23 +0200

    Add files via upload

---
+++ C# Advanced/C# OOP/05. Exceptions Handling- Only Lab/01. Square Root/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                IsTheNumberValid(number);
+                PrintTheSquareRootOfTheNumber(number);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        public static void IsTheNumberValid(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+        }
+
+        public static void PrintTheSquareRootOfTheNumber(int number) => Console.WriteLine(Math.Sqrt(number));
+    }
+}
