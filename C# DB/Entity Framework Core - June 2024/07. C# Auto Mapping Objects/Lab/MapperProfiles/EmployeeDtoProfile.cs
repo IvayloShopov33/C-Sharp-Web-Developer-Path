77c09b9e4815a13572d87c162fb7a12d26d779b5
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-07-13 00:52:39 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - June 2024/07. C# Auto Mapping Objects/Lab/MapperProfiles/EmployeeDtoProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Demo.Data.Models;
+
+namespace Demo.MapperProfiles
+{
+    public class EmployeeDtoProfile : Profile
+    {
+        public EmployeeDtoProfile()
+        {
+            this.CreateMap<Employee, EmployeeDto>()
+                    .ForMember(x => x.FullName, options =>
+                          options.MapFrom(x => $"{x.FirstName} {x.LastName}"))
+                    .ForMember(x => x.AddressText, options =>
+                          options.MapFrom(x => x.Address.AddressText))
+                    .ForMember(x => x.ManagerFullName, options =>
+                          options.MapFrom(x => $"{x.Manager.FirstName} {x.Manager.LastName}"))
+                    .ReverseMap();
+        }
+    }
+}
---
+++ C# DB/Entity Framework Core - June 2024/07. C# Auto Mapping Objects/Lab/Program.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Demo.Data;
+using Demo.Data.Models;
+using Demo.MapperProfiles;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<EmployeeDtoProfile>();
+            });
+
+            var mapper = configuration.CreateMapper();
+
+            var db = new SoftUniContext();
+            var employee = db.Employees
+                .Include(e => e.Department)
+                .Include(e => e.Manager)
+                .Include(e => e.Address)
+                .Include(e => e.Projects)
+                .Where(x => x.EmployeeId == 1)
+                .FirstOrDefault();
+
+            var employeeDto = mapper.Map<EmployeeDto>(employee);
+            Console.WriteLine(JsonConvert.SerializeObject(employeeDto, Formatting.Indented));
+
+            var employees = db.Employees
+                .Where(x => x.FirstName.StartsWith("G"))
+                .ProjectTo<EmployeeDto>(configuration)
+                .ToList();
+
+            Console.WriteLine(JsonConvert.SerializeObject(employees, Formatting.Indented));
+
+            EmployeeDto firstEmployeeDto = employees.FirstOrDefault();
+            Employee dbEmployee = mapper.Map<Employee>(firstEmployeeDto);
+
+            Console.WriteLine(JsonConvert.SerializeObject(dbEmployee, Formatting.Indented));
+        }
+    }
+
+    public class EmployeeDto
+    {
+        public string FullName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string ManagerFullName { get; set; }
+
+        public string AddressText { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public int ProjectsCount { get; set; }
+    }
+}
