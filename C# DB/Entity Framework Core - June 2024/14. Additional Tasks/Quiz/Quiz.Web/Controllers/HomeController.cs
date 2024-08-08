ee596340b332aa012717a3071579bbe598b5a7f5
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-08 17:26:40 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - June 2024/14. Additional Tasks/Quiz/Quiz.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Quiz.Services.Contracts;
+using Quiz.Web.Models;
+
+namespace Quiz.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IQuizService quizService;
+
+        public HomeController(IQuizService quizService)
+        {
+            this.quizService = quizService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var userName = this.User.Identity?.Name;
+            var userQuizzes = this.quizService.GetQuizzesByUserName(userName);
+
+            return View(userQuizzes);
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
