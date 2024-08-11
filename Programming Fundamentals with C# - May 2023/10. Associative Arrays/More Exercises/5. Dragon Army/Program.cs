2a6fcb4044adb8516315b476ab942864dd1b5b19
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-11 16:51:44 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/10. Associative Arrays/More Exercises/5. Dragon Army/Program.cs
+++ Programming Fundamentals with C# - May 2023/10. Associative Arrays/More Exercises/5. Dragon Army/Program.cs
@@ -11,6 +11,7 @@ namespace _5._Dragon_Army
             Dictionary<string, List<Dragon>> dragons =
                 new Dictionary<string, List<Dragon>>();
             int dragonsCount = int.Parse(Console.ReadLine());
+            
             for (int i = 1; i <= dragonsCount; i++)
             {
                 string[] dragonDetails = Console.ReadLine().Split();
@@ -19,23 +20,28 @@ namespace _5._Dragon_Army
                 int damage = 45;
                 int health = 250;
                 int armor = 10;
+                
                 if (dragonDetails[2] != "null")
                 {
                     damage = int.Parse(dragonDetails[2]);
                 }
+                
                 if (dragonDetails[3]!="null")
                 {
                     health = int.Parse(dragonDetails[3]);
                 }
+                
                 if (dragonDetails[4]!="null")
                 {
                     armor = int.Parse(dragonDetails[4]);
                 }
+                
                 Dragon dragon = new Dragon(name, damage, health, armor);
                 if (!dragons.ContainsKey(type))
                 {
                     dragons[type] = new List<Dragon>();
                 }
+                
                 Dragon existingDragon = dragons[type].FirstOrDefault(g => g.Name == name);
                 if (existingDragon!=null)
                 {
@@ -48,12 +54,14 @@ namespace _5._Dragon_Army
                     dragons[type].Add(dragon);
                 }
             }
+            
             foreach (KeyValuePair<string, List<Dragon>> item in dragons)
             {
                 double averageDamage = item.Value.Average(g => g.Damage);
                 double averageHealth = item.Value.Average(x => x.Health);
                 double averageArmor = item.Value.Average(f => f.Armor);
                 Console.WriteLine($"{item.Key}::({averageDamage:f2}/{averageHealth:f2}/{averageArmor:f2})");
+                
                 foreach (Dragon dragonsToPrint in item.Value.OrderBy(g=>g.Name))
                 {
                     Console.WriteLine($"-{dragonsToPrint.Name} -> damage: {dragonsToPrint.Damage}, health: {dragonsToPrint.Health}, armor: {dragonsToPrint.Armor}");
@@ -61,6 +69,7 @@ namespace _5._Dragon_Army
             }
         }
     }
+    
     public class Dragon
     {
         public Dragon(string name, int damage, int health, int armor)
@@ -70,9 +79,13 @@ namespace _5._Dragon_Army
             this.Health = health;
             this.Armor = armor;
         }
+        
         public string Name { get; private set; }
+        
         public int Damage { get; set; }
+        
         public int Health { get; set; }
+        
         public int Armor { get; set; }
     }
 }
