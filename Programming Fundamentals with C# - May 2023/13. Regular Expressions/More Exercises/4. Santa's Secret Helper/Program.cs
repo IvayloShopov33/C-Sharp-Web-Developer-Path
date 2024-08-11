74977dbbfdb935fddd5561a35188f2760fcb91f5
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-11 17:10:51 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/13. Regular Expressions/More Exercises/4. Santa's Secret Helper/Program.cs
+++ Programming Fundamentals with C# - May 2023/13. Regular Expressions/More Exercises/4. Santa's Secret Helper/Program.cs
@@ -8,26 +8,32 @@ namespace _4._Santa_s_Secret_Helper
     {
         static void Main(string[] args)
         {
-            string pattern = @"[^\@\-\!\:\>]*\@(?<name>[A-Za-z]+)[^\@\-\!\:\>]*\!(?<behaviour>G|N)\!";
             int key = int.Parse(Console.ReadLine());
+            
+            string pattern = @"[^\@\-\!\:\>]*\@(?<name>[A-Za-z]+)[^\@\-\!\:\>]*\!(?<behaviour>G|N)\!";
             string message = Console.ReadLine();
             StringBuilder stringBuilder = new StringBuilder();
-            while (message!="end")
+            
+            while (message != "end")
             {
                 char[] messageToCharArray = message.ToCharArray();
                 string decryptedMessage = string.Empty;
+                
                 foreach (char item in messageToCharArray)
                 {
                     int newIndex = item - key;
                     decryptedMessage += (char)newIndex;
                 }
+                
                 Match match = Regex.Match(decryptedMessage, pattern);
                 if (match.Success && match.Groups["behaviour"].Value == "G")
                 {
                     stringBuilder.AppendLine(match.Groups["name"].Value);
                 }
+                
                 message = Console.ReadLine();
             }
+            
             Console.WriteLine(stringBuilder);
         }
     }
