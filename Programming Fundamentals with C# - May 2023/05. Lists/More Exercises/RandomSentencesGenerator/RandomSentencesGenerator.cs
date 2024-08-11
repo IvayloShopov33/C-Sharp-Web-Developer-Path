470b1a73d8ac449ce9273c62f668ad3872014cda
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-11 16:13:51 +0300

    Update RandomSentencesGenerator.cs

--- Programming Fundamentals with C# - May 2023/05. Lists/More Exercises/RandomSentencesGenerator/RandomSentencesGenerator.cs
+++ Programming Fundamentals with C# - May 2023/05. Lists/More Exercises/RandomSentencesGenerator/RandomSentencesGenerator.cs
@@ -14,10 +14,13 @@ namespace RandomSentencesGenerator
             string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
             string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
             string[] details = { "near the river", "at home", "in the park" };
+            
             Console.Write("Hello, please write how many sentences you want to generate at random: ");
+            
             int countOfSentences = int.Parse(Console.ReadLine());
             int count = 1;
-            while (count<=countOfSentences)
+            
+            while (count <= countOfSentences)
             {
                 string randomName = GetRandomWord(names);
                 string randomPlace = GetRandomWord(places);
@@ -25,20 +28,22 @@ namespace RandomSentencesGenerator
                 string randomNoun = GetRandomWord(nouns);
                 string randomAdverb = GetRandomWord(adverbs);
                 string randomDetail = GetRandomWord(details);
+                
                 string who = $"{randomName} from {randomPlace}";
                 string action = $"{randomAdverb} {randomVerb} {randomNoun}";
                 string sentence = $"{who} {action} {randomDetail}.";
+                
                 Console.WriteLine(sentence);
                 count++;
-            }
-            
-
+            }            
         }
+        
         static string GetRandomWord(string[] words)
         {
             Random random = new Random();
             int randomIndex = random.Next(words.Length);
             string word = words[randomIndex];
+            
             return word;
         }
     }
