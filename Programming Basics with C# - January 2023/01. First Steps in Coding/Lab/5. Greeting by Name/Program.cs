4f196b64c0f3e36a8c72c81a8cf251ced513e84e
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-14 01:52:14 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/01. First Steps in Coding/Lab/5. Greeting by Name/Program.cs
+++ Programming Basics with C# - January 2023/01. First Steps in Coding/Lab/5. Greeting by Name/Program.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ConsoleApp5
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
-            int S = a * b;
-            Console.WriteLine(S);
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
         }
     }
 }
