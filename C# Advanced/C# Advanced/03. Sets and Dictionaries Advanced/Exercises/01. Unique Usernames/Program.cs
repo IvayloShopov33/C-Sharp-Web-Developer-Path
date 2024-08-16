072805456c46614f59ce168e59bd7e389f7e07c1
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-16 18:27:56 +0300

    Add files via upload

---
+++ C# Advanced/C# Advanced/03. Sets and Dictionaries Advanced/Exercises/01. Unique Usernames/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new HashSet<string>();
+            int inputLinesCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= inputLinesCount; i++)
+            {
+                string username = Console.ReadLine();
+                usernames.Add(username);
+            }
+            
+            Console.WriteLine(string.Join(Environment.NewLine, usernames));
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/03. Sets and Dictionaries Advanced/Exercises/03. Periodic Table/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _3._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+            int inputLinesCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= inputLinesCount; i++)
+            {
+                string[] inputElements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string element in inputElements)
+                {
+                    chemicalElements.Add(element);
+                }
+            }
+            
+            Console.WriteLine(string.Join(' ', chemicalElements));
+        }
+    }
+}
