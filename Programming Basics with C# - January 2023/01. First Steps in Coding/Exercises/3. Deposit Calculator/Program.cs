f46e3201a9164ad6441585b9017dfeef3169949d
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-26 02:23:53 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/01. First Steps in Coding/Exercises/3. Deposit Calculator/Program.cs
+++ Programming Basics with C# - January 2023/01. First Steps in Coding/Exercises/3. Deposit Calculator/Program.cs
@@ -8,10 +8,10 @@ namespace _03._Deposit_Calculator
         {
             double deposit = double.Parse(Console.ReadLine());
             int month = int.Parse(Console.ReadLine());
-            double rate = double.Parse(Console.ReadLine())/100;
+            double rate = double.Parse(Console.ReadLine()) / 100;
+            
             double sum = deposit + month * ((deposit * rate) / 12);
             Console.WriteLine(sum);
-
         }
     }
 }
