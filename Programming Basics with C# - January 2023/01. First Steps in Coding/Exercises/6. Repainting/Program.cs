bd1e135956fe66d75c33a64b9fd96c6b87e47d7f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-29 02:21:04 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/01. First Steps in Coding/Exercises/6. Repainting/Program.cs
+++ Programming Basics with C# - January 2023/01. First Steps in Coding/Exercises/6. Repainting/Program.cs
@@ -6,19 +6,23 @@ namespace _06._Repainting
     {
         static void Main(string[] args)
         {
-            int nylon = int.Parse(Console.ReadLine())+2;
+            int nylon = int.Parse(Console.ReadLine()) + 2;
             double paint = int.Parse(Console.ReadLine());
+
             paint = paint + paint * 0.1;   
+
             int thinner = int.Parse(Console.ReadLine());
             int hours = int.Parse(Console.ReadLine());
+
             double nylonPrice = nylon * 1.5;
             double paintPrice = paint * 14.5;
             double thinnerPrice = thinner * 5;
+
             double sum = nylonPrice + paintPrice + thinnerPrice + 0.4;
             double priceM = sum * 0.3;
             double allPrice = sum + priceM * hours;
-            Console.WriteLine(allPrice);
-            
+
+            Console.WriteLine(allPrice);           
         }
     }
 }
