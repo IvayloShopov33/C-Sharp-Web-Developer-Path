9aeb3c3a124f62cf76169afa15f791bc7d9bbb8d
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-08-28 20:27:58 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/01. First Steps in Coding/Exercises/5. Supplies for School/Program.cs
+++ Programming Basics with C# - January 2023/01. First Steps in Coding/Exercises/5. Supplies for School/Program.cs
@@ -9,14 +9,16 @@ namespace _05._Supplies_for_School
             int pens = int.Parse(Console.ReadLine());
             int markers = int.Parse(Console.ReadLine());
             int cleanLitters = int.Parse(Console.ReadLine());
-            double rate= double.Parse(Console.ReadLine())/100;           
+
+            double rate = double.Parse(Console.ReadLine()) / 100;           
             double pensPrice = pens * 5.8;
             double markersPrice = markers * 7.2;
             double cleanLittersPrice = cleanLitters * 1.2;
+
             double sum = pensPrice + markersPrice + cleanLittersPrice;
             double allWithDiscount = sum - (sum * rate);
-            Console.WriteLine(allWithDiscount);
 
+            Console.WriteLine(allWithDiscount);
         }
     }
 }
