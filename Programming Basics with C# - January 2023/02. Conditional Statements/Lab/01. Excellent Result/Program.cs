78a2b2583da49ab33caf19bd591bfb60c4ffcf18
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-03 02:19:04 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Lab/01. Excellent Result/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Lab/01. Excellent Result/Program.cs
@@ -7,7 +7,7 @@ namespace _01._Excellent_Result
         static void Main(string[] args)
         {
             double a = double.Parse(Console.ReadLine());
-            if (a>=5.5 && a<=6)
+            if (a >= 5.5 && a <= 6)
             {
                 Console.WriteLine("Excellent!");
             }
