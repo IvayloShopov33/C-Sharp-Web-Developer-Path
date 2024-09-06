8ceef662335c343304121a1bd79a92f7f4f7e116
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-07 02:53:49 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Lab/05. Number 100-200/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Lab/05. Number 100-200/Program.cs
@@ -7,15 +7,16 @@ namespace _05._Number_100_200
         static void Main(string[] args)
         {
             int a = int.Parse(Console.ReadLine());
-            if (a<100)
+            
+            if (a < 100)
             {
                 Console.WriteLine("Less than 100");
             }
-            else if(a>=100 && a<=200)
+            else if(a <= 200)
             {
                 Console.WriteLine("Between 100 and 200");
             }
-            else if (a>200)
+            else
             {
                 Console.WriteLine("Greater than 200");
             }
