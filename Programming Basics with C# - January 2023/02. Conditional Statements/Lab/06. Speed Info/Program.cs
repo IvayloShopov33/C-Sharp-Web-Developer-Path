cf3d19ebf805c1316a84578735f206486e103da7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-08 03:33:54 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Lab/06. Speed Info/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Lab/06. Speed Info/Program.cs
@@ -7,23 +7,24 @@ namespace _06._Speed_Info
         static void Main(string[] args)
         {
             double speed = double.Parse(Console.ReadLine());
-            if (speed<=10)
+            
+            if (speed <= 10)
             {
                 Console.WriteLine("slow");
             }
-            else if (speed>10 && speed<=50)
+            else if (speed <= 50)
             {
                 Console.WriteLine("average");
             }
-            else if (speed>50 && speed<=150)
+            else if (speed <= 150)
             {
                 Console.WriteLine("fast");
             }
-            else if (speed>150 && speed<=1000)
+            else if (speed <= 1000)
             {
                 Console.WriteLine("ultra fast");
             }
-            else if (speed>1000)
+            else
             {
                 Console.WriteLine("extremely fast");
             }
