3953c1275264524fcb69a79f6fbc6592270456ec
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-09 03:48:11 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Lab/07. Area of Figures/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Lab/07. Area of Figures/Program.cs
@@ -8,28 +8,34 @@ namespace _07._Area_of_Figures
         {
             string figure = Console.ReadLine();
             double area = 0;
+            
             if (figure == "square")
             {
                 double a = double.Parse(Console.ReadLine());
+                
                 area = a * a;
             }
             else if (figure == "rectangle")
             {
                 double b = double.Parse(Console.ReadLine());
                 double c = double.Parse(Console.ReadLine());
-                 area = b * c;
+                
+                area = b * c;
             }
             else if (figure == "circle")
             {
                 double radius = double.Parse(Console.ReadLine());
-                 area = Math.PI * radius * radius;
+                
+                area = Math.PI * radius * radius;
             }
-            else if(figure=="triangle")
+            else if (figure == "triangle")
             {
                 double side = double.Parse(Console.ReadLine());
                 double height = double.Parse(Console.ReadLine());
-                 area = side * height / 2;
+                
+                area = side * height / 2;
             }
+            
             Console.WriteLine($"{area:f3}");
         }
     }
