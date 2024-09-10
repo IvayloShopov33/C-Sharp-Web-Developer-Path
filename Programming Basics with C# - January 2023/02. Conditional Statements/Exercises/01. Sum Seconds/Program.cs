84f55a62db0b461a00be4432ba91f62eded6deb6
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-10 03:12:12 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/01. Sum Seconds/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/01. Sum Seconds/Program.cs
@@ -9,9 +9,11 @@ namespace _01._Sum_Seconds
             int first = int.Parse(Console.ReadLine());
             int second = int.Parse(Console.ReadLine());
             int third = int.Parse(Console.ReadLine());
+            
             int time = first + second + third;
             int min = time / 60;
             int sec = time % 60;
+            
             if (sec < 10)
             {
                 Console.WriteLine($"{min}:0{sec}");
