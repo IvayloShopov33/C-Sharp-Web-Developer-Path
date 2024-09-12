f425d207b8eaa91ab5235edfa7ea9abf9372cdba
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-13 02:29:57 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/03. Time + 15 Minutes/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/03. Time + 15 Minutes/Program.cs
@@ -8,14 +8,17 @@ namespace _03._Time___15_Minutes
         {
             int hours = int.Parse(Console.ReadLine());
             int min = int.Parse(Console.ReadLine());
-            min = min + hours * 60;
+            
+            min += hours * 60;
             min += 15;
             hours = min / 60;
-            min = min % 60;
-            if (hours==24)
+            min %= 60;
+            
+            if (hours == 24)
             {
                 hours = 0;
             }
+            
             Console.WriteLine($"{hours}:{min:D2}");
         }
     }
