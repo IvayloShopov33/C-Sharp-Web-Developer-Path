6abd47285a668cbf57025f97bd7ea9ca39ec2c3e
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-12 03:14:04 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/02. Bonus Score/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/02. Bonus Score/Program.cs
@@ -8,11 +8,12 @@ namespace _02._Bonus_Score
         {
             int num = int.Parse(Console.ReadLine());
             double bonus = 0;
+            
             if (num <= 100)
             {
                 bonus = 5;
             }
-            else if(num>1000)
+            else if (num > 1000)
             {
                 bonus = 0.1 * num;
             }
@@ -20,18 +21,18 @@ namespace _02._Bonus_Score
             {
                 bonus = 0.2 * num;
             }
+            
             if (num % 2 == 0)
             {
-                bonus += 1;
-
+                bonus++;
             }
-            else if(num%10==5)
+            else if (num % 10 == 5)
             {
-                bonus = bonus + 2;
+                bonus += 2;
             }
+            
             Console.WriteLine(bonus);
-            Console.WriteLine(num+bonus);
-        }
-        
+            Console.WriteLine(num + bonus);
+        }        
     }
 }
