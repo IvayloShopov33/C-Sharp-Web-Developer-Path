5c43c496be1daed4b82a440e785bd7ad64a70394
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-14 02:16:59 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/04. Toy Shop/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/04. Toy Shop/Program.cs
@@ -12,14 +12,18 @@ namespace _04._Toy_Shop
             int bears = int.Parse(Console.ReadLine());
             int minions = int.Parse(Console.ReadLine());
             int trucks = int.Parse(Console.ReadLine());
+            
             int toysQuantity = puzzles + dolls + bears + minions + trucks;
             double money = puzzles * 2.6 + dolls * 3 + bears * 4.1 + minions * 8.2 + trucks * 2;
-            if(toysQuantity>=50)
+            
+            if(toysQuantity >= 50)
             {
-                money = money - money * 0.25;
+                money -= money * 0.25;
             }
-            money = money - money * 0.1;
-            if (money>=priceTour)
+            
+            money -= money * 0.1;
+            
+            if (money >= priceTour)
             {
                 Console.WriteLine($"Yes! {money-priceTour:f2} lv left.");
             }
@@ -27,7 +31,6 @@ namespace _04._Toy_Shop
             {
                 Console.WriteLine($"Not enough money! {priceTour-money:f2} lv needed.");
             }
-
         }
     }
 }
