312888801a790c70820100cd1497c82e98dd804f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-16 01:34:17 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/06. World Swimming Record/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/06. World Swimming Record/Program.cs
@@ -9,11 +9,13 @@ namespace _06._World_Swimming_Record
             double worldRecord = double.Parse(Console.ReadLine());
             double length = double.Parse(Console.ReadLine());
             double timePerMeter = double.Parse(Console.ReadLine());
+
             double delay = Math.Floor(length / 15) * 12.5;
             double recordByIvan = length * timePerMeter + delay;
-            if (recordByIvan<worldRecord)
+
+            if (recordByIvan < worldRecord)
             {
-                Console.WriteLine($" Yes, he succeeded! The new world record is {recordByIvan:f2} seconds.");
+                Console.WriteLine($"Yes, he succeeded! The new world record is {recordByIvan:f2} seconds.");
             }
             else
             {
