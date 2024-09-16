eb34473c273fa47fb67da940eebf7574591e47b8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-17 00:32:29 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/07. Shopping/Program.cs
+++ Programming Basics with C# - January 2023/02. Conditional Statements/Exercises/07. Shopping/Program.cs
@@ -10,17 +10,21 @@ namespace _07._Shopping
             int gpuQuantity = int.Parse(Console.ReadLine());
             int cpuQuantity = int.Parse(Console.ReadLine());
             int ramQuantity = int.Parse(Console.ReadLine());
+            
             int gpuSum = 250 * gpuQuantity;
             double cpuPrice = gpuSum * 0.35;
             double cpuSum = cpuPrice * cpuQuantity;
+            
             double ramPrice = gpuSum * 0.1;
-            double ramSum = ramPrice * ramQuantity;
+            double ramSum = ramPrice * ramQuantity;            
             double sumAll = gpuSum + cpuSum + ramSum;
+            
             if (gpuQuantity > cpuQuantity)
             {
-                sumAll = sumAll - sumAll * 0.15;
+                sumAll -= sumAll * 0.15;
             }
-            if (budget>=sumAll)
+            
+            if (budget >= sumAll)
             {
                 Console.WriteLine($"You have {budget-sumAll:f2} leva left!");
             }
