42fa96d3f1b5487dc4ffcfafd01b3d568b3e4064
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-27 00:23:17 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Lab/11. Fruit Shop/Program.cs
+++ Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Lab/11. Fruit Shop/Program.cs
@@ -10,111 +10,123 @@ namespace _11._Fruit_Shop
             string day = Console.ReadLine();
             double quantity = double.Parse(Console.ReadLine());
             double price = 0;
-            if (day=="Monday" || day=="Tuesday" || day=="Wednesday" || day=="Thursday" || day=="Friday")
+            
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")
             {
                 if (fruit == "banana")
                 {
                     price = 2.5;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else if (fruit == "apple")
                 {
                     price = 1.2;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else if (fruit == "orange")
                 {
                     price = 0.85;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
-                else if (fruit=="grapefruit")
+                else if (fruit == "grapefruit")
                 {
                     price = 1.45;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
-                else if (fruit=="kiwi")
+                else if (fruit == "kiwi")
                 {
                     price = 2.7;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
-                else if (fruit=="pineapple")
+                else if (fruit == "pineapple")
                 {
                     price = 5.5;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
-                else if (fruit=="grapes")
+                else if (fruit == "grapes")
                 {
                     price = 3.85;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else
                 {
                     Console.WriteLine("error");
-                }
-                
+                }               
             }
-            else if (day=="Saturday" || day=="Sunday")
+            else if (day == "Saturday" || day == "Sunday")
             {
                 if (fruit == "banana")
                 {
                     price = 2.7;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else if (fruit == "apple")
                 {
                     price = 1.25;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else if (fruit == "orange")
                 {
                     price = 0.9;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else if (fruit == "grapefruit")
                 {
                     price = 1.6;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else if (fruit == "kiwi")
                 {
                     price = 3;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else if (fruit == "pineapple")
                 {
                     price = 5.6;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else if (fruit == "grapes")
                 {
                     price = 4.2;
                     double allPrice = quantity * price;
+                    
                     Console.WriteLine($"{allPrice:f2}");
                 }
                 else
                 {
                     Console.WriteLine("error");
-                }
-               
+                }              
             }
             else
             {
                 Console.WriteLine("error");
-            }
-           
+            }           
         }
     }
 }
