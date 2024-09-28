e3b7579a53b1c1d2ec15b80d8e7658e654f18de3
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-09-29 00:48:34 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/01. Cinema/Program.cs
+++ Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/01. Cinema/Program.cs
@@ -10,18 +10,20 @@ namespace _01._Cinema
             int rows = int.Parse(Console.ReadLine());
             int colons = int.Parse(Console.ReadLine());
             double income = rows * colons;
-            if (typeProjection=="Premiere")
+            
+            if (typeProjection == "Premiere")
             {
-                income=income * 12;
+                income *= 12;
             }
-            else if (typeProjection=="Normal")
+            else if (typeProjection == "Normal")
             {
-                income = income * 7.5;
+                income *= 7.5;
             }
-            else if (typeProjection=="Discount")
+            else if (typeProjection == "Discount")
             {
-                income = income * 5;
+                income *= 5;
             }
+            
             Console.WriteLine($"{income:f2} leva");
         }
     }
