356997b179172a97d7e85b443f66d36bcf3d817f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-03 19:52:58 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/05. Journey/Program.cs
+++ Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/05. Journey/Program.cs
@@ -8,33 +8,36 @@ namespace _05._Journey
         {
             double budget = double.Parse(Console.ReadLine());
             string season = Console.ReadLine();
+            
             double price = 0;
-            string typeOfHoliday = "";
-            string destination = "";
-            if (budget<=100)
+            string typeOfHoliday = string.Empty;
+            string destination = string.Empty;
+            
+            if (budget <= 100)
             {
-                 destination = "Bulgaria";
-                if (season=="summer")
+                destination = "Bulgaria";
+                
+                if (season == "summer")
                 {
                     price = budget * 0.3;
                     typeOfHoliday = "Camp";
-
                 }
-                else if (season=="winter")
+                else if (season == "winter")
                 {
                     price = 0.7 * budget;
                     typeOfHoliday = "Hotel";
                 }
             }
-            else if (budget>100 && budget<=1000)
+            else if (budget > 100 && budget <= 1000)
             {
                 destination = "Balkans";
-                if (season=="summer")
+                
+                if (season == "summer")
                 {
                     price = 0.4 * budget;
                     typeOfHoliday = "Camp";
                 }
-                else if (season=="winter")
+                else if (season == "winter")
                 {
                     price = 0.8 * budget;
                     typeOfHoliday = "Hotel";
@@ -46,6 +49,7 @@ namespace _05._Journey
                 price = 0.9 * budget;
                 typeOfHoliday = "Hotel";
             }
+            
             Console.WriteLine($"Somewhere in {destination}");
             Console.WriteLine($"{typeOfHoliday} - {price:f2}");
         }
