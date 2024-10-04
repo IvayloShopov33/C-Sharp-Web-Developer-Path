bee1d01193c5e2f9316fbd70d4f42694174592ff
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-05 00:42:36 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/07. Hotel Room/Program.cs
+++ Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/07. Hotel Room/Program.cs
@@ -11,46 +11,49 @@ namespace _07._Hotel_Room
             double price1 = 0;
             double price2 = 0;
             
-            if (month=="May" || month=="October")
-            {
-                
+            if (month == "May" || month == "October")
+            {           
                 price1 = 50 * nightsNumber;
-                if (nightsNumber>7 && nightsNumber<=14)
+                if (nightsNumber > 7 && nightsNumber <= 14)
                 {
-                    price1 = price1 - price1 * 0.05;
+                    price1 -= price1 * 0.05;
                 }
-                else if (nightsNumber>14)
+                else if (nightsNumber > 14)
                 {
-                    price1 = price1 - price1 * 0.3;
+                    price1 -= price1 * 0.3;
                 }
+                
                 price2 = 65 * nightsNumber;
-                if (nightsNumber>14)
+                if (nightsNumber > 14)
                 {
-                    price2 = price2 - price2 * 0.1;
+                    price2 -= price2 * 0.1;
                 }
             }
-            else if (month=="June" || month=="September")
+            else if (month == "June" || month == "September")
             {
                 price1 = 75.2 * nightsNumber;
-                if (nightsNumber>14)
+                if (nightsNumber > 14)
                 {
-                    price1 = price1 - price1 * 0.2;
+                    price1 -= price1 * 0.2;
                 }
+                
                 price2 = 68.7 * nightsNumber;
                 if (nightsNumber > 14)
                 {
-                    price2 = price2 - price2 * 0.1;
+                    price2 -= price2 * 0.1;
                 }
             }
             else
             {
                 price1 = 76 * nightsNumber;
                 price2 = 77 * nightsNumber;
+                
                 if (nightsNumber > 14)
                 {
-                    price2 = price2 - price2 * 0.1;
+                    price2 -= price2 * 0.1;
                 }
             }
+            
             Console.WriteLine($"Apartment: {price2:f2} lv.");
             Console.WriteLine($"Studio: {price1:f2} lv.");
         }
