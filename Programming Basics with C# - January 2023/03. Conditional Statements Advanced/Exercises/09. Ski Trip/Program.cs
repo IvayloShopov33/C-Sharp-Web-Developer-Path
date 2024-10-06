db92aa634731fa692e58fa2bbf60ac30f6b0a332
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-07 01:32:15 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/09. Ski Trip/Program.cs
+++ Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/09. Ski Trip/Program.cs
@@ -10,68 +10,70 @@ namespace _09._Ski_Trip
             string typeOfRoom = Console.ReadLine();
             string assessment = Console.ReadLine();
             double price = 0;
-            if (typeOfRoom=="room for one person")
+            
+            if (typeOfRoom == "room for one person")
             {
                 price = (days - 1) * 18;
-                if (assessment=="positive")
-                {
-                    price = price + price * 0.25;
-                }
-                else
-                {
-                    price = price - price * 0.1;
-                }
-            }
-            else if (typeOfRoom=="apartment")
-            {
-                price = (days - 1) * 25;
-                if (days<10)
-                {
-
-                    price = price - price * 0.3;
-                }
-                else if (days>=10 && days<=15)
-                {
-                    price = price - price * 0.35;
-                }
-                else
-                {
-                    price = price - price * 0.5;
-                }
                 if (assessment == "positive")
                 {
-                    price = price + price * 0.25;
+                    price += price * 0.25;
+                }
+                else
+                {
+                    price -= price * 0.1;
+                }
+            }
+            else if (typeOfRoom == "apartment")
+            {
+                price = (days - 1) * 25;
+                if (days < 10)
+                {
+                    price -= price * 0.3;
+                }
+                else if (days <= 15)
+                {
+                    price -= price * 0.35;
+                }
+                else
+                {
+                    price -= price * 0.5;
+                }
+                
+                if (assessment == "positive")
+                {
+                    price += price * 0.25;
                 }
                 else 
                 {
-                   price=price-price*0.1;
+                   price -= price * 0.1;
                 }
             }
-            else if (typeOfRoom=="president apartment")
+            else if (typeOfRoom == "president apartment")
             {
                 price = (days - 1) * 35;
                 if (days < 10)
                 {
-
-                    price = price - price * 0.1;
+                    price -= price * 0.1;
                 }
-                else if (days >= 10 && days <= 15)
+                else if (days <= 15)
                 {
-                    price = price - price * 0.15;
+                    price -= price * 0.15;
                 }
                 else
                 {
-                    price = price - price * 0.2;
+                    price -= price * 0.2;
                 }
+                
                 if (assessment == "positive")
                 {
-                    price = price + price * 0.25;
+                    price += price * 0.25;
                 }
                 else
                 {
-                    price = price - price * 0.1;
+                    price -= price * 0.1;
                 }
             }
+            
             Console.WriteLine($"{price:f2}");
         }
     }
