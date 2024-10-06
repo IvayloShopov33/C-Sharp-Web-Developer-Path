fd810da2d08000fff25f462a1099963253ba6124
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-06 15:04:32 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/08. On Time for the Exam/Program.cs
+++ Programming Basics with C# - January 2023/03. Conditional Statements Advanced/Exercises/08. On Time for the Exam/Program.cs
@@ -8,27 +8,29 @@ namespace _08._On_Time_for_the_Exam
         {
             int examHour = int.Parse(Console.ReadLine());
             int examMin = int.Parse(Console.ReadLine());
-            examMin = examMin + examHour * 60;
+            examMin += examHour * 60;
+            
             int arrivalHour = int.Parse(Console.ReadLine());
             int arrivalMin = int.Parse(Console.ReadLine());
-            arrivalMin = arrivalMin + arrivalHour * 60;
+            arrivalMin += arrivalHour * 60;
+            
             int difference = examMin - arrivalMin;
-            if (difference<0)
+            if (difference < 0)
             {
                 Console.WriteLine("Late");
-                if (difference>-60)
+                if (difference > -60)
                 {
                     Console.WriteLine($"{Math.Abs(difference)} minutes after the start");
                 }
                 else
                 {
-                    Console.WriteLine($"{Math.Abs(difference/60)}:{Math.Abs(difference%60):D2} hours after the start" );
+                    Console.WriteLine($"{Math.Abs(difference / 60)}:{Math.Abs(difference % 60):D2} hours after the start" );
                 }
             }
-            else if (difference>=0 && difference<=30)
+            else if (difference >= 0 && difference <= 30)
             {
                 Console.WriteLine("On time");
-                if (difference>0)
+                if (difference > 0)
                 {
                     Console.WriteLine($"{difference} minutes before the start");
                 }
@@ -36,7 +38,7 @@ namespace _08._On_Time_for_the_Exam
             else
             {
                 Console.WriteLine("Early");
-                if (difference<60)
+                if (difference < 60)
                 {
                     Console.WriteLine($"{difference} minutes before the start");
                 }
