bd96127e7dac3aa675250c07bcec0635bc3c44bf
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-08 01:25:38 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/04. For Loop/Lab/01. Numbers from 1 to 100/Program.cs
+++ Programming Basics with C# - January 2023/04. For Loop/Lab/01. Numbers from 1 to 100/Program.cs
@@ -6,10 +6,10 @@ namespace _01._Numbers_from_1_to_100
     {
         static void Main(string[] args)
         {
-          for (int i=1; i<=100; i++)
-            {
-                Console.WriteLine(i);
-            }
+          for (int i = 1; i <= 100; i++)
+          {
+              Console.WriteLine(i);
+          }
         }
     }
 }
