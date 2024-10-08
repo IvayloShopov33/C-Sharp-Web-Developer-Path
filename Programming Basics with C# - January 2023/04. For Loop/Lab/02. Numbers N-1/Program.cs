f3e22c74150425ce255ed49279b6c671096f682b
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-09 00:53:56 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/04. For Loop/Lab/02. Numbers N-1/Program.cs
+++ Programming Basics with C# - January 2023/04. For Loop/Lab/02. Numbers N-1/Program.cs
@@ -7,7 +7,7 @@ namespace _02._Numbers_N_1
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            for (int i=n; i>=1; i--)
+            for (int i = n; i >= 1; i--)
             {
                 Console.WriteLine(i);
             }
