cecbd8936a39c5aabb70625babf26b04932d9fab
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-14 01:01:23 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/04. For Loop/Lab/07. Sum Numbers/Program.cs
+++ Programming Basics with C# - January 2023/04. For Loop/Lab/07. Sum Numbers/Program.cs
@@ -8,11 +8,13 @@ namespace _07._Sum_Numbers
         {
             int n = int.Parse(Console.ReadLine());
             int sum = 0;
-            for (int i=0; i<n;i++)
+            
+            for (int i = 0; i < n; i++)
             {
                 int number = int.Parse(Console.ReadLine());
-                sum = sum + number;
+                sum += number;
             }
+            
             Console.WriteLine(sum);
         }
     }
