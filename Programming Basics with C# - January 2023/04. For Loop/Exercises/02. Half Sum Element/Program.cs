ee65e011279a56917720e3d7b5f31ae02069ed88
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-19 01:02:41 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/04. For Loop/Exercises/02. Half Sum Element/Program.cs
+++ Programming Basics with C# - January 2023/04. For Loop/Exercises/02. Half Sum Element/Program.cs
@@ -9,18 +9,19 @@ namespace _02._Half_Sum_Element
             int n = int.Parse(Console.ReadLine());
             int sum = 0;
             int maxNumber = int.MinValue;
+            
             for (int i = 0; i < n; i++)
             {
                 int number = int.Parse(Console.ReadLine());
                 sum += number;
+                
                 if (number > maxNumber)
                 {
                    maxNumber = number;
                 }
             }
 
-            sum = sum - maxNumber;
-
+            sum -= maxNumber;
             if (sum == maxNumber)
             {
                 Console.WriteLine("Yes");
@@ -29,7 +30,7 @@ namespace _02._Half_Sum_Element
             else
             {
                 Console.WriteLine("No");
-                Console.WriteLine($"Diff = {Math.Abs(sum-maxNumber)}");
+                Console.WriteLine($"Diff = {Math.Abs(sum - maxNumber)}");
             }
         }
     }
