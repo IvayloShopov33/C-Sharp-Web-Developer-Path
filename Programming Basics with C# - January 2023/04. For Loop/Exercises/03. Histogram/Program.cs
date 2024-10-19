3ab08b46a50ab255224f5f39ea4891185414d6aa
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-20 00:53:04 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/04. For Loop/Exercises/03. Histogram/Program.cs
+++ Programming Basics with C# - January 2023/04. For Loop/Exercises/03. Histogram/Program.cs
@@ -13,25 +13,27 @@ namespace _03._Histogram
             int p3 = 0; //400-599
             int p4 = 0; //600-799
             int p5 = 0; //>=800
+            
             for (int i = 1; i <= n; i++)
             {
                 num = int.Parse(Console.ReadLine());
-                if (num<200) //increase the quantity of numbers
+                if (num < 200) //increase the quantity of numbers
                     p1++;              
-                else if (num>=200 && num<400)               
+                else if (num < 400)               
                     p2++;
-                else if (num>=400 && num<600)            
+                else if (num < 600)            
                     p3++;   
-                else if (num>=600 && num<800)         
+                else if (num < 800)         
                     p4++;
                 else   
                     p5++;
             }
-            Console.WriteLine($"{(double)p1/n*100:f2}%");
-            Console.WriteLine($"{(double)p2/ n*100:f2}%");
-            Console.WriteLine($"{(double)p3 /n*100:f2}%");
-            Console.WriteLine($"{(double)p4 /n*100:f2}%");
-            Console.WriteLine($"{(double)p5 /n*100:f2}%");
+            
+            Console.WriteLine($"{(double)p1 / n * 100:f2}%");
+            Console.WriteLine($"{(double)p2 / n * 100:f2}%");
+            Console.WriteLine($"{(double)p3 / n * 100:f2}%");
+            Console.WriteLine($"{(double)p4 / n * 100:f2}%");
+            Console.WriteLine($"{(double)p5 / n * 100:f2}%");
         }
     }
 }
