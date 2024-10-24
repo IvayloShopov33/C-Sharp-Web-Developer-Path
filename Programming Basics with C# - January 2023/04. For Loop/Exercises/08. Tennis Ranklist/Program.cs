52d152532a11c3c6a7e28ca4a43801c80cb9ad40
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-25 01:39:15 +0300

    Update Program.cs

--- Programming Basics with C# - January 2023/04. For Loop/Exercises/08. Tennis Ranklist/Program.cs
+++ Programming Basics with C# - January 2023/04. For Loop/Exercises/08. Tennis Ranklist/Program.cs
@@ -8,6 +8,7 @@ namespace _08._Tennis_Ranklist
         {
             int tournaments = int.Parse(Console.ReadLine());
             int initialPoints = int.Parse(Console.ReadLine());
+            
             string stage;
             int points1 = 0;
             int points2 = 0;
@@ -16,28 +17,32 @@ namespace _08._Tennis_Ranklist
             int p1 = 0;
             int p2 = 0;
             int p3 = 0;
+            
             for (int i = 1; i <= tournaments; i++)
             {
                 stage = Console.ReadLine();
-                if (stage=="F")
+                if (stage == "F")
                 {
                     points1 =  1200;
                     p1++;
                 }
-                else if (stage=="W")
+                else if (stage == "W")
                 {
                     points2 = 2000;
                     p2++;
                 }
-                else if (stage=="SF")
+                else if (stage == "SF")
                 {
                     points3 = 720;
                     p3++;
                 }
-                points = initialPoints + points1*p1 + points2*p2 + points3*p3;
+                
+                points = initialPoints + points1 * p1 + points2 * p2 + points3 * p3;
             }
+            
             double average = (points1 * p1 + points2 * p2 + points3 * p3) / tournaments;
             double percentWin = (double)p2 / tournaments * 100;
+            
             Console.WriteLine($"Final points: {points}");
             Console.WriteLine($"Average points: {Math.Floor(average)}");
             Console.WriteLine($"{percentWin:f2}%");
