ebed6d87fa7896a750e6111f14b14ce8dbffa4f4
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-10-27 19:29:47 +0200

    Update Program.cs

--- Programming Basics with C# - January 2023/05. While Loop/Lab/02. Password/Program.cs
+++ Programming Basics with C# - January 2023/05. While Loop/Lab/02. Password/Program.cs
@@ -9,14 +9,16 @@ namespace _02._Password
             string username = Console.ReadLine();
             string password = Console.ReadLine();
             string newPassword = Console.ReadLine();
+            
             //string newPassword;
-            //while((newPassword=Console.ReadLine())!=password);
-            while (newPassword!=password)
+            //while((newPassword = Console.ReadLine()) != password);
+            
+            while (newPassword != password)
             {
                 newPassword = Console.ReadLine();
             }
+            
             Console.WriteLine($"Welcome {username}!");
-
         }
     }
 }
