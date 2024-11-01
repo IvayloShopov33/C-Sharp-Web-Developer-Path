eeadc0c05bfd3c7c49865feb86821f3dae0639f6
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-11-02 00:58:27 +0200

    Update Program.cs

--- Programming Basics with C# - January 2023/05. While Loop/Lab/08. Graduation/Program.cs
+++ Programming Basics with C# - January 2023/05. While Loop/Lab/08. Graduation/Program.cs
@@ -10,23 +10,27 @@ namespace _08._Graduation
             int grade = 1;
             int classRepetition = 0;
             double markSum = 0;
-            while (grade<=12)
+            
+            while (grade <= 12)
             {
                 double currentGradeMark = double.Parse(Console.ReadLine());
-                if (currentGradeMark<4)
+                if (currentGradeMark < 4)
                 {
                     classRepetition++;
-                    if (classRepetition>1)
+                    if (classRepetition > 1)
                     {
                         break;
                     }
+                    
                     continue;
                 }
+                
                 markSum += currentGradeMark;
                 grade++;
             }
-            //Two ways to come here-natural(end command-13>12) & forced(break)
-            if (classRepetition>1)
+            
+            //Two ways to come here-natural(end command-13 > 12) & forced(break)
+            if (classRepetition > 1)
             {
                 //excluded
                 Console.WriteLine($"{name} has been excluded at {grade} grade");
