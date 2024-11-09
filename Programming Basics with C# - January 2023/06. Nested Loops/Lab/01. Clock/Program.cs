f6785cac2fa91495a4d899e9dbaa827cf03a74c7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-11-10 01:13:46 +0200

    Update Program.cs

--- Programming Basics with C# - January 2023/06. Nested Loops/Lab/01. Clock/Program.cs
+++ Programming Basics with C# - January 2023/06. Nested Loops/Lab/01. Clock/Program.cs
@@ -6,11 +6,11 @@ namespace _01.Clock
     {
         static void Main(string[] args)
         {
-           for (int h=0;h<=23;h++) //върти се 24 пъти
+           for (int h = 0; h <= 23; h++) //върти се 24 пъти
             {
-                for (int m=0;m<=59;m++) //върти се 24*60 пъти
+                for (int m = 0; m <= 59; m++) //върти се 24 * 60 пъти
                 {
-                   for (int s=0; s<=59;s++) //върти се 24*60*60 пъти
+                   for (int s = 0; s <= 59; s++) //върти се 24 * 60 * 60 пъти
                     {
                         Console.WriteLine($"{h}:{m}:{s}");
                     }
