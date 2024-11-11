6c403c734f00f8457ddd848faf1c0d295f1b190a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-11-12 01:45:59 +0200

    Update Program.cs

--- Programming Basics with C# - January 2023/06. Nested Loops/Lab/02. Multiplication Table/Program.cs
+++ Programming Basics with C# - January 2023/06. Nested Loops/Lab/02. Multiplication Table/Program.cs
@@ -6,12 +6,12 @@ namespace _02._Multiplication_Table
     {
         static void Main(string[] args)
         {
-            for (int num = 1; num <=10; num++)
+            for (int num = 1; num <= 10; num++)
             {
                 for (int num1 = 1; num1 <= 10; num1++)
                 {
                     //int a = num * num1;
-                    Console.WriteLine($"{num} * {num1} = {num*num1}");
+                    Console.WriteLine($"{num} * {num1} = {num * num1}");
                 }
             }
         }
