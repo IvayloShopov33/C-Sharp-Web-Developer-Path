b97077af900006bf9f33bb1d0324c9d2e0b2db02
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-11-17 15:33:49 +0200

    Update Program.cs

--- Programming Basics with C# - January 2023/06. Nested Loops/Exercises/01. Number Pyramid/Program.cs
+++ Programming Basics with C# - January 2023/06. Nested Loops/Exercises/01. Number Pyramid/Program.cs
@@ -8,18 +8,22 @@ namespace _01._Number_Pyramid
         {
             int n = int.Parse(Console.ReadLine());
             int current = 1;
+            
             for (int rows = 1; rows <= n; rows++)
             {
                 for (int col = 1; col <= rows; col++)
                 {
                     Console.Write($"{current} ");
                     current++;
-                    if (current>n)
+                    
+                    if (current > n)
                     {
                         break;
                     }
                 }
+                
                 Console.WriteLine();
+                
                 if (current > n)
                 {
                     break;
