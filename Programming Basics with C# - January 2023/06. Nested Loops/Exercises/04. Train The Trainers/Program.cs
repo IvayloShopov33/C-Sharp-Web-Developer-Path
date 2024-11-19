52d46fc3a9b55c0e3c4028595edb1c94ab54674a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-11-20 00:52:25 +0200

    Update Program.cs

--- Programming Basics with C# - January 2023/06. Nested Loops/Exercises/04. Train The Trainers/Program.cs
+++ Programming Basics with C# - January 2023/06. Nested Loops/Exercises/04. Train The Trainers/Program.cs
@@ -8,25 +8,32 @@ namespace _04._Train_The_Trainers
         {
             int judgesMembers = int.Parse(Console.ReadLine());
             string input = Console.ReadLine();
+            
             string presentationName;
-            int presentationCount=0;
-            double grade=0;
-            double averageGrade=0;
-            while (input!="Finish")
+            int presentationCount = 0;
+            double grade = 0;
+            double averageGrade = 0;
+            
+            while (input != "Finish")
             {
                 presentationName = input;
                 grade = 0;
+                
                 for (int i = 1; i <= judgesMembers; i++)
                 {
                     grade += double.Parse(Console.ReadLine());
                 }
+                
                 grade /= judgesMembers;
                 Console.WriteLine($"{presentationName} - {grade:f2}.");
+                
                 averageGrade += grade;
                 presentationCount++;
+                
                 input = Console.ReadLine();
             }
-            Console.WriteLine($"Student's final assessment is {averageGrade/presentationCount:f2}.");
+            
+            Console.WriteLine($"Student's final assessment is {averageGrade / presentationCount:f2}.");
         }
     }
 }
