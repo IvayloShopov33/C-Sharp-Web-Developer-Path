04191ef2e39a05c39fb0a522ee2d6c2cdbb457b1
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-11-21 01:19:01 +0200

    Update Program.cs

--- Programming Basics with C# - January 2023/06. Nested Loops/Exercises/05. Special Numbers/Program.cs
+++ Programming Basics with C# - January 2023/06. Nested Loops/Exercises/05. Special Numbers/Program.cs
@@ -9,16 +9,16 @@ namespace _05._Special_Numbers
             int n = int.Parse(Console.ReadLine());
             for (int i = 1; i <= 9; i++)
             {
-                for (int k = 1; k <=9; k++)
+                for (int k = 1; k <= 9; k++)
                 {
-                    for (int l = 1; l  <= 9; l++)
+                    for (int l = 1; l <= 9; l++)
                     {
-                        for (int m = 1; m <=9; m++)
+                        for (int m = 1; m <= 9; m++)
                         {
-                            if (n%i==0 &&
-                                n%k==0 &&
-                                n%l==0 &&
-                                n%m==0)
+                            if (n % i == 0 &&
+                                n % k == 0 &&
+                                n % l == 0 &&
+                                n % m == 0)
                                 Console.Write($"{i}{k}{l}{m} ");
                         }
                     }
