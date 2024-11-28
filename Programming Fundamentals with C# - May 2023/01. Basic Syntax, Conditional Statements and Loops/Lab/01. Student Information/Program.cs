c3b80940fc37ba0b92180eacc0f7d6992ae9a085
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-11-28 02:43:42 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/01. Student Information/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/01. Student Information/Program.cs
@@ -9,6 +9,7 @@ namespace Student_Information
             string studentName = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
             double grade = double.Parse(Console.ReadLine());
+            
             Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {grade:f2}");
         }
     }
