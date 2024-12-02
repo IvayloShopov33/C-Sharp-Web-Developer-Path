6f778be5b74d26d633157f00a7bd19371a63d0b8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-02 03:13:59 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/07. Theatre Promotion/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/07. Theatre Promotion/Program.cs
@@ -8,36 +8,37 @@ namespace _07._Theatre_Promotion
         {
             string typeOfDay = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
-            if (typeOfDay=="Weekday")
+            
+            if (typeOfDay == "Weekday")
             {
-                if (age>=0 && age<=18)
+                if (age >= 0 && age <= 18)
                 {
                     Console.WriteLine("12$");
                 }
-                else if (age>=19 && age<=64)
+                else if (age <= 64)
                 {
                     Console.WriteLine("18$");
                 }
-                else if (age>=65&& age<=122)
+                else if (age <= 122)
                 {
                     Console.WriteLine("12$");
                 }
-                else if(age<0 || age>122)
+                else if (age < 0 || age > 122)
                 {
                     Console.WriteLine("Error!");
                 }
             }
-            else if (typeOfDay=="Weekend")
+            else if (typeOfDay == "Weekend")
             {
                 if (age >= 0 && age <= 18)
                 {
                     Console.WriteLine("15$");
                 }
-                else if (age >= 19 && age <= 64)
+                else if (age <= 64)
                 {
                     Console.WriteLine("20$");
                 }
-                else if (age >= 65 && age <= 122)
+                else if (age <= 122)
                 {
                     Console.WriteLine("15$");
                 }
@@ -52,11 +53,11 @@ namespace _07._Theatre_Promotion
                 {
                     Console.WriteLine("5$");
                 }
-                else if (age >= 19 && age <= 64)
+                else if (age <= 64)
                 {
                     Console.WriteLine("12$");
                 }
-                else if (age >= 65 && age <= 122)
+                else if (age <= 122)
                 {
                     Console.WriteLine("10$");
                 }
@@ -65,7 +66,6 @@ namespace _07._Theatre_Promotion
                     Console.WriteLine("Error!");
                 }
             }
-
         }
     }
 }
