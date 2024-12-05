62a921747ac44a121588439179c0ba063b70a77f
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-05 02:39:43 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/11. Multiplication Table 2.0/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/11. Multiplication Table 2.0/Program.cs
@@ -8,7 +8,8 @@ namespace _11._Multiplication_Table_2._0
         {
             int number = int.Parse(Console.ReadLine());
             int multiplier = int.Parse(Console.ReadLine());
-            if (multiplier<10 && multiplier>0)
+            
+            if (multiplier < 10 && multiplier > 0)
             {
                 for (int i = multiplier; i < 11; i++)
                 {
