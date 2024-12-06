171c74481232514dcd0c189e92354c409feaa619
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-06 02:18:37 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/12. Even Number/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/12. Even Number/Program.cs
@@ -7,11 +7,12 @@ namespace _12._Even_Number
         static void Main(string[] args)
         {
             int input = int.Parse(Console.ReadLine());
-            while (input%2!=0)
+            while (input % 2 != 0)
             {
                 Console.WriteLine("Please write an even number.");
                 input = int.Parse(Console.ReadLine());
             }
+            
             Console.WriteLine($"The number is: {Math.Abs(input)}");
         }
     }
