5a58990cb057f17680bea147e850712dea8d742c
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-07 02:37:23 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/13. Holiday between two dates/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Lab/13. Holiday between two dates/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+
 class HolidaysBetweenTwoDates 
 { 
     static void Main() 
@@ -7,11 +8,13 @@ class HolidaysBetweenTwoDates
         var startDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
         var endDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
         var holidaysCount = 0;
-        for (var date = startDate; date <= endDate; date=date.AddDays(1))
+        
+        for (var date = startDate; date <= endDate; date = date.AddDays(1))
         {
             if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
                 holidaysCount++;
         }
+        
         Console.WriteLine(holidaysCount); 
     } 
 }
