7ba7a1be374821bb90f18e2f2311544f49ead261
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-08 03:27:21 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/01. Ages/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/01. Ages/Program.cs
@@ -7,23 +7,23 @@ namespace _01._Ages
         static void Main(string[] args)
         {
             int age = int.Parse(Console.ReadLine());
-            if (age==0 || age==1 || age==2)
+            if (age == 0 || age == 1 || age == 2)
             {
                 Console.WriteLine("baby");
             }
-            else if (age>=3 && age<=13)
+            else if (age >= 3 && age <= 13)
             {
                 Console.WriteLine("child");
             }
-            else if (age>=14 && age<=19)
+            else if (age >= 14 && age <= 19)
             {
                 Console.WriteLine("teenager");
             }
-            else if (age>=20 && age<=65)
+            else if (age >= 20 && age <= 65)
             {
                 Console.WriteLine("adult");
             }
-            else if (age>65)
+            else if (age > 65)
             {
                 Console.WriteLine("elder");
             }
