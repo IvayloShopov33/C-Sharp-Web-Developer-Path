b64216ad2a0b97a7284f360d65d4b97d046a2cf7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-13 01:35:49 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/06. Strong number/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/06. Strong number/Program.cs
@@ -9,18 +9,22 @@ namespace _06._Strong_number
             int number = int.Parse(Console.ReadLine());
             int numberCopy = number;
             int sum = 0;
-            while (numberCopy>0)
+            
+            while (numberCopy > 0)
             {
                 int digit = numberCopy % 10;
                 numberCopy = numberCopy / 10;
                 int factoriel = 1;
+                
                 for (int i = 1; i <= digit; i++)
                 {
                     factoriel *= i;
                 }
+                
                 sum += factoriel;
             }
-            if (sum==number)
+            
+            if (sum == number)
             {
                 Console.WriteLine("yes");
             }
