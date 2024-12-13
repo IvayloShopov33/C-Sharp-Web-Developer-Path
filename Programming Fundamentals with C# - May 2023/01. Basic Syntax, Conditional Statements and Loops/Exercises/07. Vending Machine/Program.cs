907eef1cb62c6b5e49e424e5db114d6eec32735a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-14 01:51:56 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/07. Vending Machine/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/07. Vending Machine/Program.cs
@@ -9,10 +9,11 @@ namespace _7._Vending_Machine
             string coins = Console.ReadLine();
             double sumOfMoney = 0;
             bool flag = false;
-            while (coins!="Start")
+            
+            while (coins != "Start")
             {
-               double money = double.Parse(coins);
-                if (money==0.1 || money==0.2 || money==0.5 || money==1 || money==2)
+                double money = double.Parse(coins);
+                if (money == 0.1 || money == 0.2 || money == 0.5 || money == 1 || money == 2)
                 {
                     sumOfMoney += money;
                 }
@@ -20,29 +21,32 @@ namespace _7._Vending_Machine
                 {
                     Console.WriteLine($"Cannot accept {money}");
                 }
+                
                 coins = Console.ReadLine();
             }
+            
             string product = Console.ReadLine();
             double price = 0;
-            while (product!="End")
+            
+            while (product != "End")
             {
-                if (product=="Nuts")
+                if (product == "Nuts")
                 {
                     price = 2.0;
                 }
-                else if (product=="Water")
+                else if (product == "Water")
                 {
                     price = 0.7;
                 }
-                else if (product=="Crisps")
+                else if (product == "Crisps")
                 {
                     price = 1.5;
                 }
-                else if (product=="Soda")
+                else if (product == "Soda")
                 {
                     price = 0.8;
                 }
-                else if (product=="Coke")
+                else if (product == "Coke")
                 {
                     price = 1.0;
                 }
@@ -52,18 +56,19 @@ namespace _7._Vending_Machine
                     Console.WriteLine("Invalid product");
                 }
 
-                if (sumOfMoney>=price && !flag)
+                if (sumOfMoney >= price && !flag)
                 {
                     sumOfMoney -= price;
                     Console.WriteLine($"Purchased {product.ToLower()}");
                 }
-                else if (sumOfMoney<0 && !flag)
+                else if (sumOfMoney < 0 && !flag)
                 {
                     Console.WriteLine("Sorry, not enough money");
                 }
 
                 product = Console.ReadLine();
             }
+            
             Console.WriteLine($"Change: {sumOfMoney:f2}");
         }
     }
