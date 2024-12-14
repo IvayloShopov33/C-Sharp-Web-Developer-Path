3e5d1f5ec9f862e2c77e6b204e0e00ab937130d9
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-15 00:57:16 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/08. Triangle of Numbers/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/08. Triangle of Numbers/Program.cs
@@ -7,11 +7,12 @@ namespace _8._Triangle_of_Numbers
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            
             for (int i = 1; i <= n; i++)
             {
                 for (int j = 1; j <= i; j++)
                 {
-                    if (j==i)
+                    if (j == i)
                     {
                         Console.Write(i);
                         Console.WriteLine();
