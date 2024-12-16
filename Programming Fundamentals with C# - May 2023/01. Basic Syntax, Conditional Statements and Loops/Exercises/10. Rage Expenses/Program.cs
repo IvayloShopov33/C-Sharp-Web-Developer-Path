e93e981ca53ac2965c1a180a50e0536913c472ee
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-17 01:16:59 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/10. Rage Expenses/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/10. Rage Expenses/Program.cs
@@ -8,13 +8,15 @@ namespace _10._Rage_Expenses
         {
             int lostGamesCount = int.Parse(Console.ReadLine());
             float headsetPrice = float.Parse(Console.ReadLine());
-            float mousePrice= float.Parse(Console.ReadLine());
-            float keyboardPrice= float.Parse(Console.ReadLine());
-            float displayPrice= float.Parse(Console.ReadLine());
-            int headsetCount = lostGamesCount/2;
-            int mouseCount = lostGamesCount/3;
-            int keyboardCount = lostGamesCount/6;
-            int displayCount = keyboardCount/2;
+            float mousePrice = float.Parse(Console.ReadLine());
+            float keyboardPrice = float.Parse(Console.ReadLine());
+            float displayPrice = float.Parse(Console.ReadLine());
+            
+            int headsetCount = lostGamesCount / 2;
+            int mouseCount = lostGamesCount / 3;
+            int keyboardCount = lostGamesCount / 6;
+            int displayCount = keyboardCount / 2;
+            
             double expenses = headsetPrice * headsetCount + mousePrice * mouseCount + keyboardPrice * keyboardCount + displayPrice * displayCount;
             Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
         }
