5d7a1ad7d240adc6637561dea44ae030f43ca05a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-18 01:30:22 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/11. Orders/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/Exercises/11. Orders/Program.cs
@@ -8,15 +8,19 @@ namespace _11._Orders
         {
             int orders = int.Parse(Console.ReadLine());
             decimal sum = 0;
+            
             for (int i = 0; i < orders; i++)
             {
                 decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
                 int days = int.Parse(Console.ReadLine());
                 int capsulesCount = int.Parse(Console.ReadLine());
+                
                 decimal ordersPrice = (days * capsulesCount) * pricePerCapsule;
                 Console.WriteLine($"The price for the coffee is: ${ordersPrice:F2}");
+                
                 sum += ordersPrice;
             }
+            
             Console.WriteLine($"Total: ${sum:F2}");
         }
     }
