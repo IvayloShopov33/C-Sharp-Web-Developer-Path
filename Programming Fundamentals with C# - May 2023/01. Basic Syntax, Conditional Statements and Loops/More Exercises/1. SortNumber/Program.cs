a37ebcf73be57131cd40083c0c5906166fd1c75b
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-19 17:44:48 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/More Exercises/1. SortNumber/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/More Exercises/1. SortNumber/Program.cs
@@ -7,11 +7,13 @@ namespace SortNumber__exception_
         static void Main(string[] args)
         {
             double num1 = double.Parse(Console.ReadLine());
-            double num2= double.Parse(Console.ReadLine());
-            double num3= double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double num3 = double.Parse(Console.ReadLine());
+            
             double[] Arr = { num1, num2, num3 };
             Array.Sort(Arr);
             Array.Reverse(Arr);
+            
             for (int i = 0; i < Arr.Length; i++)
             {
                 Console.WriteLine(Arr[i]);
