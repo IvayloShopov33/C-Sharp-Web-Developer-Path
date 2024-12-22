aea4b77f998c2213b7d1a044eaa6893c42c6cd22
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-22 18:23:48 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/More Exercises/4. ReverseString/Program.cs
+++ Programming Fundamentals with C# - May 2023/01. Basic Syntax, Conditional Statements and Loops/More Exercises/4. ReverseString/Program.cs
@@ -8,15 +8,15 @@ namespace _4.ReverseString_except
         static void Main(string[] args)
         {
             string str = Console.ReadLine();
-            //string strReversed=string.Empty;
-            //for (int i = str.Length-1; i >= 0; i--)
+            //string strReversed = string.Empty;
+            //for (int i = str.Length - 1; i >= 0; i--)
             //{
             //    strReversed += str[i];
             //}
             //Console.WriteLine(strReversed);
-            string str1= new string (str.Reverse().ToArray());
+            
+            string str1 = new string (str.Reverse().ToArray());
             Console.WriteLine(str1);
-
         }
     }
 }
