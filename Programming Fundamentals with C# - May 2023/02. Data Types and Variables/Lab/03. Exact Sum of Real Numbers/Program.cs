7087859b038160868e894bbb6f8bbe59475f2bbc
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-26 00:04:20 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/03. Exact Sum of Real Numbers/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -8,11 +8,13 @@ namespace _3._Exact_Sum_of_Real_Numbers
         {
             int numbersCount = int.Parse(Console.ReadLine());
             decimal sum = 0;
+
             for (int i = 0; i < numbersCount; i++)
             {
                 decimal number = decimal.Parse(Console.ReadLine());
                 sum += number;
             }
+
             Console.WriteLine(sum);
         }
     }
