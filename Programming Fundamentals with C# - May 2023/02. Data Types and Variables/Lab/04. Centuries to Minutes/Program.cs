3615d2c97783f71b5df08d5df48e86fb69c0e6a3
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-26 17:45:42 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/04. Centuries to Minutes/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/04. Centuries to Minutes/Program.cs
@@ -7,10 +7,12 @@ namespace _4._Centuries_to_Minutes
         static void Main(string[] args)
         {
             int centuries = int.Parse(Console.ReadLine());
-            long years =(long)centuries*100;
-            long days= (long)((long) years * 365.2422);
+            
+            long years = (long)centuries * 100;
+            long days = (long)((long) years * 365.2422);
             long hours = days * 24;
-            long minutes =hours*60;
+            long minutes = hours * 60;
+            
             Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
         }
     }
