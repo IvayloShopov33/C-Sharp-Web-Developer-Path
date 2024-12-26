5cdd0c2de2288b68023785d98be92e5c8766dda7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-27 00:05:48 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/05. Special Numbers/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/05. Special Numbers/Program.cs
@@ -7,18 +7,21 @@ namespace _05._Special_Numbers
         static void Main(string[] args)
         {
             int numbersCount = int.Parse(Console.ReadLine());
+            
             for (int i = 1; i <= numbersCount; i++)
             {
                 int number = i;
                 int numberCopy = number;
                 int sumOfDigits = 0;
-                while (number>0)
+                
+                while (number > 0)
                 {
                     int digit = number % 10;
                     sumOfDigits += digit;
                     number /= 10;
                 }
-                if (sumOfDigits==5 || sumOfDigits==7 || sumOfDigits==11) 
+                
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11) 
                 {
                     Console.WriteLine($"{numberCopy} -> True");
                 }
