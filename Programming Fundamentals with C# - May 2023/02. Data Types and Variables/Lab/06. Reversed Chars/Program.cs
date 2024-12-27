de2390c61d505b6a0114d0243891d63d53920f51
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-28 00:24:08 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/06. Reversed Chars/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/06. Reversed Chars/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+
 namespace _6._Reversed_Chars
 {
     class Program
@@ -7,8 +8,9 @@ namespace _6._Reversed_Chars
         static void Main(string[] args)
         {
             char letter1 = char.Parse(Console.ReadLine());
-           char letter2= char.Parse(Console.ReadLine());
-            char letter3= char.Parse(Console.ReadLine());
+            char letter2 = char.Parse(Console.ReadLine());
+            char letter3 = char.Parse(Console.ReadLine());
+            
             char[] reversedLetters = { letter3, letter2, letter1 };
             Console.WriteLine(string.Join(' ', reversedLetters));
         }
