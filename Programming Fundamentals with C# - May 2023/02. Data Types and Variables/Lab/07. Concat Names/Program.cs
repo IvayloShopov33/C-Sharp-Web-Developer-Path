8a6b9fe9ce225a7001e68c336cfd452e93c7f871
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-29 00:39:38 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/07. Concat Names/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/07. Concat Names/Program.cs
@@ -9,6 +9,7 @@ namespace _7._Concat_Names
             string name1 = Console.ReadLine();
             string name2 = Console.ReadLine();
             string delimeter = Console.ReadLine();
+            
             Console.WriteLine($"{name1}{delimeter}{name2}");
         }
     }
