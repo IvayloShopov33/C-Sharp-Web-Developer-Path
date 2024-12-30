6597bb273c72f239e108a5f58e5ee3ff7f1f3bd3
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2024-12-30 15:26:09 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/08. Town Info/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/08. Town Info/Program.cs
@@ -8,7 +8,8 @@ namespace _8._Town_Info
         {
             string nameOfTown = Console.ReadLine();
             long population = long.Parse(Console.ReadLine());
-            long area=long.Parse(Console.ReadLine());
+            long area = long.Parse(Console.ReadLine());
+            
             Console.WriteLine($"Town {nameOfTown} has population of {population} and area {area} square km.");
         }
     }
