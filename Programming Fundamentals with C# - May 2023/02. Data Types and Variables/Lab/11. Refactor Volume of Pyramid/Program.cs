d1640944bc846f7db358f812cc337ce0ef9d181a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-02 04:50:02 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/11. Refactor Volume of Pyramid/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Lab/11. Refactor Volume of Pyramid/Program.cs
@@ -9,10 +9,12 @@ namespace _11._Refactor_Volume_of_Pyramid
             double length = double.Parse(Console.ReadLine());
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
+            
             Console.Write("Length: ");
             Console.Write("Width: ");
             Console.Write("Height: ");
-             double volume = (length * width * height) / 3;
+            
+            double volume = (length * width * height) / 3;
             Console.Write($"Pyramid Volume: {volume:f2}");
         }
     }
