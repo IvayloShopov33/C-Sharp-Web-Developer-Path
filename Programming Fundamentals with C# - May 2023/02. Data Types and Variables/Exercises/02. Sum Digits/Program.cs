502d238932cae2c5a2d442d283437ee147b1d0e4
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-04 15:28:57 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Exercises/02. Sum Digits/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Exercises/02. Sum Digits/Program.cs
@@ -8,12 +8,15 @@ namespace _02._Sum_Digits
         {
             int number = int.Parse(Console.ReadLine());
             int sum = 0;
-            while (number!=0)
+            
+            while (number != 0)
             {
                 int lastDigit = number % 10;
                 sum += lastDigit;
+                
                 number /= 10;
             }
+            
             Console.WriteLine(sum);
         }
     }
