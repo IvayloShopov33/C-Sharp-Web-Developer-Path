26029702390032b458622b9d136d448e66cd81d3
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-06 01:02:04 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Exercises/04. Sum of Chars/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Exercises/04. Sum of Chars/Program.cs
@@ -8,11 +8,13 @@ namespace _04._Sum_of_Chars
         {
             int lettersCount = int.Parse(Console.ReadLine());
             int sum = 0;
+            
             for (int i = 0; i < lettersCount; i++)
             {
                 char letter = char.Parse(Console.ReadLine());
                 sum += letter;
             }
+            
             Console.WriteLine($"The sum equals: {sum}");
         }
     }
