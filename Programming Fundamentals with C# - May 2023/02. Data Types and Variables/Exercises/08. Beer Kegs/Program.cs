c85f6e946fcd5c96908444b8277ceb04b57b8bf6
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-09 18:47:43 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Exercises/08. Beer Kegs/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Exercises/08. Beer Kegs/Program.cs
@@ -9,18 +9,21 @@ namespace _08._Beer_Kegs
             int beerKegsCount = int.Parse(Console.ReadLine());
             double biggestVolume = double.MinValue;
             string biggestModel = string.Empty;
+
             for (int i = 0; i < beerKegsCount; i++)
             {
                 string modelOfKeg = Console.ReadLine();
                 float radius = float.Parse(Console.ReadLine());
                 int height = int.Parse(Console.ReadLine());
                 double volume = Math.PI * Math.Pow(radius, 2) * height;
-                if (biggestVolume<volume)
+
+                if (biggestVolume < volume)
                 {
                     biggestVolume = volume;
                     biggestModel = modelOfKeg;
                 }
             }
+
             Console.WriteLine(biggestModel);
         }
     }
