406970bcc73d10bde1363e14984a3598acc2e57e
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-11 15:22:03 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Exercises/10. Poke Mon/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/Exercises/10. Poke Mon/Program.cs
@@ -6,20 +6,25 @@ namespace _10._Poke_Mon
     {
         static void Main(string[] args)
         {
-            int pokePower = int.Parse(Console.ReadLine()); //N
-            int distanceBetweenTargets = int.Parse(Console.ReadLine()); //M
-            int exhaustionFactor = int.Parse(Console.ReadLine()); //Y
+            int pokePower = int.Parse(Console.ReadLine()); // N
+            int distanceBetweenTargets = int.Parse(Console.ReadLine()); // M
+            int exhaustionFactor = int.Parse(Console.ReadLine()); // Y
+            
             int pokedTargets = 0;
             int pokePowerCopy = pokePower;
-            while (pokePower>=distanceBetweenTargets)
+            
+            while (pokePower >= distanceBetweenTargets)
             {
                 pokePower -= distanceBetweenTargets;
-                if (pokePower==pokePowerCopy/2 && exhaustionFactor!=0)
+                
+                if (pokePower == pokePowerCopy / 2 && exhaustionFactor != 0)
                 {
                     pokePower /= exhaustionFactor;
                 }
+                
                 pokedTargets++;
             }
+            
             Console.WriteLine(pokePower);
             Console.WriteLine(pokedTargets);
         }
