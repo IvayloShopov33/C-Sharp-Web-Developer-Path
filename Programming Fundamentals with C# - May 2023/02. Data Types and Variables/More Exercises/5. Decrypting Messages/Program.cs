881d8de2df484e3e9baf8960273b2d31d0e3fbc5
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-16 00:21:02 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/02. Data Types and Variables/More Exercises/5. Decrypting Messages/Program.cs
+++ Programming Fundamentals with C# - May 2023/02. Data Types and Variables/More Exercises/5. Decrypting Messages/Program.cs
@@ -9,12 +9,15 @@ namespace _5._Decrypting_Messages
             int key = int.Parse(Console.ReadLine());
             int lettersCount = int.Parse(Console.ReadLine());
             string message = string.Empty;
+            
             for (int i = 0; i < lettersCount; i++)
             {
                 char letter = char.Parse(Console.ReadLine());
                 letter = (char)(letter + key);
+                
                 message += letter;
             }
+            
             Console.WriteLine(message);
         }
     }
