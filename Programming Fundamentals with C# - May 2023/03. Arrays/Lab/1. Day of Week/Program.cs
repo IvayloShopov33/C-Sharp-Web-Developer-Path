86ff28572afcb84f8b1c9fb3edf7a4b4f46b8a71
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-19 01:22:36 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/Lab/1. Day of Week/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/Lab/1. Day of Week/Program.cs
@@ -8,9 +8,10 @@ namespace _1._Day_of_Week
         {
             string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
             int dayOfWeek = int.Parse(Console.ReadLine());
-            if (dayOfWeek>=1 && dayOfWeek<=7)
+            
+            if (dayOfWeek >= 1 && dayOfWeek <= 7)
             {
-                Console.WriteLine(days[dayOfWeek-1]);
+                Console.WriteLine(days[dayOfWeek - 1]);
             }
             else
             {
