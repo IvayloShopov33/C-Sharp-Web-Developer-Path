3591140b83c32123621f0582e5e40be78cca80ed
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-20 01:45:47 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/Lab/2. Print Numbers in Reverse Order/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/Lab/2. Print Numbers in Reverse Order/Program.cs
@@ -8,6 +8,7 @@ namespace _2._Print_Numbers_in_Reverse_Order
         {
             int numbersCount = int.Parse(Console.ReadLine());
             int[] Arr = new int[numbersCount];
+            
             for (int i = 0; i < numbersCount; i++)
             {
                 int number = int.Parse(Console.ReadLine());
