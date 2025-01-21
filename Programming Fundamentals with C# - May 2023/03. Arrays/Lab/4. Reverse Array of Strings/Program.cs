808b4b966216f20307a88ed9796897a4e3a36822
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-22 00:10:51 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/Lab/4. Reverse Array of Strings/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/Lab/4. Reverse Array of Strings/Program.cs
@@ -7,7 +7,7 @@ namespace _4._Reverse_Array_of_Strings
         static void Main(string[] args)
         {
             string[] array = Console.ReadLine().Split(' ');
-            for (int i = array.Length-1; i >= 0; i--)
+            for (int i = array.Length - 1; i >= 0; i--)
             {
                 Console.Write($"{array[i]} ");
             }
