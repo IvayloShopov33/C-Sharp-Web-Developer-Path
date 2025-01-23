096aca4510ceaeb99684732556f5272194b0c9d7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-24 01:57:12 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/Lab/7. Equal Arrays/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/Lab/7. Equal Arrays/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+
 namespace _7._Equal_Arrays
 {
     class Program
@@ -9,15 +10,18 @@ namespace _7._Equal_Arrays
             int[] firstArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int[] secondArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             bool isEqual = true;
+            
             for (int i = 0; i < firstArray.Length; i++)
             {
-                if (firstArray[i]!=secondArray[i])
+                if (firstArray[i] != secondArray[i])
                 {
                     isEqual = false;
                     Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    
                     break;
                 }
             }
+            
             if (isEqual)
             {
                 Console.WriteLine($"Arrays are identical. Sum: {firstArray.Sum()}");
