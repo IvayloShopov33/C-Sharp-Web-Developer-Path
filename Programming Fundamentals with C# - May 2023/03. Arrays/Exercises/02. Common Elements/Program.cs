031fd22fc8414cd10c80bd6b72f7e6d50839c7f1
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-27 01:34:11 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/Exercises/02. Common Elements/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/Exercises/02. Common Elements/Program.cs
@@ -8,12 +8,12 @@ namespace _2._Common_Elements
         {
             string[] firstLine = Console.ReadLine().Split(' ');
             string[] secondLine = Console.ReadLine().Split(' ');
+            
             for (int i = 0; i < secondLine.Length; i++)
             {
-
                 for (int j = 0; j < firstLine.Length; j++)
                 {
-                    if (secondLine[i]==firstLine[j])
+                    if (secondLine[i] == firstLine[j])
                     {
                         Console.Write($"{firstLine[j]} ");
                         break;
