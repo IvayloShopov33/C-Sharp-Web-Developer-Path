42356122d6af4e11c5b09d14caf40fb9078c522a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-28 01:38:44 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/Exercises/04. Array Rotation/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/Exercises/04. Array Rotation/Program.cs
@@ -9,15 +9,18 @@ namespace _4._Array_Rotation
         {
             int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int rotations = int.Parse(Console.ReadLine());
+            
             for (int i = 0; i < rotations; i++)
             {
                 int firstToLast = numbers[0];
-                for (int j = 0; j < numbers.Length-1; j++)
+                for (int j = 0; j < numbers.Length - 1; j++)
                 {
                     numbers[j] = numbers[j + 1];
                 }
+                
                 numbers[numbers.Length - 1] = firstToLast;
             }
+            
             Console.WriteLine(String.Join(' ', numbers));
         }
     }
