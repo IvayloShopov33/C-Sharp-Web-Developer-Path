670ff324bccef5571c4bb829b79248f17a2a8312
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-01-30 01:01:58 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/Exercises/06. Equal Sum/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/Exercises/06. Equal Sum/Program.cs
@@ -9,26 +9,32 @@ namespace _06._Equal_Sum
         {
             int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             bool areEqual = false;
+            
             for (int i = 0; i < numbers.Length; i++)
             {
                 int number = numbers[i];
-                    int sumBeforeDigit = 0;
+                int sumBeforeDigit = 0;
                 int sumAfterDigit = 0;
+                
                 for (int j = 0; j < i; j++)
                 {
                     sumBeforeDigit += numbers[j];
                 }
-                for (int k = i+1; k < numbers.Length; k++)
+                
+                for (int k = i + 1; k < numbers.Length; k++)
                 {
                     sumAfterDigit += numbers[k];
                 }
-                if (sumBeforeDigit==sumAfterDigit)
+                
+                if (sumBeforeDigit == sumAfterDigit)
                 {
                     areEqual = true;
                     Console.WriteLine(i); //the index of the element which satisfies the condition
+                    
                     break;
                 }
             }
+            
             if (!areEqual)
             {
                 Console.WriteLine("no");
