b0d9d3b04f5b97af9fc1f85b76386981ec9d8302
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-01 19:45:25 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/Exercises/08. Magic Sum/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/Exercises/08. Magic Sum/Program.cs
@@ -9,11 +9,12 @@ namespace _8._Magic_Sum
         {
             int[] arrayOfNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int number = int.Parse(Console.ReadLine());
+            
             for (int i = 0; i < arrayOfNumbers.Length; i++)
             {
-                for (int j = i+1; j < arrayOfNumbers.Length; j++)
+                for (int j = i + 1; j < arrayOfNumbers.Length; j++)
                 {
-                    if (arrayOfNumbers[i]+arrayOfNumbers[j]==number)
+                    if (arrayOfNumbers[i] + arrayOfNumbers[j] == number)
                     {
                         Console.Write($"{arrayOfNumbers[i]} {arrayOfNumbers[j]}");
                         Console.WriteLine();
