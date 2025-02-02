4990ca71e2811573957899e132d69ef8a5b30932
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-03 01:40:16 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/03. Arrays/More Exercises/2. Pascal Triangle/Program.cs
+++ Programming Fundamentals with C# - May 2023/03. Arrays/More Exercises/2. Pascal Triangle/Program.cs
@@ -5,36 +5,32 @@ namespace _2._Pascal_Triangle
     class Program
     {
         static void Main(string[] args)
-        {
-           
-                int n = int.Parse(Console.ReadLine());
+        {          
+            int n = int.Parse(Console.ReadLine());
+            long[][] pascalTriangle = new long[n][];
 
-                long[][] pascalTriangle = new long[n][];
+            for (int i = 0; i < n; i++)
+            {
+                pascalTriangle[i] = new long[i + 1];
+                pascalTriangle[i][0] = 1;
 
-                for (int i = 0; i < n; i++)
+                for (int j = 1; j < i; j++)
                 {
-                    pascalTriangle[i] = new long[i + 1];
-                    pascalTriangle[i][0] = 1;
-
-                    for (int j = 1; j < i; j++)
-                    {
-                        pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
-                    }
-
-                    pascalTriangle[i][i] = 1;
+                    pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
                 }
 
-                for (int i = 0; i < n; i++)
+                pascalTriangle[i][i] = 1;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
                 {
-                    for (int j = 0; j <= i; j++)
-                    {
-                        Console.Write(pascalTriangle[i][j] + " ");
-                    }
-                    Console.WriteLine();
+                    Console.Write(pascalTriangle[i][j] + " ");
                 }
+                
+                Console.WriteLine();
             }
         }
-
     }
-
-
+}
