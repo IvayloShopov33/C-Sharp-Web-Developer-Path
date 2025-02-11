327470df6b605d7bfca130bcfa54cd7b966c07e3
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-11 02:00:44 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Lab/08. Math Power/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Lab/08. Math Power/Program.cs
@@ -8,12 +8,15 @@ namespace _8._Math_Power
         {
             double baseNumber = double.Parse(Console.ReadLine());
             int powerNumber = int.Parse(Console.ReadLine());
+            
             double finalNumber = MathPower(baseNumber, powerNumber);
             Console.WriteLine(finalNumber);
         }
+        
         static double MathPower(double baseNumber, int powerNumber)
         {
             double finalNumber =  Math.Pow(baseNumber, powerNumber);
+            
             return finalNumber;
         }
     }
