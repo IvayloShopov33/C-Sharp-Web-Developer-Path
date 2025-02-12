796c9e950349a9e1cdbd6d40137b8258ca7227da
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-12 02:23:28 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Lab/09. Greater of Two Values/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Lab/09. Greater of Two Values/Program.cs
@@ -9,34 +9,44 @@ namespace _9._Greater_of_Two_Values
             string valueType = Console.ReadLine();
             string value1 = Console.ReadLine();
             string value2 = Console.ReadLine();
-            if (valueType=="int")
+            
+            if (valueType == "int")
             {
                 GetMaxInt(value1, value2);
             }
-            else if (valueType=="string")
+            else if (valueType == "string")
             {
                 GetMaxString(value1, value2);
             }
-            else if (valueType=="char")
+            else if (valueType == "char")
             {
                 GetMaxChar(value1, value2);
             }
         }
+        
         static void GetMaxInt(string value1, string value2)
         {
             double value3 = double.Parse(value1);
             double value4 = double.Parse(value2);
-            if (value3>value4)
+            
+            if (value3 > value4)
+            {
                 Console.WriteLine(value3);
-            else if (value3<value4)
+            }
+            else if (value3 < value4)
+            {
                 Console.WriteLine(value4);
+            }
             else
+            {
                 Console.WriteLine(value3);
+            }
         }
+        
         static void GetMaxString(string value1, string value2)
         {
             
-             if (string.Compare(value1, value2,true)==1)
+            if (string.Compare(value1, value2,true) == 1)
             {
                 Console.WriteLine(value1);
             }
@@ -45,16 +55,24 @@ namespace _9._Greater_of_Two_Values
                 Console.WriteLine(value2);
             }
         }
+        
         static void GetMaxChar (string value1, string value2)
         {
             char a = char.Parse(value1);
             char b = char.Parse(value2);
-            if (a>b)
+            
+            if (a > b)
+            {
                 Console.WriteLine(a);
-            else if(b>a)
+            }
+            else if (b > a)
+            {
                 Console.WriteLine(b);
+            }
             else
+            {
                 Console.WriteLine(a);
+            }
         }
     }
 }
