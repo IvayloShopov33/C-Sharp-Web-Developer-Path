66531b22414eb9d085b653fcaeacb56a3fd0b27c
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-15 00:13:32 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Exercises/01. Smallest of Three Numbers/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Exercises/01. Smallest of Three Numbers/Program.cs
@@ -9,6 +9,7 @@ namespace _1._Smallest_of_Three_Numbers
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
+            
             if (a <= b && a <= c)
                 FirstIsTheSmallest(a, b, c);
             else if (b <= a && b <= c)
@@ -16,14 +17,17 @@ namespace _1._Smallest_of_Three_Numbers
             else if (c <= a && c <= b)
                 ThirdIsTheSmallest(a, b, c);
         }
+        
         static void FirstIsTheSmallest(int a, int b, int c)
         {
             Console.WriteLine(a);
         }
+        
         static void SecondIsTheSmallest(int a, int b, int c)
         {
             Console.WriteLine(b);
         }
+        
         static void ThirdIsTheSmallest(int a, int b, int c)
         {
             Console.WriteLine(c);
