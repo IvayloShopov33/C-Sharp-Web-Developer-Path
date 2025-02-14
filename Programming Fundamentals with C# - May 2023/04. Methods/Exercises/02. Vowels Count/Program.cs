823015a95f7efa74eeba4051e0461385ab2284b8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-15 00:14:27 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Exercises/02. Vowels Count/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Exercises/02. Vowels Count/Program.cs
@@ -9,18 +9,21 @@ namespace _2._Vowels_Count
             string str = Console.ReadLine();
             Console.WriteLine(VowelsInString(str)); 
         }
+        
         static int VowelsInString(string str)
         {
             char[] ch = str.ToCharArray();
             int vowelsCount = 0;
+            
             for (int i = 0; i < ch.Length; i++)
             {
-                if (ch[i]=='a' || ch[i]=='e' || ch[i]=='i' || ch[i]=='o' || ch[i]=='u'
-                    || ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U')
+                if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u'||
+                    ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U')
                 {
                     vowelsCount++;
                 }
             }
+            
             return vowelsCount;
         }
     }
