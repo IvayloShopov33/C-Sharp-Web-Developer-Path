ab71ed1023abefdea3c082bb8f5ec7e22cf15b56
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-16 00:41:08 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Exercises/03. Characters in Range/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Exercises/03. Characters in Range/Program.cs
@@ -16,10 +16,7 @@ namespace _3._Characters_in_Range
         {
             if (a > b)
             {
-                int start = b + 1;
-                int end = a;
-                
-                for (int i = start; i < end; i++)
+                for (int i = b + 1; i < a; i++)
                 {
                     Console.Write($"{(char)i} ");
                 }
