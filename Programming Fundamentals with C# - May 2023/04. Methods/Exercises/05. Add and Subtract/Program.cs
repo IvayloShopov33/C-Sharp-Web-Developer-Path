6a266dbd70767ee7b10c6c0a458e40de41c324f7
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-18 00:28:17 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Exercises/05. Add and Subtract/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Exercises/05. Add and Subtract/Program.cs
@@ -9,18 +9,24 @@ namespace _5._Add_and_Subtract
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
+            
             int result = Add(a, b);
             int finalAction = Subtract(result, c);
+            
             Console.WriteLine(finalAction);
         }
+        
         static int Add(int a, int b)
         {
             int result = a + b;
+            
             return result;
         }
+        
         static int Subtract(int result, int c)
         {
             int finalAction = result - c;
+            
             return finalAction;
         }
     }
