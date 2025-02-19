0378edd5468fdf8feb915fd73d151ff7c7dd58fe
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-19 02:03:11 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Exercises/06. Middle Characters/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Exercises/06. Middle Characters/Program.cs
@@ -7,7 +7,7 @@ namespace _6._Middle_Characters
         static void Main(string[] args)
         {
             string text = Console.ReadLine();
-            if ((text.Length-1)%2==0)
+            if ((text.Length - 1) % 2 == 0)
             {
                 GetMiddleCharacter(text);
             }
@@ -16,16 +16,19 @@ namespace _6._Middle_Characters
                 GetMiddleCharacterSecondOption(text);
             }
         }
+        
         static void GetMiddleCharacter(string text)
         {
-            char middlechar = text[(text.Length - 1)/2];
+            char middlechar = text[(text.Length - 1) / 2];
             Console.WriteLine(middlechar);
         }
+        
         static void GetMiddleCharacterSecondOption(string text)
         {
             string middleCharacters = string.Empty;
             char middlechar1 = text[(text.Length - 1) / 2];
-            char middlechar2 = text[((text.Length - 1) / 2)+1];
+            char middlechar2 = text[((text.Length - 1) / 2) + 1];
+            
             middleCharacters += middlechar1;
             middleCharacters += middlechar2;
             Console.WriteLine(middleCharacters);
