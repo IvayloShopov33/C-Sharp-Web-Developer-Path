24fc139af8dfb0f6b19555785a61ab5807f3f3fb
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-21 00:32:19 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Exercises/08. Factorial Division/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Exercises/08. Factorial Division/Program.cs
@@ -8,18 +8,21 @@ namespace _08._Factorial_Division
         {
             int firstNumber = int.Parse(Console.ReadLine());
             int secondNumber = int.Parse(Console.ReadLine());
+            
             decimal factorialOfTheFirstNumber = GetFactorialOfTheNumber(firstNumber);
             decimal factorialOfTheSecondNumber = GetFactorialOfTheNumber(secondNumber);
+            
             decimal finalResult = factorialOfTheFirstNumber / factorialOfTheSecondNumber;
             Console.WriteLine($"{finalResult:f2}");
         }
+        
         static decimal GetFactorialOfTheNumber(int number)
         {
             decimal result = 1;
-                for (int i = 2; i <= number; i++)
-                {
-                    result *= i;
-                }
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
 
             return result;
         }
