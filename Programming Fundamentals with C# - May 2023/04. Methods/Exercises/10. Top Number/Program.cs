7d3de199cb5d5901ea528e8ad91595147bb22bce
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-23 02:29:27 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/Exercises/10. Top Number/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/Exercises/10. Top Number/Program.cs
@@ -10,16 +10,17 @@ namespace _10._Top_Number
             int number = int.Parse(Console.ReadLine());
             TopNumber(number);
         }
+        
         static void TopNumber(int number)
         {
             for (int i = 1; i <= number; i++)
             {
                 int[] n = i.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
-                if (n.Sum()%8==0)
+                if (n.Sum() % 8 == 0)
                 {
                     for (int j = 0; j < n.Length; j++)
                     {
-                        if (n[j]%2==1)
+                        if (n[j] % 2 == 1)
                         {
                             Console.WriteLine(i);
                             break;
