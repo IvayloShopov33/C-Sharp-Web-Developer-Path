8d8ed0c84398b08278b703cbfa07a9eb053d22ee
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-25 00:22:32 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/More Exercises/1. Data Types/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/More Exercises/1. Data Types/Program.cs
@@ -9,29 +9,32 @@ namespace _1._Data_Types
             string dataType = Console.ReadLine();
             DataType(dataType);
         }
+        
         static void DataType(string dataType)
         {
-            if (dataType=="int")
+            if (dataType == "int")
             {
                 int number = int.Parse(Console.ReadLine());
-                Console.WriteLine(number*2);
+                Console.WriteLine(number * 2);
             }
-            else if (dataType=="real")
+            else if (dataType == "real")
             {
                 double number = double.Parse(Console.ReadLine());
                 double result = number * 1.5;
                 Console.WriteLine($"{result:f2}");
             }
-            else if (dataType=="string")
+            else if (dataType == "string")
             {
                 string str = Console.ReadLine();
                 string[] str1 = new string[str.Length + 2];
                 str1[0] = "$";
                 str1[str1.Length - 1] = "$";
-                for (int i = 1; i < str1.Length-1; i++)
+                
+                for (int i = 1; i < str1.Length - 1; i++)
                 {
                     str1[i] += str[i - 1];
                 }
+                
                 for (int i = 0; i < str1.Length; i++)
                 {
                     Console.Write(str1[i]);
