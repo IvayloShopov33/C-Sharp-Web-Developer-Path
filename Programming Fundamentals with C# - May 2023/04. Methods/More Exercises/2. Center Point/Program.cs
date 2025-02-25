704c7d1de28d942fa41dc111450d5be412602b4e
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-02-26 01:12:07 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/04. Methods/More Exercises/2. Center Point/Program.cs
+++ Programming Fundamentals with C# - May 2023/04. Methods/More Exercises/2. Center Point/Program.cs
@@ -9,14 +9,17 @@ namespace _2._Center_Point
             double x1 = double.Parse(Console.ReadLine());
             double y1 = double.Parse(Console.ReadLine());
             double x2 = double.Parse(Console.ReadLine());
-           double y2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            
             CloserToCenterPoint(x1, y1, x2, y2);
         }
-        static void CloserToCenterPoint(double x1, double y1,double x2, double y2)
+        
+        static void CloserToCenterPoint(double x1, double y1, double x2, double y2)
         {
             double distance1 = CalculateDistanceToCenter(x1, y1);
             double distance2 = CalculateDistanceToCenter(x2, y2);
-            if (distance1<=distance2)
+            
+            if (distance1 <= distance2)
             {
                 Console.WriteLine($"({x1}, {y1})");
             }
@@ -25,9 +28,11 @@ namespace _2._Center_Point
                 Console.WriteLine($"({x2}, {y2})");
             }
         }
+        
         static double CalculateDistanceToCenter(double x, double y)
         {
             double distance = Math.Sqrt(x * x + y * y);
+            
             return distance;
         }
     }
