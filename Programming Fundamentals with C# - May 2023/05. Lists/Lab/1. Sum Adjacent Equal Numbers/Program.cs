0276c16820f4a16fef82bfe8bbf034d381b9886c
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-02 00:08:38 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/05. Lists/Lab/1. Sum Adjacent Equal Numbers/Program.cs
+++ Programming Fundamentals with C# - May 2023/05. Lists/Lab/1. Sum Adjacent Equal Numbers/Program.cs
@@ -11,17 +11,17 @@ namespace _1._Sum_Adjacent_Equal_Numbers
             List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
             for (int i = 0; i < numbers.Count; i++)
             {
-                if (i+1<=numbers.Count-1)
+                if (i + 1 <= numbers.Count - 1)
                 {
-
-                    if (numbers[i]==numbers[i+1])
+                    if (numbers[i] == numbers[i + 1])
                     {
                         numbers[i] += numbers[i + 1];
                         numbers.RemoveAt(i + 1);
-                         i = -1;
+                        i = -1;
                     }
                 }
             }
+            
             Console.WriteLine(string.Join(' ', numbers));
         }
     }
