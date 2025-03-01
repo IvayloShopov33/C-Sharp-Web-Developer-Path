5dc4d20fdac360ba14ba15938a74341ebfecd21d
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-02 00:10:15 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/05. Lists/Lab/2. Gauss' Trick/Program.cs
+++ Programming Fundamentals with C# - May 2023/05. Lists/Lab/2. Gauss' Trick/Program.cs
@@ -10,14 +10,17 @@ namespace _2._Gauss__Trick
         {
             List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
             List<int> newNumbers = new List<int>();
-            for (int i = 0; i < numbers.Count/2; i++)
+            
+            for (int i = 0; i < numbers.Count / 2; i++)
             {
                 newNumbers.Add(numbers[i] + numbers[numbers.Count - 1 - i]);                
             }
-            if (numbers.Count%2==1)
+            
+            if (numbers.Count % 2 == 1)
             {
                 newNumbers.Add(numbers[numbers.Count / 2]);
             }
+            
             Console.WriteLine(string.Join(' ', newNumbers));
         }
     }
