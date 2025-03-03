ba783a48558b099d81441e40950828eac4828a9c
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-03 14:26:38 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/05. Lists/Lab/4. List of Products/Program.cs
+++ Programming Fundamentals with C# - May 2023/05. Lists/Lab/4. List of Products/Program.cs
@@ -11,15 +11,17 @@ namespace _4._List_of_Products
             int quantityOfProducts = int.Parse(Console.ReadLine());
             string product;
             List<string> listOfProducts = new List<string>();
+            
             for (int i = 0; i < quantityOfProducts; i++)
             {
                 product = Console.ReadLine();
                 listOfProducts.Add(product);
             }
+            
             listOfProducts.Sort();
             for (int i = 0; i < listOfProducts.Count; i++)
             {
-                Console.WriteLine($"{i+1}.{listOfProducts[i]}");
+                Console.WriteLine($"{i + 1}.{listOfProducts[i]}");
             }
         }
     }
