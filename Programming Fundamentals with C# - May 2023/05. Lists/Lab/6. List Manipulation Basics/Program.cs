50015df4e8afb1367b2fe504e9a1468bd7c0a419
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-04 01:41:53 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/05. Lists/Lab/6. List Manipulation Basics/Program.cs
+++ Programming Fundamentals with C# - May 2023/05. Lists/Lab/6. List Manipulation Basics/Program.cs
@@ -10,6 +10,7 @@ namespace _6._List_Manipulation_Basics
         {
             List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
             string[] input = Console.ReadLine().Split();
+            
             while (input[0] != "end")
             {
                 if (input[0] == "Add")
@@ -38,18 +39,21 @@ namespace _6._List_Manipulation_Basics
         static List<int> Add(string[] input, List<int> num)
         {
             num.Add(int.Parse(input[1]));
+            
             return num;
         }
 
         static List<int> Remove(string[] input, List<int> numbers)
         {
             numbers.Remove(int.Parse(input[1]));
+            
             return numbers;
         }
 
         static List<int> RemoveAt(string[] input, List<int> numbers)
         {
             numbers.RemoveAt(int.Parse(input[1]));
+            
             return numbers;
         }
 
@@ -58,6 +62,7 @@ namespace _6._List_Manipulation_Basics
             int number = int.Parse(input[1]);
             int index = int.Parse(input[2]);
             numbers.Insert(index, number);
+            
             return numbers;
         }
     }
