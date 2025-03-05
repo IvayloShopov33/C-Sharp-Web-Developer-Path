a2a163bc11c51ab2e28ce1dd3fc848ac44c3f05a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-06 01:28:17 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/05. Lists/Exercises/01. Train/Program.cs
+++ Programming Fundamentals with C# - May 2023/05. Lists/Exercises/01. Train/Program.cs
@@ -11,9 +11,10 @@ namespace _1._Train
             List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
             int maxPassengers = int.Parse(Console.ReadLine());
             string[] input = Console.ReadLine().Split();
-            while (input[0]!="end")
+            
+            while (input[0] != "end")
             {
-                if (input.Length==2)
+                if (input.Length == 2)
                 {
                     wagons.Add(int.Parse(input[1]));
                 }
@@ -21,7 +22,7 @@ namespace _1._Train
                 {
                     for (int i = 0; i < wagons.Count; i++)
                     {
-                        if (wagons[i]+int.Parse(input[0])<=maxPassengers)
+                        if (wagons[i] + int.Parse(input[0]) <= maxPassengers)
                         {
                             wagons[i] += int.Parse(input[0]);
                             break;
@@ -31,6 +32,7 @@ namespace _1._Train
 
                 input = Console.ReadLine().Split();
             }
+            
             Console.WriteLine(string.Join(' ', wagons));
         }
     }
