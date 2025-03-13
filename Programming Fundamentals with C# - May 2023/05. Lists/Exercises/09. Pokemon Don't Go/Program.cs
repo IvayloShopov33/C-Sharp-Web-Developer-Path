cc06f3b92c5f16ec19c56d27f753cb0f11587c92
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-14 00:34:05 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/05. Lists/Exercises/09. Pokemon Don't Go/Program.cs
+++ Programming Fundamentals with C# - May 2023/05. Lists/Exercises/09. Pokemon Don't Go/Program.cs
@@ -9,19 +9,21 @@ namespace _9._Pokemon_Don_t_Go
         static void Main(string[] args)
         {
             List<int> distancesToPokemon = Console.ReadLine().Split().Select(int.Parse).ToList();
-            int index ;
+            int index;
             int sumOfRemovedElements = 0;
-            while (distancesToPokemon.Count>0)
+            
+            while (distancesToPokemon.Count > 0)
             {
                 index = int.Parse(Console.ReadLine());
-                if (index>=0 && index<distancesToPokemon.Count)
+                if (index >= 0 && index < distancesToPokemon.Count)
                 {
                     int copyElement = distancesToPokemon[index];
                     distancesToPokemon.RemoveAt(index);
                     sumOfRemovedElements += copyElement;
+                    
                     for (int i = 0; i < distancesToPokemon.Count; i++)
                     {
-                        if (distancesToPokemon[i]<=copyElement)
+                        if (distancesToPokemon[i] <= copyElement)
                         {
                             distancesToPokemon[i] += copyElement;
                         }
@@ -31,32 +33,15 @@ namespace _9._Pokemon_Don_t_Go
                         }
                     }
                 }
-                else if (index<0)
+                else if (index < 0)
                 {
-                    int copyElement = distancesToPokemon[distancesToPokemon.Count-1];
+                    int copyElement = distancesToPokemon[distancesToPokemon.Count - 1];
                     int removedElement = distancesToPokemon[0];
                     sumOfRemovedElements += removedElement;
+                    
                     distancesToPokemon.RemoveAt(0);
                     distancesToPokemon.Insert(0, copyElement);
-                    for (int i = 0; i < distancesToPokemon.Count; i++)
-                    {
-                        if (distancesToPokemon[i]<=removedElement)
-                        {
-                            distancesToPokemon[i] += removedElement;
-                        }
-                        else
-                        {
-                            distancesToPokemon[i] -= removedElement;
-                        }
-                    }
-                }
-                else if (index>distancesToPokemon.Count-1)
-                {
-                    int copyElement = distancesToPokemon[0];
-                    int removedElement = distancesToPokemon[distancesToPokemon.Count-1];
-                    sumOfRemovedElements += removedElement;
-                    distancesToPokemon.RemoveAt(distancesToPokemon.Count-1);
-                    distancesToPokemon.Insert(distancesToPokemon.Count, copyElement);
+                    
                     for (int i = 0; i < distancesToPokemon.Count; i++)
                     {
                         if (distancesToPokemon[i] <= removedElement)
@@ -69,8 +54,29 @@ namespace _9._Pokemon_Don_t_Go
                         }
                     }
                 }
-                
+                else if (index > distancesToPokemon.Count - 1)
+                {
+                    int copyElement = distancesToPokemon[0];
+                    int removedElement = distancesToPokemon[distancesToPokemon.Count - 1];
+                    sumOfRemovedElements += removedElement;
+                    
+                    distancesToPokemon.RemoveAt(distancesToPokemon.Count - 1);
+                    distancesToPokemon.Insert(distancesToPokemon.Count, copyElement);
+                    
+                    for (int i = 0; i < distancesToPokemon.Count; i++)
+                    {
+                        if (distancesToPokemon[i] <= removedElement)
+                        {
+                            distancesToPokemon[i] += removedElement;
+                        }
+                        else
+                        {
+                            distancesToPokemon[i] -= removedElement;
+                        }
+                    }
+                }               
             }
+            
             Console.WriteLine(sumOfRemovedElements);
         }
     }
