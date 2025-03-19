233ee3eba603269149ca45f21b87f5557d803e60
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-20 00:40:05 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/05. Lists/More Exercises/5. Drum Set/Program.cs
+++ Programming Fundamentals with C# - May 2023/05. Lists/More Exercises/5. Drum Set/Program.cs
@@ -9,9 +9,10 @@ namespace _5._Drum_Set
         static void Main(string[] args)
         {
             float money = float.Parse(Console.ReadLine());
-            List<int> drumsQuality = Console.ReadLine().Split().Select(int.Parse).ToList();
-            List<int> drumsQualityCopy= drumsQuality.ToList();
+            List<int> drumsQuality = Console.ReadLine().Split().Select(int.Parse).ToList();            
+            List<int> drumsQualityCopy = drumsQuality.ToList();
             string input = Console.ReadLine();
+            
             while (input != "Hit it again, Gabsy!")
             {
                 int power = int.Parse(input);
@@ -19,13 +20,15 @@ namespace _5._Drum_Set
                 {
                     drumsQuality[i] -= power;
                 }
+                
                 for (int i = 0; i < drumsQualityCopy.Count; i++)
                 {
-                    if (drumsQuality[i]<=0)
+                    if (drumsQuality[i] <= 0)
                     {
                         int indexNum = i;
                         float moneyForNewDrum = drumsQualityCopy[indexNum] * 3;
-                        if (moneyForNewDrum<=money)
+                        
+                        if (moneyForNewDrum <= money)
                         {
                             money -= moneyForNewDrum;
                             drumsQuality[indexNum] = drumsQualityCopy[indexNum];
@@ -35,6 +38,7 @@ namespace _5._Drum_Set
 
                 input = Console.ReadLine();
             }
+            
             drumsQuality.RemoveAll(x => x <= 0);
             Console.WriteLine(string.Join(' ', drumsQuality));
             Console.WriteLine($"Gabsy has {money:f2}lv.");
