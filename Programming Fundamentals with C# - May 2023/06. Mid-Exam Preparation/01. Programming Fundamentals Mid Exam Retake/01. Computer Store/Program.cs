5a814f990c22c89437573aa59e10ff32cee16a89
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-21 21:15:17 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/01. Programming Fundamentals Mid Exam Retake/01. Computer Store/Program.cs
+++ Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/01. Programming Fundamentals Mid Exam Retake/01. Computer Store/Program.cs
@@ -7,10 +7,12 @@ namespace _01._Computer_Store
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
+            
             double sumOfPricesWithoutTaxes = 0;
             double sumOfPricesWithTaxes = 0;
             double taxes = 0;
             double price = 0;
+            
             while (input != "special" && input != "regular")
             {
                 price = double.Parse(input);
@@ -18,12 +20,15 @@ namespace _01._Computer_Store
                 {
                     Console.WriteLine("Invalid price!");
                     input = Console.ReadLine();
+                    
                     continue;
                 }
+                
                 sumOfPricesWithoutTaxes += price;
                 taxes += price * 0.2;
                 price += price * 0.2;
                 sumOfPricesWithTaxes += price;
+                
                 input = Console.ReadLine();
             }
 
@@ -36,7 +41,6 @@ namespace _01._Computer_Store
             {
                 Console.WriteLine("Invalid order!");
             }
-
             else
             {
                 Console.WriteLine("Congratulations you've just bought a new computer!");
