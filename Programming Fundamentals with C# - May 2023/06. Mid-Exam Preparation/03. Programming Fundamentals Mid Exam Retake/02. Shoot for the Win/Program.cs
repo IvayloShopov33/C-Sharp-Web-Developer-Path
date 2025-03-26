039ba933d138d2c5435443d15af298bc73f1be79
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-27 00:08:41 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/03. Programming Fundamentals Mid Exam Retake/02. Shoot for the Win/Program.cs
+++ Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/03. Programming Fundamentals Mid Exam Retake/02. Shoot for the Win/Program.cs
@@ -10,19 +10,21 @@ namespace _02._Shoot_for_the_Win
             int[] targets = Console.ReadLine().Split().Select(int.Parse).ToArray();
             string input = Console.ReadLine();
             int shots = 0;
-            while (input!="End")
+            
+            while (input != "End")
             {
                 int targetIndex = int.Parse(input);
-                if (targetIndex>=0 && targetIndex<targets.Length && targets[targetIndex]!=-1)
+                if (targetIndex >= 0 && targetIndex < targets.Length && targets[targetIndex] != -1)
                 {
                     shots++;
                     int targetNumber = targets[targetIndex];
                     targets[targetIndex] = -1;
+                    
                     for (int i = 0; i < targets.Length; i++)
                     {
-                        if (i!=targetIndex && targets[i]!=-1)
+                        if (i != targetIndex && targets[i] != -1)
                         {
-                            if (targets[i]>targetNumber)
+                            if (targets[i] > targetNumber)
                             {
                                 targets[i] -= targetNumber;
                             }
