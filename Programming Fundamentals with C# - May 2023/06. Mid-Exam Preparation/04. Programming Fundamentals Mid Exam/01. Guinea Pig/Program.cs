e7fb2fd4e5d911c3e3fc395af030e8aa579ef4e0
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-28 01:24:56 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/04. Programming Fundamentals Mid Exam/01. Guinea Pig/Program.cs
+++ Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/04. Programming Fundamentals Mid Exam/01. Guinea Pig/Program.cs
@@ -10,35 +10,42 @@ namespace _01._Guinea_Pig
             float hay = float.Parse(Console.ReadLine());
             float cover = float.Parse(Console.ReadLine());
             float weight = float.Parse(Console.ReadLine());
+            
             food *= 1000;
             hay *= 1000;
             cover *= 1000;
             weight *= 1000;
+            
             int days = 0;
             bool isEnough = false;
-            while (food>0 && cover>0 && hay>0)
+            
+            while (food > 0 && cover > 0 && hay > 0)
             {
                 days++;
-                if (days==31)
+                if (days == 31)
                 {
                     isEnough = true;
                     break;
                 }
+                
                 food -= 300;
-                if (days%2==0)
+                if (days % 2 == 0)
                 {
                     hay -= food * 0.05f;
                 }
-                if (days%3==0)
+                
+                if (days % 3 == 0)
                 {
                     cover -= weight / 3;
                 }
             }
+            
             if (isEnough)
             {
                 food /= 1000;
                 hay /= 1000;
                 cover /= 1000;
+                
                 Console.WriteLine($"Everything is fine! Puppy is happy! Food: {food:f2}, Hay: {hay:f2}, Cover: {cover:f2}.");
             }
             else
