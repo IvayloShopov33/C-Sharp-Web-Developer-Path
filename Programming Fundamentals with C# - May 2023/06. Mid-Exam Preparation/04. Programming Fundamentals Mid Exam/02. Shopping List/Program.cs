48578ade520edc1bcb324abf7623eba9ab7046c9
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-29 22:01:31 +0200

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/04. Programming Fundamentals Mid Exam/02. Shopping List/Program.cs
+++ Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/04. Programming Fundamentals Mid Exam/02. Shopping List/Program.cs
@@ -10,36 +10,35 @@ namespace _02._Shopping_List
         {
             List<string> groceries = Console.ReadLine().Split('!').ToList();
             string[] input = Console.ReadLine().Split();
-            while (input[0]!="Go" && input[1]!="Shopping!")
+            
+            while (input[0] != "Go" && input[1] != "Shopping!")
             {
                 string item = input[1];
-                if (input[0]=="Urgent")
+                if (input[0] == "Urgent")
                 {
                     if (!groceries.Contains(item))
                     {
                         groceries.Insert(0, item);
                     }
                 }
-
-                else if (input[0]=="Unnecessary")
+                else if (input[0] == "Unnecessary")
                 {
                     if (groceries.Contains(item))
                     {
                         groceries.Remove(item);
                     }
                 }
-
-                else if (input[0]=="Correct")
+                else if (input[0] == "Correct")
                 {
                     if (groceries.Contains(item))
                     {
                         string newItem = input[2];
                         int index = groceries.IndexOf(item);
+                        
                         groceries.Insert(index + 1, newItem);
                         groceries.RemoveAt(index);
                     }
                 }
-
                 else
                 {
                     if (groceries.Contains(item))
