562c707df11c8446105962acb4c4e1d9d3284971
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-03-31 00:15:18 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/05. Programming Fundamentals Mid Exam/01. Bonus Scoring System/Program.cs
+++ Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/05. Programming Fundamentals Mid Exam/01. Bonus Scoring System/Program.cs
@@ -9,17 +9,20 @@ namespace _01._Bonus_Scoring_System
             int studentsCount = int.Parse(Console.ReadLine());
             int lecturesCount = int.Parse(Console.ReadLine());
             int additionalBonus = int.Parse(Console.ReadLine());
+            
             int maxBonus = 0;
             double maxBonusDouble = 0;
             int attendances;
             double totalStudentBonus;
             int totalBonus;
             int studentWithHighestBonusAttendances = 0;
+            
             for (int i = 1; i <= studentsCount; i++)
             {
                 attendances = int.Parse(Console.ReadLine());
                 totalStudentBonus = (attendances * 1.0 / lecturesCount) * (5 + additionalBonus);
                 totalBonus = (int)Math.Ceiling(totalStudentBonus);
+                
                 if (maxBonusDouble < totalStudentBonus)
                 {
                     maxBonusDouble = totalStudentBonus;
@@ -27,9 +30,9 @@ namespace _01._Bonus_Scoring_System
                     maxBonus = totalBonus;
                 }
             }
+            
             Console.WriteLine($"Max Bonus: {maxBonus}.");
             Console.WriteLine($"The student has attended {studentWithHighestBonusAttendances} lectures.");
         }
     }
 }
-
