a9ce08542cdcaed73af7c91b13f47cbb96f40532
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-01 00:46:10 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/05. Programming Fundamentals Mid Exam/02.MuOnline/Program.cs
+++ Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/05. Programming Fundamentals Mid Exam/02.MuOnline/Program.cs
@@ -10,25 +10,26 @@ namespace _02.MuOnline
             int health = 100;
             int bitcoins = 0;
             string[] room;
+            
             for (int i = 0; i < rooms.Length; i++)
             {
                 room = rooms[i].Split();
-                if (room[0]=="potion")
+                if (room[0] == "potion")
                 {
                     int hp = int.Parse(room[1]);
                     health += hp;
-                    if (health>100)
+                    
+                    if (health > 100)
                     {
                         int difference = health - hp;
                         hp = 100 - difference;
-                        health = 100;
-                        
+                        health = 100;                      
                     }
+                    
                     Console.WriteLine($"You healed for {hp} hp.");
-                        Console.WriteLine($"Current health: {health} hp.");
+                    Console.WriteLine($"Current health: {health} hp.");
                 }
-
-                else if (room[0]=="chest")
+                else if (room[0] == "chest")
                 {
                     int bitcoinsInTheRoom = int.Parse(room[1]);
                     bitcoins += bitcoinsInTheRoom;
@@ -38,10 +39,12 @@ namespace _02.MuOnline
                 {
                     int strength = int.Parse(room[1]);
                     health -= strength;
-                    if (health<=0)
+                    
+                    if (health <= 0)
                     {
                         Console.WriteLine($"You died! Killed by {room[0]}.");
                         Console.WriteLine($"Best room: {i+1}");
+                        
                         return;
                     }
                     else
