eee08581a439581ccf4936c76b2656c2f794af83
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-02 01:07:27 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/05. Programming Fundamentals Mid Exam/03. Inventory/Program.cs
+++ Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/05. Programming Fundamentals Mid Exam/03. Inventory/Program.cs
@@ -10,35 +10,37 @@ namespace _03._Inventory
         {
             List<string> items = Console.ReadLine().Split(", ").ToList();
             string[] input = Console.ReadLine().Split(" - ");
-            while (input[0]!="Craft!")
+            
+            while (input[0] != "Craft!")
             {
                 string item = input[1];
-                if (input[0]=="Collect")
+                if (input[0] == "Collect")
                 {
                     if (!items.Contains(item))
                     {
                         items.Add(item);
                     }
                 }
-                else if (input[0]=="Drop")
+                else if (input[0] == "Drop")
                 {
                     if (items.Contains(item))
                     {
                         items.Remove(item);
                     }
                 }
-                else if (input[0]=="Combine Items")
+                else if (input[0] == "Combine Items")
                 {
                     string[] combinedItems = item.Split(':');
                     string oldItem = combinedItems[0];
                     string newItem = combinedItems[1];
+                    
                     if (items.Contains(oldItem))
                     {
                         int index = items.IndexOf(oldItem);
                         items.Insert(index + 1, newItem);
                     }
                 }
-                else if (input[0]=="Renew")
+                else if (input[0] == "Renew")
                 {
                     if (items.Contains(item))
                     {
