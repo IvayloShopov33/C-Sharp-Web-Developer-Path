4b659be863e9643118828b90494324e26cae744b
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-02 01:08:50 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/06. Programming Fundamentals Mid Exam Retake/01. Black Flag/Program.cs
+++ Programming Fundamentals with C# - May 2023/06. Mid-Exam Preparation/06. Programming Fundamentals Mid Exam Retake/01. Black Flag/Program.cs
@@ -9,9 +9,11 @@ namespace _01._Black_Flag
             int daysOfPirating = int.Parse(Console.ReadLine());
             double plunderForOneDay = double.Parse(Console.ReadLine());
             double expectedPlunder = double.Parse(Console.ReadLine());
+            
             int days = 0;
             double allPlunder = 0;
             double plunderForOneDayExtra = 0;
+            
             while (days != daysOfPirating)
             {
                 days++;
@@ -20,7 +22,6 @@ namespace _01._Black_Flag
                     plunderForOneDayExtra = plunderForOneDay * 0.5;
                     allPlunder += plunderForOneDay + plunderForOneDayExtra;
                 }
-
                 else
                 {
                     allPlunder += plunderForOneDay;
@@ -36,7 +37,6 @@ namespace _01._Black_Flag
             {
                 Console.WriteLine($"Ahoy! {allPlunder:f2} plunder gained.");
             }
-
             else
             {
                 double percent = allPlunder / expectedPlunder * 100;
