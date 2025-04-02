daf5d6024cc0435885162827aaabdd49a6048d03
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-03 01:16:57 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/07. Mid Exam/01. The Biscuit Factory/Program.cs
+++ Programming Fundamentals with C# - May 2023/07. Mid Exam/01. The Biscuit Factory/Program.cs
@@ -9,25 +9,30 @@ namespace _01._The_Biscuit_Factory
             int biscuitsCount = int.Parse(Console.ReadLine());
             int workersCount = int.Parse(Console.ReadLine());
             int enemiesbiscuitsCount = int.Parse(Console.ReadLine());
+            
             int allbiscuits = 0;
             int days = 0;
             int biscuitsPerDay = 0;
-            while (days<30)
+             
+            while (days < 30)
             {
                 days++;
                 biscuitsPerDay = biscuitsCount * workersCount;
-                if (days%3==0)
+                
+                if (days % 3 == 0)
                 {
                     double production = 0.75 * biscuitsPerDay;
                     biscuitsPerDay = (int)Math.Floor(production);
                 }
+                
                 allbiscuits += biscuitsPerDay;
             }
 
             int difference = Math.Abs(allbiscuits - enemiesbiscuitsCount);
-            double percent = difference*1.0 / enemiesbiscuitsCount * 100;
+            double percent = difference * 1.0 / enemiesbiscuitsCount * 100;
             Console.WriteLine($"You have produced {allbiscuits} biscuits for the past month.");
-            if (allbiscuits>enemiesbiscuitsCount)
+            
+            if (allbiscuits > enemiesbiscuitsCount)
             {
                 Console.WriteLine($"You produce {percent:f2} percent more biscuits.");
             }
