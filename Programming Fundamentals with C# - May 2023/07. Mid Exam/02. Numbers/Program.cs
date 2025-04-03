e74da6f4a40ddf54eae43955e7be9aa65871a930
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-04 00:26:38 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/07. Mid Exam/02. Numbers/Program.cs
+++ Programming Fundamentals with C# - May 2023/07. Mid Exam/02. Numbers/Program.cs
@@ -10,37 +10,36 @@ namespace _02._Numbers
         {
             List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
             string[] input = Console.ReadLine().Split();
-            while (input[0]!="Finish")
+            
+            while (input[0] != "Finish")
             {
-                int value = int.Parse(input[1]);
-                if (input[0]=="Add")
+                int @value = int.Parse(input[1]);
+                if (input[0] == "Add")
                 {
-                    numbers.Add(value);
+                    numbers.Add(@value);
                 }
-
-                else if (input[0]=="Remove")
+                else if (input[0] == "Remove")
                 {
-                    if (numbers.Contains(value))
+                    if (numbers.Contains(@value))
                     {
-                        int indexOfTheValue = numbers.IndexOf(value);
+                        int indexOfTheValue = numbers.IndexOf(@value);
                         numbers.RemoveAt(indexOfTheValue);
                     }
                 }
-
-                else if (input[0]=="Replace")
+                else if (input[0] == "Replace")
                 {
-                    if (numbers.Contains(value))
+                    if (numbers.Contains(@value))
                     {
-                        int indexOfTheValue = numbers.IndexOf(value);
+                        int indexOfTheValue = numbers.IndexOf(@value);
                         int replacementValue = int.Parse(input[2]);
+                        
                         numbers.Insert(indexOfTheValue + 1, replacementValue);
                         numbers.RemoveAt(indexOfTheValue);
                     }
                 }
-
-                else if (input[0]=="Collapse")
+                else if (input[0] == "Collapse")
                 {
-                    numbers.RemoveAll(x => x < value);
+                    numbers.RemoveAll(x => x < @value);
                 }
 
                 input = Console.ReadLine().Split();
