d5ecc95545dda8ecf1b9278289203dea3a7ebf20
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-07 01:11:40 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/08. Objects and Classes/Lab/2. Big Factorial/Program.cs
+++ Programming Fundamentals with C# - May 2023/08. Objects and Classes/Lab/2. Big Factorial/Program.cs
@@ -9,10 +9,12 @@ namespace _2._Big_Factorial
         {
             int n = int.Parse(Console.ReadLine());
             BigInteger result = 1;
+            
             for (int i = 1; i <= n; i++)
             {
                 result *= i;
             }
+            
             Console.WriteLine(result);
         }
     }
