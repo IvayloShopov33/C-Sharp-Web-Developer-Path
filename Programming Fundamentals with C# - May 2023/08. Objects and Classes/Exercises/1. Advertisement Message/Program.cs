710ee202521f1d677e46d58151f2e1b02b1859f8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-13 01:19:10 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/08. Objects and Classes/Exercises/1. Advertisement Message/Program.cs
+++ Programming Fundamentals with C# - May 2023/08. Objects and Classes/Exercises/1. Advertisement Message/Program.cs
@@ -15,21 +15,23 @@ namespace _1._Advertisement_Message
                 "Try it yourself, I am very satisfied.", "I feel great!" };
 
             string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
-
             string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
 
             int randomSentencesCount = int.Parse(Console.ReadLine());
             Random randomElement = new Random();
+            
             for (int i = 0; i < randomSentencesCount; i++)
             {
                 int phraseIndex = randomElement.Next(phrases.Length);
                 string phrase = phrases[phraseIndex];
                 int eventIndex = randomElement.Next(events.Length);
                 string newEvent = events[eventIndex];
+                
                 int authorIndex = randomElement.Next(authors.Length);
                 string author = authors[authorIndex];
                 int cityIndex = randomElement.Next(cities.Length);
                 string city = cities[cityIndex];
+                
                 Console.WriteLine($"{phrase} {newEvent} {author} – {city}.");
             }
         }
