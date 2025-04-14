c3b68517563313402993148340bd9d240bc8f4a1
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-15 00:30:43 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/08. Objects and Classes/Exercises/3. Articles 2.0/Program.cs
+++ Programming Fundamentals with C# - May 2023/08. Objects and Classes/Exercises/3. Articles 2.0/Program.cs
@@ -13,6 +13,7 @@ namespace _3._Articles_2._0
                 string title = articlesParts[0];
                 string content = articlesParts[1];
                 string author = articlesParts[2];
+                
                 Article article = new Article(title, content, author);
                 Console.WriteLine(article);
             }
@@ -27,9 +28,11 @@ namespace _3._Articles_2._0
             Content = content;
             Author = author;
         }
+        
         public string Title { get; set; }
 
         public string Content { get; set; }
+        
         public string Author { get; set; }
 
         public override string ToString()
