a82cadc8063a9303fadc4c132baf87d4ea923b02
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-16 00:48:16 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/08. Objects and Classes/Exercises/4. Students/Program.cs
+++ Programming Fundamentals with C# - May 2023/08. Objects and Classes/Exercises/4. Students/Program.cs
@@ -10,15 +10,18 @@ namespace _4._Students
         {
             int studentsCount = int.Parse(Console.ReadLine());
             List<Student> students = new List<Student>();
+            
             for (int i = 1; i <= studentsCount; i++)
             {
                 string[] studentDetails = Console.ReadLine().Split();
                 string firstName = studentDetails[0];
                 string lastName = studentDetails[1];
                 float grade = float.Parse(studentDetails[2]);
+                
                 Student student = new Student(firstName, lastName, grade);
                 students.Add(student);               
             }
+            
             List<Student> arrangedStudents = students.OrderByDescending(x=> x.Grade).ToList();
             foreach (var student in arrangedStudents)
             {
@@ -29,16 +32,15 @@ namespace _4._Students
 
     public class Student
     {
-
         public Student(string firstName, string lastName, float grade)
         {
-            FirstName = firstName;
-            LastName = lastName;
-            Grade = grade;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
         }
+        
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public float Grade { get; set; }
-
     }
 }
