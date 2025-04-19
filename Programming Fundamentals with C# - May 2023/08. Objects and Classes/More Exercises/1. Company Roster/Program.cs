13d33bce2f63d9574cd7fa3ac4e911f3c4ae4214
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-20 01:41:17 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/08. Objects and Classes/More Exercises/1. Company Roster/Program.cs
+++ Programming Fundamentals with C# - May 2023/08. Objects and Classes/More Exercises/1. Company Roster/Program.cs
@@ -10,15 +10,18 @@ namespace _1._Company_Roster
         {
             List<Employee> employees = new List<Employee>();
             int employeesCount = int.Parse(Console.ReadLine());
+            
             for (int i = 1; i <= employeesCount; i++)
             {
                 string[] employeeDetails = Console.ReadLine().Split();
                 string name = employeeDetails[0];
                 double salary = double.Parse(employeeDetails[1]);
                 string department = employeeDetails[2];
+                
                 Employee employee = new Employee(name, salary, department);
                 employees.Add(employee);
             }
+            
             var highestAverageSalaryDepartment = employees.GroupBy(f => f.Department).Select(g => new
             {
                 Department = g.Key,
@@ -26,9 +29,9 @@ namespace _1._Company_Roster
             }).OrderByDescending(g => g.AverageSalary).First();
 
             Console.WriteLine($"Highest Average Salary: {highestAverageSalaryDepartment.Department}");
-
             var employeesInDepartment = employees.Where(e => e.Department == highestAverageSalaryDepartment.Department)
                 .OrderByDescending(e => e.Salary);
+            
             foreach (var employee in employeesInDepartment)
             {
                 Console.WriteLine($"{employee.Name} {employee.Salary:f2}");
@@ -44,8 +47,11 @@ namespace _1._Company_Roster
             this.Salary = salary;
             this.Department = department;
         }
+        
         public string Name { get; private set; }
+        
         public double Salary { get; private set; }
+        
         public string Department { get; private set; }
     }
 }
