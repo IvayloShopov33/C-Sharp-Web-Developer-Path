cb3a51bbc3c931b4120faec5df6d008b08cff5aa
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-04-24 17:21:07 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/08. Objects and Classes/More Exercises/5. Shopping Spree/Program.cs
+++ Programming Fundamentals with C# - May 2023/08. Objects and Classes/More Exercises/5. Shopping Spree/Program.cs
@@ -10,8 +10,10 @@ namespace _5._Shopping_Spree
         {
             List<Person> people = new List<Person>();
             List<Product> products = new List<Product>();
+
             InitializePeople(people);
             InitializeProducts(products);
+
             PurchaseProducts(people, products);
             PrintPurchasedProducts(people);
         }
@@ -24,10 +26,12 @@ namespace _5._Shopping_Spree
                 string[] personDetails = allPeople[i].Split('=');
                 string name = personDetails[0];
                 int money = int.Parse(personDetails[1]);
+
                 Person person = new Person(name, money);
                 people.Add(person);
             }
         }
+
         static void InitializeProducts(List<Product> products)
         {
             string[] allProducts = Console.ReadLine().Split(';');
@@ -36,10 +40,12 @@ namespace _5._Shopping_Spree
                 string[] productDetails = allProducts[i].Split('=');
                 string nameOfProduct = productDetails[0];
                 int cost = int.Parse(productDetails[1]);
+
                 Product product = new Product(nameOfProduct, cost);
                 products.Add(product);
             }
         }
+
         static void PurchaseProducts(List<Person> people, List<Product> products)
         {
             string[] shoppingCommands = Console.ReadLine().Split();
@@ -47,8 +53,10 @@ namespace _5._Shopping_Spree
             {
                 string nameOfTheBuyer = shoppingCommands[0];
                 string productToBuy = shoppingCommands[1];
+
                 Person buyer = people.First(g => g.Name == nameOfTheBuyer);
                 Product productNameToBuy = products.First(x => x.NameOfProduct == productToBuy);
+
                 if (buyer.Money >= productNameToBuy.Cost)
                 {
                     buyer.bagOfProducts.Add(productToBuy);
@@ -59,9 +67,11 @@ namespace _5._Shopping_Spree
                 {
                     Console.WriteLine($"{buyer.Name} can't afford {productToBuy}");
                 }
+
                 shoppingCommands = Console.ReadLine().Split();
             }
         }
+
         static void PrintPurchasedProducts(List<Person> people)
         {
             foreach (Person person in people)
@@ -81,13 +91,16 @@ namespace _5._Shopping_Spree
     public class Person
     {
         public List<string> bagOfProducts;
+
         public Person(string name, int money)
         {
-            Name = name;
-            Money = money;
+            this.Name = name;
+            this.Money = money;
             this.bagOfProducts = new List<string>();
         }
+
         public string Name { get; private set; }
+
         public int Money { get; set; }
     }
 
@@ -95,10 +108,12 @@ namespace _5._Shopping_Spree
     {
         public Product(string nameOfProduct, int cost)
         {
-            NameOfProduct = nameOfProduct;
-            Cost = cost;
+            this.NameOfProduct = nameOfProduct;
+            this.Cost = cost;
         }
+
         public string NameOfProduct { get; private set; }
+
         public int Cost { get; private set; }
     }
 }
