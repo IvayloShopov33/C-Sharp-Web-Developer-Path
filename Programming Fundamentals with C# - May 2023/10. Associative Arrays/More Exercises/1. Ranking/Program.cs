5bce204fd5598c165d093c115af24b4b9bea8e35
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-05-08 02:09:55 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/10. Associative Arrays/More Exercises/1. Ranking/Program.cs
+++ Programming Fundamentals with C# - May 2023/10. Associative Arrays/More Exercises/1. Ranking/Program.cs
@@ -12,25 +12,30 @@ namespace _1._Ranking
                 new Dictionary<string, string>();
             List<Student> students = new List<Student>();
             string[] contestsWithPasswords = Console.ReadLine().Split(':');
-            while (contestsWithPasswords[0]!="end of contests")
+            
+            while (contestsWithPasswords[0] != "end of contests")
             {
                 contests.Add(contestsWithPasswords[0], contestsWithPasswords[1]);
                 contestsWithPasswords = Console.ReadLine().Split(':');
             }
+            
             string[] usernamesWithPoints = Console.ReadLine().Split("=>");
-            while (usernamesWithPoints[0]!="end of submissions")
+            while (usernamesWithPoints[0] != "end of submissions")
             {
                 string contest = usernamesWithPoints[0];
                 string password = usernamesWithPoints[1];
-                if (contests.ContainsKey(contest) && contests[contest]==password)
+                
+                if (contests.ContainsKey(contest) && contests[contest] == password)
                 {
                     string username = usernamesWithPoints[2];
                     int points = int.Parse(usernamesWithPoints[3]);
+                    
                     if (!students.Any(g => g.Name == username))
                     {
                         Dictionary<string, int> studentDetails =
                             new Dictionary<string, int>();
                         studentDetails.Add(contest, points);
+                        
                         Student student = new Student(username, studentDetails);
                         students.Add(student);
                     }
@@ -51,39 +56,47 @@ namespace _1._Ranking
                         }
                     }
                 }
+                
                 usernamesWithPoints = Console.ReadLine().Split("=>");
             }
+            
             string bestStudent = string.Empty;
             int maxPoints = 0;
+            
             foreach (Student student in students)
             {
                 int studentMaxPoints = student.Courses.Values.Sum();
-                if (maxPoints<studentMaxPoints)
+                if (maxPoints < studentMaxPoints)
                 {
                     maxPoints = studentMaxPoints;
                     bestStudent = student.Name;
                 }
             }
-                Console.WriteLine($"Best candidate is {bestStudent} with total {maxPoints} points.");
+            
+            Console.WriteLine($"Best candidate is {bestStudent} with total {maxPoints} points.");
             Console.WriteLine("Ranking:");
+            
             foreach (Student student in students.OrderBy(x => x.Name))
             {
                 Console.WriteLine($"{student.Name}");
-                foreach (var course in student.Courses.OrderByDescending(x=>x.Value))
+                foreach (var course in student.Courses.OrderByDescending(x => x.Value))
                 {
                     Console.WriteLine($"#  {course.Key} -> {course.Value}");
                 }
             }
         }
     }
+    
     public class Student
     {
         public Student(string name, Dictionary<string, int> courses)
         {
-            Name = name;
-            Courses = courses;
+            this.Name = name;
+            this.Courses = courses;
         }
-       public string Name { get; private set; }
+        
+        public string Name { get; private set; }
+        
         public Dictionary<string, int> Courses { get; set; }
     }
 }
