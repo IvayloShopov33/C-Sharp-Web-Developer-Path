49c80e1fe9523a67a9ff26d53d4d13e22af74add
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-05-13 00:22:53 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/11. Bitwise Operations/1. Binary Digits Count/Program.cs
+++ Programming Fundamentals with C# - May 2023/11. Bitwise Operations/1. Binary Digits Count/Program.cs
@@ -7,17 +7,20 @@ namespace _1._Binary_Digits_Count
         static void Main(string[] args)
         {
             int decimalNumber = int.Parse(Console.ReadLine());
-            char binaryNumber =char.Parse(Console.ReadLine());
+            char binaryNumber = char.Parse(Console.ReadLine());
+            
             string decimalNumberToString = Convert.ToString(decimalNumber, 2);
             char[] binaryNumberToCharArray = decimalNumberToString.ToCharArray();
             int equalNumbersCount = 0;
+            
             foreach (var character in binaryNumberToCharArray)
             {
-                if (character==binaryNumber)
+                if (character == binaryNumber)
                 {
                     equalNumbersCount++;
                 }
             }
+            
             Console.WriteLine(equalNumbersCount);
         }
     }
