2d60943471a5c7d073ebdc6cdc0c2953bdef9d1c
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-05-14 00:32:01 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/11. Bitwise Operations/3. P-th Bit/Program.cs
+++ Programming Fundamentals with C# - May 2023/11. Bitwise Operations/3. P-th Bit/Program.cs
@@ -8,13 +8,19 @@ namespace _3._P_th_Bit
         {
             int number = int.Parse(Console.ReadLine());
             int positionOfElementToPrint = int.Parse(Console.ReadLine());
+            
             string numberToBinary = Convert.ToString(number, 2);
             char[] binaryNumberToCharArray = numberToBinary.ToCharArray();
             Array.Reverse(binaryNumberToCharArray);
-            if (positionOfElementToPrint>binaryNumberToCharArray.Length-1)
+            
+            if (positionOfElementToPrint > binaryNumberToCharArray.Length - 1)
+            {
                 Console.WriteLine(0);
+            }
             else
-            Console.WriteLine(binaryNumberToCharArray[positionOfElementToPrint]);
+            {
+                Console.WriteLine(binaryNumberToCharArray[positionOfElementToPrint]);
+            }
         }
     }
 }
