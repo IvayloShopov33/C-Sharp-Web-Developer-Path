60bd153293dcab82835f587da2386a74b5f50c48
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-05-15 01:25:10 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/11. Bitwise Operations/4. Bit Destroyer/Program.cs
+++ Programming Fundamentals with C# - May 2023/11. Bitwise Operations/4. Bit Destroyer/Program.cs
@@ -8,16 +8,20 @@ namespace _4._Bit_Destroyer
         {
             int inputNumber = int.Parse(Console.ReadLine());
             int position = int.Parse(Console.ReadLine());
+            
             string inputNumberToBinary = Convert.ToString(inputNumber, 2);
             char[] numberBinaryToCharArray = inputNumberToBinary.ToCharArray();
             Array.Reverse(numberBinaryToCharArray);
+            
             numberBinaryToCharArray[position] = '0';
             Array.Reverse(numberBinaryToCharArray);
             string numberToString = string.Empty;
+            
             for (int i = 0; i < numberBinaryToCharArray.Length; i++)
             {
                 numberToString += numberBinaryToCharArray[i];
             }
+            
             string newDecimalNumber = Convert.ToInt32(numberToString, 2).ToString();
             Console.WriteLine(newDecimalNumber);
         }
