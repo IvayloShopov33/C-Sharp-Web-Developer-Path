ffefea6582c695ddb0416f53fb811fb1f3902892
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-06-04 03:22:34 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/12. String and Text Processing/More Exercises/2. Ascii Sumator/Program.cs
+++ Programming Fundamentals with C# - May 2023/12. String and Text Processing/More Exercises/2. Ascii Sumator/Program.cs
@@ -9,18 +9,22 @@ namespace _2._Ascii_Sumator
             char firstChar = char.Parse(Console.ReadLine());
             char lastChar = char.Parse(Console.ReadLine());
             string textOfChars = Console.ReadLine();
+            
             char[] textToCharArray = textOfChars.ToCharArray();
             int totalSumOfASCIICodes = 0;
+            
             foreach  (char character in textToCharArray)
             {
                 int firstCharASCIICode = firstChar;
                 int lastCharASCIICode = lastChar;
                 int currentCharASCIICode = character;
-                if (currentCharASCIICode>firstCharASCIICode && currentCharASCIICode<lastCharASCIICode)
+                
+                if (currentCharASCIICode > firstCharASCIICode && currentCharASCIICode < lastCharASCIICode)
                 {
                     totalSumOfASCIICodes += currentCharASCIICode;
                 }
             }
+            
             Console.WriteLine(totalSumOfASCIICodes);
         }
     }
