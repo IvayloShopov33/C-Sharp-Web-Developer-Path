754c686fb157675a89cdded20a46321849691c95
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-06-16 20:29:42 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/13. Regular Expressions/Exercises/3. SoftUni Bar Income/Program.cs
+++ Programming Fundamentals with C# - May 2023/13. Regular Expressions/Exercises/3. SoftUni Bar Income/Program.cs
@@ -11,7 +11,8 @@ namespace _3._SoftUni_Bar_Income
             Regex regex = new Regex(pattern);
             string input;
             double totalSum = 0;
-            while ((input=Console.ReadLine())!="end of shift")
+
+            while ((input = Console.ReadLine()) != "end of shift")
             {
                 Match match = regex.Match(input);
                 if (match.Success)
@@ -19,12 +20,15 @@ namespace _3._SoftUni_Bar_Income
                     string customer = match.Groups["customer"].Value;
                     string product = match.Groups["product"].Value;
                     int quantity = int.Parse(match.Groups["quantity"].Value);
+
                     double pricePerUnit = double.Parse(match.Groups["price"].Value);
                     double sumOfOneProduct = quantity*pricePerUnit;
+
                     Console.WriteLine($"{customer}: {product} - {sumOfOneProduct:f2}");
                     totalSum += sumOfOneProduct;
                 }
             }
+
             Console.WriteLine($"Total income: {totalSum:f2}");
         }
     }
