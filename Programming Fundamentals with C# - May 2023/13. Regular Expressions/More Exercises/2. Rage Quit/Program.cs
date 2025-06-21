3e3714a77ebf5cfb839c637b48a445897fa4fdb5
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-06-21 22:25:57 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/13. Regular Expressions/More Exercises/2. Rage Quit/Program.cs
+++ Programming Fundamentals with C# - May 2023/13. Regular Expressions/More Exercises/2. Rage Quit/Program.cs
@@ -12,14 +12,18 @@ namespace _2._Rage_Quit
             StringBuilder outputMessage = new StringBuilder();
             Regex symbol = new Regex(@"[!-\/:-~ ]+");
             Regex multiplicate = new Regex(@"\d+");
+            
             List<char> uniqueSymbols = new List<char>();
             string message = Console.ReadLine();
+            
             MatchCollection symbolCollection = symbol.Matches(message);
             MatchCollection multiplaceCollection = multiplicate.Matches(message);
+            
             for (int i = 0; i < symbolCollection.Count; i++)
             {
                 char[] symbols = symbolCollection[i].Value.ToUpper().ToCharArray();
                 int number = int.Parse(multiplaceCollection[i].Value);
+                
                 for (int j = number; j >= 1; j--)
                 {
                     foreach (char item in symbols)
@@ -28,10 +32,12 @@ namespace _2._Rage_Quit
                         {
                             uniqueSymbols.Add(item);
                         }
+                        
                         outputMessage.Append(item);
                     }
                 }
             }
+            
             Console.WriteLine($"Unique symbols used: {uniqueSymbols.Count}");
             Console.WriteLine(outputMessage);
         }
