2c34425983a743bec502fad4a4e0de0b2a7d1b60
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-06-29 04:27:56 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/01. Programming Fundamentals Final Exam/2. Ad Astra/Program.cs
+++ Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/01. Programming Fundamentals Final Exam/2. Ad Astra/Program.cs
@@ -11,12 +11,15 @@ namespace _2._Ad_Astra
             string items = Console.ReadLine();
             MatchCollection matchCollection = Regex.Matches(items, pattern);
             int calories = 0;
+            
             foreach (Match item in matchCollection)
             {
                 calories += int.Parse(item.Groups["calories"].Value);
             }
+            
             int days = calories / 2000;
             Console.WriteLine($"You have food to last you for: {days} days!");
+            
             foreach (Match item in matchCollection)
             {
                 Console.WriteLine($"Item: {item.Groups["item"]}, Best before: {item.Groups["date"]}, Nutrition: {item.Groups["calories"]}");
