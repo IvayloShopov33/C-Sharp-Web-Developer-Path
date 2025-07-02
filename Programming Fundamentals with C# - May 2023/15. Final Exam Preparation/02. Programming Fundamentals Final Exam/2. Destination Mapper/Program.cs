ae56958292b38ad9536ac75c29575d8121d4c063
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-02 06:27:45 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/02. Programming Fundamentals Final Exam/2. Destination Mapper/Program.cs
+++ Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/02. Programming Fundamentals Final Exam/2. Destination Mapper/Program.cs
@@ -11,13 +11,16 @@ namespace _2._Destination_Mapper
             string pattern = @"(\=|\/)(?<Destination>[A-Z]([A-Za-z]+){2,})\1";
             string input = Console.ReadLine();
             MatchCollection matchCollection = Regex.Matches(input, pattern);
+
             int travelPoints = 0;
             List<string> destionations = new List<string>();
+
             foreach (Match match in matchCollection)
             {
                 travelPoints += match.Groups["Destination"].Value.Length;
                 destionations.Add(match.Groups["Destination"].Value);
             }
+
             Console.WriteLine($"Destinations: {string.Join(", ", destionations)}");
             Console.WriteLine($"Travel Points: {travelPoints}");
         }
