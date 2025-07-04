db6c6d1a9aa5c8c5e0e817a9a70dfb068f66106b
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-05 01:16:52 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/03. Programming Fundamentals Final Exam/2. Mirror Words/Program.cs
+++ Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/03. Programming Fundamentals Final Exam/2. Mirror Words/Program.cs
@@ -11,6 +11,7 @@ namespace _2._Mirror_Words
             Dictionary<string, string> mirrorWords =
                 new Dictionary<string, string>();
             MatchCollection matchCollection = InitializeValidAndMirrorWords(mirrorWords);
+
             PrintMirrorWords(mirrorWords, matchCollection);
         }
 
@@ -19,26 +20,33 @@ namespace _2._Mirror_Words
             string pattern = @"(\#|\@)(?<firstWord>[A-Za-z]){3,}\1\1(?<secondWord>[A-Za-z]){3,}\1";
             string input = Console.ReadLine();
             MatchCollection matchCollection = Regex.Matches(input, pattern);
+
             foreach (Match match in matchCollection)
             {
                 CaptureCollection firstWordCaptures = match.Groups["firstWord"].Captures;
                 string firstWord = string.Empty;
+
                 foreach (var item in firstWordCaptures)
                 {
                     firstWord += item;
                 }
+
                 CaptureCollection secondWordCaptures = match.Groups["secondWord"].Captures;
                 string secondWord = string.Empty;
+
                 foreach (var item in secondWordCaptures)
                 {
                     secondWord += item;
                 }
+
                 char[] secondWordToCharArray = secondWord.ToCharArray();
                 string mirrorWord = string.Empty;
+
                 for (int i = secondWordToCharArray.Length - 1; i >= 0; i--)
                 {
                     mirrorWord += secondWordToCharArray[i];
                 }
+
                 if (firstWord == mirrorWord)
                 {
                     mirrorWords.Add(firstWord, secondWord);
@@ -63,14 +71,17 @@ namespace _2._Mirror_Words
             {
                 Console.WriteLine("The mirror words are:");
                 int count = 0;
+
                 foreach (KeyValuePair<string, string> item in mirrorWords)
                 {
                     string textToPrint = $"{item.Key} <=> {item.Value}, ";
                     count++;
+
                     if (mirrorWords.Count == count)
                     {
                         textToPrint = textToPrint.Remove(textToPrint.Length - 2);
                     }
+
                     Console.Write(textToPrint);
                 }
             }
