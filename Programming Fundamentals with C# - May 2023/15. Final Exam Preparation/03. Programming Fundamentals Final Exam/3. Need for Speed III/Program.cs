0da85185ebebc5a04339d7b49099999252cb7b53
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-06 21:55:43 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/03. Programming Fundamentals Final Exam/3. Need for Speed III/Program.cs
+++ Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/03. Programming Fundamentals Final Exam/3. Need for Speed III/Program.cs
@@ -10,19 +10,24 @@ namespace _3._Need_for_Speed_III
         {
             List<Car> cars = new List<Car>();
             InitializeCars(cars);
+
             ReceiveCommands(cars);
             PrintCarsWithTheirDetails(cars);
         }
+
         static void InitializeCars(List<Car> cars)
         {
             int carsCount = int.Parse(Console.ReadLine());
             for (int i = 1; i <= carsCount; i++)
             {
                 string[] carDetails = Console.ReadLine().Split('|');
+
                 string model = carDetails[0];
                 int mileage = int.Parse(carDetails[1]);
                 int fuel = int.Parse(carDetails[2]);
+
                 Car car = new Car(model, mileage, fuel);
+
                 if (!cars.Contains(car))
                 {
                     cars.Add(car);
@@ -37,18 +42,22 @@ namespace _3._Need_for_Speed_III
             {
                 string[] commands = input.Split(" : ");
                 string carModel = commands[1];
+
                 if (cars.Any(x => x.Model == carModel))
                 {
                     Car selectedCar = cars.First(g => g.Model == carModel);
+
                     if (commands[0] == "Drive")
                     {
                         int distance = int.Parse(commands[2]);
                         int fuel = int.Parse(commands[3]);
+
                         if (selectedCar.Fuel >= fuel)
                         {
                             selectedCar.Mileage += distance;
                             selectedCar.Fuel -= fuel;
                             Console.WriteLine($"{carModel} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+
                             if (selectedCar.Mileage >= 100000)
                             {
                                 cars.Remove(selectedCar);
@@ -64,6 +73,7 @@ namespace _3._Need_for_Speed_III
                     {
                         int additionalFuel = int.Parse(commands[2]);
                         int refueledQuantity = selectedCar.Fuel + additionalFuel;
+
                         if (refueledQuantity > 75)
                         {
                             selectedCar.Fuel = 75;
@@ -75,12 +85,14 @@ namespace _3._Need_for_Speed_III
                             selectedCar.Fuel = refueledQuantity;
                             refueledQuantity = additionalFuel;
                         }
+
                         Console.WriteLine($"{carModel} refueled with {refueledQuantity} liters");
                     }
                     else if (commands[0] == "Revert")
                     {
                         int kilometers = int.Parse(commands[2]);
                         selectedCar.Mileage -= kilometers;
+
                         if (selectedCar.Mileage >= 10000)
                         {
                             Console.WriteLine($"{carModel} mileage decreased by {kilometers} kilometers");
@@ -102,16 +114,20 @@ namespace _3._Need_for_Speed_III
             }
         }
     }
+
     public class Car
     {
         public Car(string model, int mileage, int fuel)
         {
-            Model = model;
-            Mileage = mileage;
-            Fuel = fuel;
+            this.Model = model;
+            this.Mileage = mileage;
+            this.Fuel = fuel;
         }
+
         public string Model { get; private set; }
+
         public int Mileage { get; set; }
+
         public int Fuel { get; set; }
     }
 }
