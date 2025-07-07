56d2912b765f6e1ba4c7859834d069cb5d215f1b
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-08 02:40:44 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/04. Programming Fundamentals Final Exam/2. Fancy Barcodes/Program.cs
+++ Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/04. Programming Fundamentals Final Exam/2. Fancy Barcodes/Program.cs
@@ -9,18 +9,22 @@ namespace _2._Fancy_Barcodes
         {
             string pattern = @"^\@\#+(?<item>[A-Z][A-Za-z0-9]{4,}[A-Z])\@\#+$";
             int barcodesCount = int.Parse(Console.ReadLine());
+            
             for (int i = 1; i <= barcodesCount; i++)
             {
                 string barcode = Console.ReadLine();
                 Match match = Regex.Match(barcode, pattern);
+                
                 if (match.Success)
                 {
                     CaptureCollection captures = match.Groups["item"].Captures;
                     string itemName = string.Empty;
+                    
                     foreach (var capture in captures)
                     {
                         itemName += capture;
                     }
+                    
                     string digits = string.Empty;
                     foreach (char character in itemName)
                     {
@@ -29,6 +33,7 @@ namespace _2._Fancy_Barcodes
                             digits += character;
                         }
                     }
+                    
                     if (digits.Length == 0)
                     {
                         Console.WriteLine("Product group: 00");
@@ -37,7 +42,6 @@ namespace _2._Fancy_Barcodes
                     {
                         Console.WriteLine($"Product group: {digits}");
                     }
-
                 }
                 else
                 {
