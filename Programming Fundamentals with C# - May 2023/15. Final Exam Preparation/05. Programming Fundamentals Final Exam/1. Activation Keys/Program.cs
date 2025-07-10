7b7054b0bb5b36a6e9779bbd885d3ccd266875cf
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-10 14:57:09 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/05. Programming Fundamentals Final Exam/1. Activation Keys/Program.cs
+++ Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/05. Programming Fundamentals Final Exam/1. Activation Keys/Program.cs
@@ -8,11 +8,13 @@ namespace _1._Activation_Keys
         {
             string activationKey = Console.ReadLine();
             string[] commands = Console.ReadLine().Split(">>>");
+
             while (commands[0] != "Generate")
             {
                 if (commands[0] == "Contains")
                 {
                     string substring = commands[1];
+
                     if (activationKey.Contains(substring))
                     {
                         Console.WriteLine($"{activationKey} contains {substring}");
@@ -26,8 +28,10 @@ namespace _1._Activation_Keys
                 {
                     int startIndex = int.Parse(commands[2]);
                     int endIndex = int.Parse(commands[3]);
+
                     string letters = activationKey.Substring(startIndex, endIndex - startIndex);
                     string newLetters = string.Empty;
+
                     if (commands[1] == "Upper")
                     {
                         newLetters = letters.ToUpper();
@@ -36,19 +40,22 @@ namespace _1._Activation_Keys
                     {
                         newLetters = letters.ToLower();
                     }
+
                     activationKey = activationKey.Replace(letters, newLetters);
                     Console.WriteLine(activationKey);
-
                 }
                 else if (commands[0] == "Slice")
                 {
                     int startingIndex = int.Parse(commands[1]);
                     int endingIndex = int.Parse(commands[2]);
+
                     activationKey = activationKey.Remove(startingIndex, endingIndex - startingIndex);
                     Console.WriteLine(activationKey);
                 }
+
                 commands = Console.ReadLine().Split(">>>");
             }
+
             Console.WriteLine($"Your activation key is: {activationKey}");
         }
     }
