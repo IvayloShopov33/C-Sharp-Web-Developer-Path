7f758a36aafbf1b7bb4a97165cadceb117398710
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-11 21:03:15 +0300

    Update Program.cs

--- Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/05. Programming Fundamentals Final Exam/2. Emoji Detector/Program.cs
+++ Programming Fundamentals with C# - May 2023/15. Final Exam Preparation/05. Programming Fundamentals Final Exam/2. Emoji Detector/Program.cs
@@ -11,6 +11,7 @@ namespace _2._Emoji_Detector
             string pattern = @"(\:{2}|\*{2})(?<emoji>[A-Z][a-z]{2,})\1";
             string input = Console.ReadLine();
             int coolThreshold = 1;
+
             foreach (char item in input)
             {
                 if (char.IsDigit(item))
@@ -19,28 +20,35 @@ namespace _2._Emoji_Detector
                     coolThreshold *= digit;
                 }
             }
+
             MatchCollection matchCollection = Regex.Matches(input, pattern);
             List<string> coolEmojis = new List<string>();
+
             foreach (Match match in matchCollection)
             {
                 CaptureCollection captureCollection = match.Groups["emoji"].Captures;
                 string emojiToString = string.Empty;
+
                 foreach (var item in captureCollection)
                 {
                     emojiToString += item;
                 }
+
                 int asciiCode = 0;
                 foreach (char item in emojiToString)
                 {
                     asciiCode += item;
                 }
+
                 if (asciiCode > coolThreshold)
                 {
                     coolEmojis.Add(match.Value);
                 }
             }
+
             Console.WriteLine($"Cool threshold: {coolThreshold}");
             Console.WriteLine($"{matchCollection.Count} emojis found in the text. The cool ones are:");
+
             foreach (string emoji in coolEmojis)
             {
                 Console.WriteLine(emoji);
