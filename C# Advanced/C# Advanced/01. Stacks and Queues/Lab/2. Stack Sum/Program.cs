89121aee77656eeb4ca8e93eb970cd957dfe5e06
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-15 02:28:43 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/01. Stacks and Queues/Lab/2. Stack Sum/Program.cs
+++ C# Advanced/C# Advanced/01. Stacks and Queues/Lab/2. Stack Sum/Program.cs
@@ -6,24 +6,28 @@
         {
             Stack<int> stackOfNumbers = new Stack<int>();
             int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
             foreach (int number in numbers)
             {
                 stackOfNumbers.Push(number);
             }
+            
             string[] commands = Console.ReadLine().Split();
             string action = commands[0].ToLower();
+            
             while (action != "end")
             {
                 int firstNumber = int.Parse(commands[1]);
-                if (action=="add")
+                
+                if (action == "add")
                 {
                     int secondNumber = int.Parse(commands[2]);
                     stackOfNumbers.Push(firstNumber);
                     stackOfNumbers.Push(secondNumber);
                 }
-                else if (action=="remove")
+                else if (action == "remove")
                 {
-                    if (stackOfNumbers.Count>=firstNumber)
+                    if (stackOfNumbers.Count >= firstNumber)
                     {
                         for (int i = 0; i < firstNumber; i++)
                         {
@@ -31,9 +35,11 @@
                         }
                     }
                 }
+                
                 commands = Console.ReadLine().Split();
                 action = commands[0].ToLower();
             }
+            
             Console.WriteLine($"Sum: {stackOfNumbers.Sum()}");
         }
     }
