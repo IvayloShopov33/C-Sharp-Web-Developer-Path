e6ac0cbdcbd3aa1e949ae955326e853a56881e84
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-21 15:46:23 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/01. Stacks and Queues/Lab/8. Traffic Jam/Program.cs
+++ C# Advanced/C# Advanced/01. Stacks and Queues/Lab/8. Traffic Jam/Program.cs
@@ -5,20 +5,23 @@
         static void Main(string[] args)
         {
             Queue<string> queue = new Queue<string>();
-            int numberOfAllowedPassedCars=int.Parse(Console.ReadLine());
-            string carOrCommand=Console.ReadLine();
             int carsPassed = 0;
-            while (carOrCommand!="end")
+
+            int numberOfAllowedPassedCars = int.Parse(Console.ReadLine());
+            string carOrCommand = Console.ReadLine();
+            
+            while (carOrCommand != "end")
             {
-                if (carOrCommand=="green")
+                if (carOrCommand == "green")
                 {
                     int countOfCars = 0;
                     foreach (string car in queue.ToList())
                     {
-                        if (countOfCars==numberOfAllowedPassedCars || queue.Count==0)
+                        if (countOfCars == numberOfAllowedPassedCars || queue.Count == 0)
                         {
                             break;
                         }
+
                         Console.WriteLine($"{queue.Dequeue()} passed!");
                         countOfCars++;
                         carsPassed++;
@@ -28,8 +31,10 @@
                 {
                     queue.Enqueue(carOrCommand);
                 }
+
                 carOrCommand = Console.ReadLine();
             }
+
             Console.WriteLine($"{carsPassed} cars passed the crossroads.");
         }
     }
