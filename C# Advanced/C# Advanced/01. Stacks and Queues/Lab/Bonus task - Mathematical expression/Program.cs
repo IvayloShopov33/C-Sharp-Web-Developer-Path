3f6cea0626fa7bbe2a4902921e4605d121d2d0c6
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-22 17:32:31 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/01. Stacks and Queues/Lab/Bonus task - Mathematical expression/Program.cs
+++ C# Advanced/C# Advanced/01. Stacks and Queues/Lab/Bonus task - Mathematical expression/Program.cs
@@ -8,13 +8,16 @@ namespace Bonus_task___Mathematical_expression
         {
             string mathExpression = Console.ReadLine(); 
             double result = Evaluate(mathExpression);
+
             Console.WriteLine($"This expression is equal to: {result}");
         }
+
         static double Evaluate(string expression)
         {
             Stack<double> numbers = new Stack<double>();
             Stack<char> operators = new Stack<char>();
             string validOperators = "+-*/^";
+
             for (int i = 0; i < expression.Length; i++)
             {
                 char character = expression[i];
@@ -24,30 +27,35 @@ namespace Bonus_task___Mathematical_expression
                 }
                 else if (character == ')')
                 {
-                    while (operators.Peek()!='(')
+                    while (operators.Peek() != '(')
                     {
                         char @operator = operators.Pop();
                         double firstNum = numbers.Pop();
                         double secondNum = numbers.Pop();
+
                         double newValue = ApplyOperation(@operator, secondNum, firstNum);
                         numbers.Push(newValue);
                     }
+
                     operators.Pop();
                 }
                 else if (validOperators.Contains(character))
                 {
-                    if (character=='-' && operators.Count>0 && operators.Peek() == '+')
+                    if (character == '-' && operators.Count > 0 && operators.Peek() == '+')
                     {
-                            operators.Pop();
+                        operators.Pop();
                     }
+
                     while (operators.Count > 0 && Priority(operators.Peek()) >= Priority(character))
                     {
-                        char @operator=operators.Pop();
-                        double secondNum=numbers.Pop();
-                        double firstNum=numbers.Pop();
+                        char @operator = operators.Pop();
+                        double secondNum = numbers.Pop();
+                        double firstNum = numbers.Pop();
+
                         double newValue = ApplyOperation(@operator, firstNum, secondNum);
                         numbers.Push(newValue);
                     }
+
                     operators.Push(character);
                 }
                 else if (char.IsDigit(character) || character == '.')
@@ -57,26 +65,33 @@ namespace Bonus_task___Mathematical_expression
                     {
                         number.Append(character);
                         i++;
+
                         if (i == expression.Length)
                         {
                             break;
                         }
+
                         character = expression[i];
                     }
+
                     i--;
                     numbers.Push(double.Parse(number.ToString()));
                 }
             }
+
             while (operators.Count > 0)
             {
                 char @operator = operators.Pop();
                 double firstNum = numbers.Pop();
                 double secondNum = numbers.Pop();
+
                 double newValue = ApplyOperation(@operator, secondNum, firstNum);
                 numbers.Push(newValue);
             }
+
             return numbers.Peek();
         }
+
         static double ApplyOperation(char operation, double operand1, double operand2)
         {
             switch (operation)
@@ -89,6 +104,7 @@ namespace Bonus_task___Mathematical_expression
                 default: return 0.0;
             }
         }
+
         static int Priority(char operation)
         {
             switch (operation)
