b6efbf88275bbbe87ee9fc11f6b15600bdcda8e8
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-07-28 07:53:28 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/01. Stacks and Queues/Exercises/06. Songs Queue/Program.cs
+++ C# Advanced/C# Advanced/01. Stacks and Queues/Exercises/06. Songs Queue/Program.cs
@@ -4,8 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Queue<string> songs= new Queue<string>();
+            Queue<string> songs = new Queue<string>();
             string[] initialSongs = Console.ReadLine().Split(", ");
+
             foreach (string song in initialSongs)
             {
                 songs.Enqueue(song);
@@ -14,34 +15,38 @@
             string[] commands;
             while (true)
             {
-                if (songs.Count==0)
+                if (songs.Count == 0)
                 {
                     Console.WriteLine("No more songs!");
                     break;
                 }
                 
                 commands = Console.ReadLine().Split();
-                if (commands[0]=="Play")
+                if (commands[0] == "Play")
                 {
                     songs.Dequeue();
                 }
-                else if (commands[0]=="Add")
+                else if (commands[0] == "Add")
                 {
                     string songName = string.Empty;
                     for (int i = 1; i < commands.Length; i++)
                     {
-                        songName += commands[i]+ " ";
+                        songName += commands[i] + " ";
                     }
                     
-                    songName=songName.Trim();
+                    songName = songName.Trim();
 
                     //check if the queue contains that specific song
-                    if (!songs.Contains(songName)) 
+                    if (!songs.Contains(songName))
+                    {
                         songs.Enqueue(songName);
+                    }
                     else
+                    {
                         Console.WriteLine($"{songName} is already contained!");
+                    }
                 }
-                else if (commands[0]=="Show")
+                else if (commands[0] == "Show")
                 {
                     Console.WriteLine(string.Join(", ", songs));
                 }
