3f9b3740dbb302138a1a0e480346a27c445a0241
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-08-08 15:22:38 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/02. Multidimensional Arrays/Lab/5. Square With Maximum Sum/Program.cs
+++ C# Advanced/C# Advanced/02. Multidimensional Arrays/Lab/5. Square With Maximum Sum/Program.cs
@@ -8,6 +8,7 @@
             int rows = sizesOfTheMatrix[0];
             int cols = sizesOfTheMatrix[1];
             int[,] matrixOfNumbers = new int[rows, cols];
+
             for (int row = 0; row < matrixOfNumbers.GetLength(0); row++)
             {
                 int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
@@ -21,15 +22,18 @@
             int[] subMatrixLineParts = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
             int subRows = subMatrixLineParts[0];
             int subCols = subMatrixLineParts[1];
+
             long maxSum = long.MinValue;
             int maxSumRow = 0;
             int maxSumColumn = 0;
+
             for (int row = 0; row < matrixOfNumbers.GetLength(0) - subRows + 1; row++)
             {
                 for (int col = 0; col < matrixOfNumbers.GetLength(1) - subCols + 1; col++)
                 {
                     //calculate the sum from the current NXN matrix
                     long currentSum = 0;
+
                     for (int i = 0; i < subRows; i++)
                     {
                         for (int j = 0; j < subCols; j++)
@@ -37,6 +41,7 @@
                             currentSum += matrixOfNumbers[row + i, col + j];
                         }
                     }
+
                     //check if it is bigger than the maximum sum
                     if (currentSum > maxSum)
                     {
@@ -54,6 +59,7 @@
                 {
                     Console.Write(matrixOfNumbers[row, col] + " ");
                 }
+
                 Console.WriteLine();
             }
 
