da658921e9db28e07af85749a8b94f1aa1f62560
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-08-10 01:14:53 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/02. Multidimensional Arrays/Lab/7. Pascal Triangle/Program.cs
+++ C# Advanced/C# Advanced/02. Multidimensional Arrays/Lab/7. Pascal Triangle/Program.cs
@@ -6,13 +6,16 @@
         {
             int rows = int.Parse(Console.ReadLine());
             long[][] triangleOfPascal = new long[rows][];
+
             for (int row = 0; row < rows; row++)
             {
                 //initialize array in the jagged array
                 triangleOfPascal[row] = new long[row + 1];
+
                 //calculate the elements of the Pascal's triangle on each row
                 triangleOfPascal[row][0] = 1;
                 triangleOfPascal[row][triangleOfPascal[row].Length - 1] = 1;
+
                 for (int col = 1; col < triangleOfPascal[row].Length - 1; col++)
                 {
                     triangleOfPascal[row][col] = triangleOfPascal[row - 1][col - 1] + triangleOfPascal[row - 1][col];
