c3f4e1a83bbc6706d279af1b3cdf128647c2e4aa
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-08-15 02:30:30 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/02. Multidimensional Arrays/Exercises/05. Snake Moves/Program.cs
+++ C# Advanced/C# Advanced/02. Multidimensional Arrays/Exercises/05. Snake Moves/Program.cs
@@ -4,56 +4,61 @@
     {
         static void Main(string[] args)
         {
-            int[] dimensionsOfTheMatrix = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse).ToArray();
+            int[] dimensionsOfTheMatrix = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
             int rows = dimensionsOfTheMatrix[0];
             int cols = dimensionsOfTheMatrix[1];
             char[,] snakeMoves = new char[rows, cols];
+
             string snake = Console.ReadLine();
             bool leftToRight = true;
             int counter = 0;
+
             for (int row = 0; row < snakeMoves.GetLength(0); row++)
             {
-                //check the next direction of the snake
+                // check the next direction of the snake
                 if (leftToRight)
                 {
                     for (int col = 0; col < snakeMoves.GetLength(1); col++)
                     {
-                        //put the elements in the matrix
+                        // put the elements in the matrix
                         snakeMoves[row, col] = snake[counter++];
+
                         if (snake.Length == counter)
                         {
                             counter = 0;
                         }
                     }
 
-                    //change the direction
+                    // change the direction
                     leftToRight = false;
                 }
                 else
                 {
                     for (int col = snakeMoves.GetLength(1) - 1; col >= 0; col--)
                     {
-                        //put the elements in the matrix
+                        // put the elements in the matrix
                         snakeMoves[row, col] = snake[counter++];
+
                         if (snake.Length == counter)
                         {
                             counter = 0;
                         }
                     }
 
-                    //change the direction
+                    // change the direction
                     leftToRight = true;
                 }
             }
 
-            //print the matrix
+            // print the matrix
             for (int i = 0; i < snakeMoves.GetLength(0); i++)
             {
                 for (int j = 0; j < snakeMoves.GetLength(1); j++)
                 {
                     Console.Write(snakeMoves[i, j]);
                 }
+
                 Console.WriteLine();
             }
         }
