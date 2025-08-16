2d135720eb7a6df2a4876d9064b21106a866fb76
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-08-16 11:24:20 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/02. Multidimensional Arrays/Exercises/06. Jagged Array Manipulator/Program.cs
+++ C# Advanced/C# Advanced/02. Multidimensional Arrays/Exercises/06. Jagged Array Manipulator/Program.cs
@@ -6,17 +6,19 @@
         {
             int rowsCount = int.Parse(Console.ReadLine());
             double[][] jaggedArray = new double[rowsCount][];
+
             for (int row = 0; row < jaggedArray.Length; row++)
             {
                 double[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
-                //put the elements in the jagged array
+
+                // put the elements in the jagged array
                 jaggedArray[row] = numbers;
             }
 
-            //check the lengths of the arrays and multiply/divide their values
+            // check the lengths of the arrays and multiply/divide their values
             for (int row = 0; row < jaggedArray.Length - 1; row++)
             {
-                //multiply if their lengths are equal or divide if they are not equal
+                // multiply if their lengths are equal or divide if they are not equal
                 if (jaggedArray[row].Length == jaggedArray[row + 1].Length)
                 {
                     for (int col = 0; col < jaggedArray[row].Length; col++)
@@ -39,15 +41,17 @@
                 }
             }
 
-            //execute commands (Add, Subtract or End)
+            // execute commands (Add, Subtract or End)
             string[] commands;
+
             while (true)
             {
                 commands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                //check if the name of the command is "End"
+
+                // check if the name of the command is "End"
                 if (commands[0] == "End")
                 {
-                    //print the final state of the jagged array and stop the program
+                    // print the final state of the jagged array and stop the program
                     for (int i = 0; i < jaggedArray.Length; i++)
                     {
                         Console.WriteLine(string.Join(' ', jaggedArray[i]));
@@ -59,17 +63,18 @@
                 int row = int.Parse(commands[1]);
                 int col = int.Parse(commands[2]);
                 int value = int.Parse(commands[3]);
-                //execute the command if the coordinates are valid
+
+                // execute the command if the coordinates are valid
                 if (row >= 0 && row < jaggedArray.Length && col >= 0 && col < jaggedArray[row].Length)
                 {
                     if (commands[0] == "Add")
                     {
-                        //add the value to the element with these coordinates
+                        // add the value to the element with these coordinates
                         jaggedArray[row][col] += value;
                     }
                     else if (commands[0] == "Subtract")
                     {
-                        //subtract the value from the element with these coordinates
+                        // subtract the value from the element with these coordinates
                         jaggedArray[row][col] -= value;
                     }
                 }
