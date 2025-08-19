6020a91e776d0d1c30de3553464547d06c345efe
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-08-20 01:34:07 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/02. Multidimensional Arrays/Exercises/10. Radioactive Mutant Vampire Bunnies/Program.cs
+++ C# Advanced/C# Advanced/02. Multidimensional Arrays/Exercises/10. Radioactive Mutant Vampire Bunnies/Program.cs
@@ -5,20 +5,23 @@
         static void Main(string[] args)
         {
             int[] matrixSize = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
             int rows = matrixSize[0];
             int cols = matrixSize[1];
+
             int playerRow = 0;
             int playerCol = 0;
             char[,] matrix = new char[rows, cols];
+
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 string input = Console.ReadLine();
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    //put the elements in the matrix
+                    // put the elements in the matrix
                     matrix[row, col] = input[col];
                     
-                    //check if the current element is the player
+                    // check if the current element is the player
                     if (matrix[row, col] == 'P')
                     {
                         playerRow = row;
@@ -33,7 +36,7 @@
                 int nextRow = 0;
                 int nextCol = 0;
                 
-                //check the direction
+                // check the direction
                 switch (direction)
                 {
                     case 'U':
@@ -55,9 +58,10 @@
                 bool hasWon = false;
                 bool isDead = false;
                 
-                //remove the player from his current position
+                // remove the player from his current position
                 matrix[playerRow, playerCol] = '.';
-                //check if the player is outside of the matrix and if it is true than he has won the game
+
+                // check if the player is outside of the matrix and if it is true than he has won the game
                 if (playerRow + nextRow < 0 || playerRow + nextRow >= matrix.GetLength(0) || playerCol + nextCol < 0 || playerCol + nextCol >= matrix.GetLength(1))
                 {
                     hasWon = true;
@@ -67,7 +71,7 @@
                     playerRow += nextRow;
                     playerCol += nextCol;
                     
-                    //check if the player reached a bunny or not
+                    // check if the player reached a bunny or not
                     if (matrix[playerRow, playerCol] == 'B')
                     {
                         isDead = true;
@@ -85,7 +89,7 @@
                     {
                         if (matrix[row, col] == 'B')
                         {
-                            //add the coordinates of the bunnies to the list
+                            // add the coordinates of the bunnies to the list
                             bunniesCoordinates.Add(new int[] { row, col });
                         }
                     }
@@ -96,10 +100,10 @@
                     int bunnyRow = coordinates[0];
                     int bunnyCol = coordinates[1];
 
-                    //check if it is possible for the bunnies to propagate down
+                    // check if it is possible for the bunnies to propagate down
                     if (bunnyRow + 1 >= 0 && bunnyRow + 1 < matrix.GetLength(0))
                     {
-                        //check if the bunny reached the player
+                        // check if the bunny reached the player
                         if (matrix[bunnyRow + 1, bunnyCol] == 'P')
                         {
                             isDead = true;
@@ -108,10 +112,10 @@
                         matrix[bunnyRow + 1, bunnyCol] = 'B';
                     }
 
-                    //check if it is possible for the bunnies to propagate up
+                    // check if it is possible for the bunnies to propagate up
                     if (bunnyRow - 1 >= 0 && bunnyRow - 1 < matrix.GetLength(0))
                     {
-                        //check if the bunny reached the player
+                        // check if the bunny reached the player
                         if (matrix[bunnyRow - 1, bunnyCol] == 'P')
                         {
                             isDead = true;
@@ -120,10 +124,10 @@
                         matrix[bunnyRow - 1, bunnyCol] = 'B';
                     }
 
-                    //check if it is possible for the bunnies to propagate right
+                    // check if it is possible for the bunnies to propagate right
                     if (bunnyCol + 1 >= 0 && bunnyCol + 1 < matrix.GetLength(1))
                     {
-                        //check if the bunny reached the player
+                        // check if the bunny reached the player
                         if (matrix[bunnyRow, bunnyCol + 1] == 'P')
                         {
                             isDead = true;
@@ -132,10 +136,10 @@
                         matrix[bunnyRow, bunnyCol + 1] = 'B';
                     }
 
-                    //check if it is possible for the bunnies to propagate left
+                    // check if it is possible for the bunnies to propagate left
                     if (bunnyCol - 1 >= 0 && bunnyCol - 1 < matrix.GetLength(1))
                     {
-                        //check if the bunny reached the player
+                        // check if the bunny reached the player
                         if (matrix[bunnyRow, bunnyCol - 1] == 'P')
                         {
                             isDead = true;
@@ -145,7 +149,7 @@
                     }
                 }
 
-                //check if the player has won and stop the program if it is true
+                // check if the player has won and stop the program if it is true
                 if (hasWon)
                 {
                     for (int row = 0; row < matrix.GetLength(0); row++)
@@ -154,6 +158,7 @@
                         {
                             Console.Write(matrix[row, col]);
                         }
+
                         Console.WriteLine();
                     }
 
@@ -161,7 +166,7 @@
                     Environment.Exit(0);
                 }
 
-                //check if the player is dead and stop the program if it is true
+                // check if the player is dead and stop the program if it is true
                 if (isDead)
                 {
                     for (int row = 0; row < matrix.GetLength(0); row++)
@@ -170,6 +175,7 @@
                         {
                             Console.Write(matrix[row, col]);
                         }
+
                         Console.WriteLine();
                     }
 
