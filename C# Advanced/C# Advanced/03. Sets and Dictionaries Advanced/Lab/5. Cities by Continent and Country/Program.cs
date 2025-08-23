d9b0671a291929d68b92b3eaf500cb52cb31552a
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-08-24 02:05:33 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/03. Sets and Dictionaries Advanced/Lab/5. Cities by Continent and Country/Program.cs
+++ C# Advanced/C# Advanced/03. Sets and Dictionaries Advanced/Lab/5. Cities by Continent and Country/Program.cs
@@ -7,36 +7,39 @@
             Dictionary<string, Dictionary<string, List<string>>> citiesByCountryAndContinent =
                 new Dictionary<string, Dictionary<string, List<string>>>();
             int inputLinesCount = int.Parse(Console.ReadLine());
+
             string[] continentsDetails;
             string continent = string.Empty;
             string country = string.Empty;
             string city = string.Empty;
+
             for (int i = 1; i <= inputLinesCount; i++)
             {
                 continentsDetails = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
                 continent = continentsDetails[0];
                 country = continentsDetails[1];
                 city = continentsDetails[2];
 
-                //check if the continent is not in the dictionary
+                // check if the continent is not in the dictionary
                 if (!citiesByCountryAndContinent.ContainsKey(continent))
                 {
-                    //add the continent and the country to the dictionary
+                    // add the continent and the country to the dictionary
                     citiesByCountryAndContinent.Add(continent, new Dictionary<string, List<string>>());
                 }
 
-                //check if the country is not present in the dictionary
+                // check if the country is not present in the dictionary
                 if (!citiesByCountryAndContinent[continent].ContainsKey(country))
                 {
-                    //add the country to the dictionary
+                    // add the country to the dictionary
                     citiesByCountryAndContinent[continent].Add(country, new List<string>());
                 }
 
-                //add the city to the dictionary
+                // add the city to the dictionary
                 citiesByCountryAndContinent[continent][country].Add(city);
             }
 
-            //print the continents with their countries and cities
+            // print the continents with their countries and cities
             foreach (var item in citiesByCountryAndContinent)
             {
                 Console.WriteLine(item.Key + ":");
