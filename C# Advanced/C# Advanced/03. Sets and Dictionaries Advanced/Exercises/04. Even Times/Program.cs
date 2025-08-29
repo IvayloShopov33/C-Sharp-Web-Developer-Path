9d0c3a67071601f48aa80065ee62371173c7ca94
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-08-30 02:31:09 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/03. Sets and Dictionaries Advanced/Exercises/04. Even Times/Program.cs
+++ C# Advanced/C# Advanced/03. Sets and Dictionaries Advanced/Exercises/04. Even Times/Program.cs
@@ -6,23 +6,24 @@
         {
             Dictionary<int, int> numbers = new Dictionary<int, int>();
             int numbersCount = int.Parse(Console.ReadLine());
+
             for (int i = 1; i <= numbersCount; i++)
             {
                 int inputNumber = int.Parse(Console.ReadLine());
                 
-                //check if the element is not present in the dictionary
+                // check if the element is not present in the dictionary
                 if (!numbers.ContainsKey(inputNumber))
                 {
                     numbers.Add(inputNumber, 0);
                 }
 
-                //increase the value of this number
+                // increase the value of this number
                 numbers[inputNumber]++;
             }
 
             foreach (var number in numbers)
             {
-                //check if the number is appeared an even number of times
+                // check if the number is appeared an even number of times
                 if (number.Value % 2 == 0)
                 {
                     Console.WriteLine(number.Key);
