7e757268b7667a3a96dcdecaea58d612cce3020e
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-08-31 17:49:41 +0300

    Update Program.cs

--- C# Advanced/C# Advanced/03. Sets and Dictionaries Advanced/Exercises/05. Count Symbols/Program.cs
+++ C# Advanced/C# Advanced/03. Sets and Dictionaries Advanced/Exercises/05. Count Symbols/Program.cs
@@ -6,20 +6,21 @@
         {
             SortedDictionary<char, int> charactersOccurrences = new SortedDictionary<char, int>();
             string inputText = Console.ReadLine();
+
             foreach (char @char in inputText)
             {
-                //check if the character is not present in the sorted dictionary
+                // check if the character is not present in the sorted dictionary
                 if (!charactersOccurrences.ContainsKey(@char))
                 {
-                    //add the character to the sorted dictionary
+                    // add the character to the sorted dictionary
                     charactersOccurrences.Add(@char, 0);
                 }
 
-                //increase its value
+                // increase its value
                 charactersOccurrences[@char]++;
             }
 
-            //print all characters with their occurrences in ascending order
+            // print all characters with their occurrences in ascending order
             foreach (var item in charactersOccurrences)
             {
                 Console.WriteLine($"{item.Key}: {item.Value} time/s");
