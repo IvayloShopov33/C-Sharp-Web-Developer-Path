e89e923ff0d7ef206e02036fe68fa143b6bbe7b5
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-09-07 02:26:36 +0300

    Update LineNumbers.cs

--- C# Advanced/C# Advanced/04. Streams, Files and Directories/Lab/2. LineNumbers/LineNumbers.cs
+++ C# Advanced/C# Advanced/04. Streams, Files and Directories/Lab/2. LineNumbers/LineNumbers.cs
@@ -18,13 +18,15 @@ namespace LineNumbers
             using StreamReader input = new StreamReader(inputFilePath);
             using StreamWriter output = new StreamWriter(outputFilePath);
 
-            //print all lines with a counter
+            // print all lines with a counter
             int counter = 0;
+            
             while (!input.EndOfStream)
             {
                 counter++;
                 string line = input.ReadLine();
                 string textToPrint = $"{counter}. {line}";
+                
                 output.WriteLine(textToPrint);
                 Console.WriteLine(textToPrint);
             }
