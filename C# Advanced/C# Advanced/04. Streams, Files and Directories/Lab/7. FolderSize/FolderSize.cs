f2a6dfd64bafa86700668f99927086f599279e07
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-09-11 18:09:23 +0300

    Update FolderSize.cs

--- C# Advanced/C# Advanced/04. Streams, Files and Directories/Lab/7. FolderSize/FolderSize.cs
+++ C# Advanced/C# Advanced/04. Streams, Files and Directories/Lab/7. FolderSize/FolderSize.cs
@@ -14,18 +14,19 @@ namespace FolderSize
 
         public static void GetFolderSize(string folderPath, string outputFilePath)
         {
-            //convert the total sum from bytes to kilobytes
+            // convert the total sum from bytes to kilobytes
             decimal sumOfFileSizes = CalculateTotalSumOfFileSizes(folderPath) / 1024.0m;
+
             string outputText = $"{sumOfFileSizes} KB";
             File.WriteAllText(outputFilePath, outputText);
-
         }
 
         static long CalculateTotalSumOfFileSizes(string folderPath)
         {
             string[] files = Directory.GetFiles(folderPath);
             long totalSumOfFileSizes = 0;
-            //add the sizes of files and directories to the total sum
+
+            // add the sizes of files and directories to the total sum
             foreach (string file in files)
             {
                 totalSumOfFileSizes += new FileInfo(file).Length;
