16c592dc1e97d7f5f46ddd2bb5eaf368876d57cc
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-10-04 00:02:35 +0200

    Update Program.cs

--- C# Advanced/C# Advanced/06. Defining Classes/Exercises/03. Oldest Family Member/Program.cs
+++ C# Advanced/C# Advanced/06. Defining Classes/Exercises/03. Oldest Family Member/Program.cs
@@ -6,11 +6,13 @@
         {
             Family family = new Family();
             int familyMembersCount = int.Parse(Console.ReadLine());
+            
             for (int i = 1; i <= familyMembersCount; i++)
             {
                 string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
                 string name = personDetails[0];
                 int age = int.Parse(personDetails[1]);
+                
                 Person person = new Person(name, age);
                 family.AddMember(person);
             }
