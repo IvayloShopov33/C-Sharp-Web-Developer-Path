c356690b6395fd80d5bbbcffbb81b5409ad03fec
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-10-28 00:29:56 +0200

    Update Program.cs

--- C# Advanced/C# Advanced/07. Generics/Exercises/11. Implement Custom Stack/Program.cs
+++ C# Advanced/C# Advanced/07. Generics/Exercises/11. Implement Custom Stack/Program.cs
@@ -5,24 +5,29 @@
         static void Main(string[] args)
         {
             CustomStack<int> stack = new CustomStack<int>();
+
             stack.Push(1);
             stack.Push(2);
             stack.Push(3);
             stack.Push(4);
             stack.Push(5);
+
             stack.Pop();
             stack.Push(6);
+
             stack.Pop();
             stack.ForEach(Console.WriteLine);
 
             CustomStack<string> customStack = new CustomStack<string>();
+
             customStack.Push("Hello");
             customStack.Push("world!");
+
             customStack.ForEach(Console.WriteLine);
 
             foreach (string item in customStack)
             {
-                Console.Write(item+" ");
+                Console.Write(item + " ");
             }
         }
     }
