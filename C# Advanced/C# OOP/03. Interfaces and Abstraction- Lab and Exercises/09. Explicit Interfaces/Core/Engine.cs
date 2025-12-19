801b5998827ff92250e67ebdd2475f525ffc45f1
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-12-19 17:18:00 +0200

    Update Engine.cs

--- C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/09. Explicit Interfaces/Core/Engine.cs
+++ C# Advanced/C# OOP/03. Interfaces and Abstraction- Lab and Exercises/09. Explicit Interfaces/Core/Engine.cs
@@ -21,6 +21,7 @@ namespace ExplicitInterfaces.Core
 
                 IPerson person = new Citizen(input[0], input[1], int.Parse(input[2]));
                 IResident resident = new Citizen(input[0], input[1], int.Parse(input[2]));
+
                 Console.WriteLine(person.GetName());
                 Console.WriteLine(resident.GetName() + person.GetName());
             }
