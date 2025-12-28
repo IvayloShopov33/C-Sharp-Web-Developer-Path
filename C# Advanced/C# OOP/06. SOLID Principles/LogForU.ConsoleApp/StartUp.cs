1a75593b76d6b2d0aacce49f9c02446f3483c1cf
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2025-12-28 21:43:45 +0200

    Reorganize using directives in StartUp.cs

--- C# Advanced/C# OOP/06. SOLID Principles/LogForU.ConsoleApp/StartUp.cs
+++ C# Advanced/C# OOP/06. SOLID Principles/LogForU.ConsoleApp/StartUp.cs
@@ -1,11 +1,13 @@
 ﻿using LogForU.ConsoleApp.Core;
 using LogForU.ConsoleApp.Core.Interfaces;
 using LogForU.ConsoleApp.CustomLayouts;
+
 using LogForU.Core.Appenders;
 using LogForU.Core.Appenders.Interfaces;
 using LogForU.Core.IO;
 using LogForU.Core.IO.Interfaces;
 using LogForU.Core.Layouts;
+
 namespace LogForU.ConsoleApp
 {
     public class StartUp
