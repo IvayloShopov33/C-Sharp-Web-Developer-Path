a769ae3d62fa0923f02757368bd555ca2c4a05fa
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2026-03-13 18:32:48 +0200

    Update ConnectionManager.cs

--- C# DB/Entity Framework Core - June 2024/02. ORM Fundamentals/Exercises/MiniORM/ConnectionManager.cs
+++ C# DB/Entity Framework Core - June 2024/02. ORM Fundamentals/Exercises/MiniORM/ConnectionManager.cs
@@ -8,10 +8,10 @@ namespace MiniORM;
 internal class ConnectionManager : IDisposable
 {
     private readonly DatabaseConnection connection;
+
     public ConnectionManager(DatabaseConnection connection)
     {
         this.connection = connection;
-
         this.connection.Open();
     }
 
