0f48d287746f16f5ff1b85d98c5716ea1f94b998
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2026-04-12 21:35:35 +0300

    Update Artist.cs

--- C# DB/Entity Framework Core - June 2024/05. Linq/Lab/Data/Models/Artist.cs
+++ C# DB/Entity Framework Core - June 2024/05. Linq/Lab/Data/Models/Artist.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Data.Models
@@ -17,6 +18,7 @@ namespace Demo.Data.Models
 
         [Key]
         public int Id { get; set; }
+
         public DateTime CreatedOn { get; set; }
         public DateTime? ModifiedOn { get; set; }
         public bool IsDeleted { get; set; }
