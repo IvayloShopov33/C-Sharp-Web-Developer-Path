6cb6af23523083e77c85014b383de097116d9e3d
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2026-04-18 18:57:50 +0300

    Update ArtistMetadatum.cs

--- C# DB/Entity Framework Core - June 2024/05. Linq/Lab/Data/Models/ArtistMetadatum.cs
+++ C# DB/Entity Framework Core - June 2024/05. Linq/Lab/Data/Models/ArtistMetadatum.cs
@@ -20,9 +20,13 @@ namespace Demo.Data.Models
         public DateTime? ModifiedOn { get; set; }
 
         public bool IsDeleted { get; set; }
+
         public DateTime? DeletedOn { get; set; }
+
         public int ArtistId { get; set; }
+
         public int Type { get; set; }
+
         public string? Value { get; set; }
         public int? SourceId { get; set; }
         public string? SourceItemId { get; set; }
