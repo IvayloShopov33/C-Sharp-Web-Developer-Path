51656a3744f066be0f498bd704d0eca77d28a240
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2026-05-04 20:12:39 +0300

    Update Artist.cs

--- C# DB/Entity Framework Core - June 2024/06. Advanced Querying/Lab/Data/Models/Artist.cs
+++ C# DB/Entity Framework Core - June 2024/06. Advanced Querying/Lab/Data/Models/Artist.cs
@@ -18,8 +18,11 @@ namespace Demo.Data.Models
         public DateTime CreatedOn { get; set; }
 
         public DateTime? ModifiedOn { get; set; }
+
         public bool IsDeleted { get; set; }
+
         public DateTime? DeletedOn { get; set; }
+
         public string? Name { get; set; }
 
         [Column(TypeName = "money")]
