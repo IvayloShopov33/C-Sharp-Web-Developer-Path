086dee79d5a444839cb5f79c0ae3cf02ab9221ee
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2026-09-14 18:14:01 +0300

    Update 02. Champions Cup.js

--- SoftUniada 2024/02. Champions Cup.js
+++ SoftUniada 2024/02. Champions Cup.js
@@ -3,10 +3,12 @@ function solve(input) {
     const width = 5 * input;
     let addedRows = input / 2;
     let moreDots = 0;
+    
     for (let index = 0; index < addedRows; index++) {
         let row = '.'.repeat(input + moreDots);
         row += '#'.repeat(width - 2 * (input + moreDots));
         row += '.'.repeat(input + moreDots);
+        
         console.log(row);
         moreDots++;
     }
@@ -14,9 +16,11 @@ function solve(input) {
     for (let index = 0; index < 1 + addedRows; index++) {
         let row = '.'.repeat(input + moreDots);
         row += '#';
+        
         const reversedRow = row.split('').reverse().join('');
         row += '.'.repeat(width - 2 * (row.length));
         row += reversedRow;
+        
         console.log(row);
         moreDots++;
     }
@@ -24,6 +28,7 @@ function solve(input) {
     let row = '.'.repeat(input + moreDots - 1);
     row += '#'.repeat(width - 2 * (input + moreDots - 1));
     row += '.'.repeat(input + moreDots - 1);
+    
     console.log(row);
     moreDots -= 3;
 
@@ -36,6 +41,7 @@ function solve(input) {
 
     const specificText = 'D^A^N^C^E^';
     const distanceToText = (width - 10) / 2;
+    
     row = '.'.repeat(distanceToText);
     row += specificText;
     row += '.'.repeat(distanceToText);
